cb9f28dba91d05c18808d2adb6085c4066eb8c57
Brian Broll <brian.broll@vanderbilt.edu>
2015-10-19 12:29:15 -0500

    Enabled CORS. Fixes #83

--- src/server/Server.js
+++ src/server/Server.js
@@ -68,6 +68,13 @@ Server.prototype.configureRoutes = function() {
     this.app.use(cookieParser());
     this.app.use(expressSession({secret: sessionSecret}));
 
+    // CORS
+    this.app.use(function(req, res, next) {
+      res.header("Access-Control-Allow-Origin", "*");
+      res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+      next();
+    });
+
     // Add routes
     this.app.use('/rpc', this.rpcManager.router);
     this.app.use('/api', createRouter.call(this));
