7a0fff8fd1e0c05e67fc19cf88ab67c4fd3090cc
Brian Broll <brian.broll@vanderbilt.edu>
2016-02-01 09:29:09 -0600

    Updated var frame in msg receiving. Fixes #179

--- src/client/build.js
+++ src/client/build.js
@@ -29140,7 +29140,7 @@ Process.prototype._createMsg = function (name) {
  */
 Process.prototype.receiveSocketMessage = function (fields) {
     var varFrame = this.context.outerContext.variables,
-        names = this.context.variables.names(),
+        names = varFrame.names(),
         content;
 
     // If we haven't received a message, do nothing
