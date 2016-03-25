1ef83fcaab4a23a04b16fe576391eb782ca61595
Brian Broll <brian.broll@vanderbilt.edu>
2016-03-25 12:36:54 -0500

    Updated examples/ dir structure. Fixes #276

--- src/server/examples/index.js
+++ src/server/examples/index.js
@@ -2,118 +2,51 @@
 var fs = require('fs'),
     path = require('path');
 
-var example = function(name) {
-    name = path.join(__dirname, name + '.xml');
-    return fs.readFileSync(name, 'utf8');
-};
-
 // Create the dictionary of examples
 var examples = {};
 
-// Multiplayer examples
-[
-    // Database example
-    {
-        RoomName: 'Database',
-        cachedProjects: {
-            database: example('database'),
-            client: example('client')
-        }
-    },
-    // Dice Game
-    {
-        RoomName: 'Dice',
-        cachedProjects: {
-            p1: example('dice'),
-            p2: example('dice')
-        }
-    },
-    // Caesar Shift
-    {
-        RoomName: 'Caesar Shift',
-        cachedProjects: {
-            eve: example('cs-eve'),
-            alice: example('cs-alice'),
-            bob: example('cs-bob'),
-            'super eve': example('cs-super-eve')
-        }
-    },
-    // Pong
-    {
-        RoomName: 'Pong',
-        cachedProjects: {
-            left: example('Pong-left'),
-            right: example('Pong-right')
-        }
-    },
-    // TicTacToe (no rpcs)
-    {
-        RoomName: 'TicTacToe',
-        cachedProjects: {
-            X: example('TTT-advanced-X'),
-            O: example('TTT-advanced-O')
-        }
-    },
-    // TicTacToe
-    {
-        RoomName: 'Simple TicTacToe',
-        cachedProjects: {
-            X: example('TicTacToe-x'),
-            O: example('TicTacToe-o')
-        }
-    },
-    {
-        RoomName: 'MarcoPolo',
-        cachedProjects: {
-            player1: example('marco'),
-            player2: example('polo')
-        }
-    },
-    // Single user examples (RPC's and stuff)
-    {
-        RoomName: 'AirQuality',
-        cachedProjects: {
-            AirQuality: example('Air Quality')
-        }
-    },
-    {
-        RoomName: 'Earthquakes',
-        cachedProjects: {
-            Earthquakes: example('Earthquakes')
-        }
-    },
-    {
-        RoomName: 'Weather',
-        cachedProjects: {
-            Weather: example('Weather')
-        }
-    },
-    {
-        RoomName: 'SimpleHangman',
-        cachedProjects: {
-            Hangman: example('SimpleHangman')
-        }
-    }
-]
-.forEach(item => {
-    var roles = Object.keys(item.cachedProjects),
-        src;
+// Read in the directories
+fs.readdirSync(__dirname)
+    .map(dir => path.join(__dirname, dir))
+    .filter(name => fs.lstatSync(name).isDirectory())
+    // create pairs -> [dirname, [xml, files]]
+    .map(dir => [path.basename(dir), fs.readdirSync(dir)
+        .filter(name => path.extname(name) === '.xml')]
+    )
+    // create the example data objects
+    .map(pair => {
+        var result = {},
+            clients = pair[1];
 
-    item.roles = {};
-    for (var i = roles.length; i--;) {
-        item.roles[roles[i]] = null;
-        // TODO: FIXME: the cachedProjects are not the correct format
-        src = item.cachedProjects[roles[i]];
-        item.cachedProjects[roles[i]] = {
-            SourceCode: src,
-            ProjectName: roles[i],
-            RoomName: item.RoomName,
-            Media: '<media></media>'
-        };
-    }
+        result.RoomName = pair[0];
+        result.cachedProjects = {};
 
-    // Add to examples dictionary
-    examples[item.RoomName] = item;
-});
+        // Add project source
+        for (var i = clients.length; i--;) {
+            result.cachedProjects[clients[i].replace('.xml', '')] =
+                fs.readFileSync(path.join(__dirname, pair[0], clients[i]), 'utf8');
+        }
+        return result;
+    })
+    .forEach(item => {
+        var roles = Object.keys(item.cachedProjects),
+            src;
+
+        item.roles = {};
+        for (var i = roles.length; i--;) {
+            item.roles[roles[i]] = null;
+            // TODO: FIXME: the cachedProjects are not the correct format
+            src = item.cachedProjects[roles[i]];
+            item.cachedProjects[roles[i]] = {
+                SourceCode: src,
+                ProjectName: roles[i],
+                RoomName: item.RoomName,
+                Media: '<media></media>'
+            };
+        }
+
+        // Add to examples dictionary
+        examples[item.RoomName] = item;
+    });
 
 module.exports = Object.freeze(examples);
