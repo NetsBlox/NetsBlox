b370bc7f7befbff72abf44f481cea110a962dc4e
MD Asif Hasan <d.asif.hasan@gmail.com>
2016-11-03 18:08:01 -0500

    #518 Connect N (#533)
    * #518 Connect N
    
    * WIP #518 Added ConnectN tests
    
    * #518 Resolved CodeClimate issues
    
    * WIP #518 Resolved CodeClimate Issues
    
    * WIP #518 Renamed ConnectNRPC to ConnectN
    
    * WIP #518
    Fixed validity check for a move.
    Updated RPC to send friendly message on failed move.
    Updated Client to show messages on failed move.
    
    * WIP #518 cleaned up console.log calls
    
    * WIP #518
    Updated the ConnectN RPC to send friendly message for NewGame calls.
    Updated the ConnectN Client side to show the messages sent by the RPC NewGame call.
    
    * WIP #518
    Remove NumDotsToConnect variable from the client side.
    Checking for a valid initial grid size and N is done on the server side.
    Included logic to set to default grid size and the value for N if invalid data is passed.
    
    * WIP #518
    - Server would return winner as "DRAW" when match is drawn.
    - Cleaned up client side
    
    * WIP #518 Cleaned the connectn test file.
    
    * WIP #518 fix CodeClimate style isues
    
    * WIP #518 fix CodeClimate style isues
    
    * WIP #518 fix CodeClimate style isues
    
    * WIP #518 fix set row and col size to default on invalid values.
    
    * WIP #518
    - Removed unused blockes: "is game over" and "winner"
    - Reset the starting parameters to new game RPC to 5x5 and 4 dots to connect
    
    * WIP #518  Add the connectN RPC blocks to the "Remote Calls..." dialog
    
    * WIP #518 Added comments for Play and NewGame RPC calls in NetsBlox.

---
+++ test/server/procedures/connectn.spec.js
@@ -0,0 +1,58 @@
+/*globals describe,it,before,beforeEach,afterEach*/
+'use strict';
+
+var ConnectN = require('../../../src/server/rpc/procedures/ConnectN/ConnectN.js'),
+    RPCMock = require('../../assets/MockRPC'),
+    connectn = new RPCMock(ConnectN),
+    assert = require('assert');
+
+// Quick mocks for express req, res objects
+
+describe('ConnectN Tests', function() {
+
+    describe('newGame', function() {
+        it('should detect invalid number for rows', function() {
+            var result = connectn.newGame({row: -4}),
+                board = connectn._rpc.board;
+            assert.equal(board.length, 3);
+        });
+
+        it('should detect invalid number for column', function() {
+            var result = connectn.newGame({column: -4}),
+                board = connectn._rpc.board;
+            assert.equal(board[0].length, 3);
+        });
+
+        it('should default to 3 rows; 3 columns', function() {
+            var result = connectn.newGame(),
+                board = connectn._rpc.board;
+
+            assert.equal(board.length, 3);
+            assert.equal(board[0].length, 3);
+        });
+    });
+
+    describe('play', function() {
+        afterEach(function() {
+            connectn.newGame();
+        });
+        beforeEach(function() {
+            connectn.newGame();
+        });
+
+        it('should not play in bad position', function() {
+            var code = connectn.play({uuid: 'p1', row: 3, column: -1}).code;
+            assert.equal(code, 400);
+        });
+
+        it('should not play if winner is found', function() {
+            var code;
+            connectn._rpc._winner = 'cat';
+            code = connectn.play({uuid: 'p1', row: 3, column: -1}).code;
+            assert.equal(code, 400);
+        });
+    });
+
+
+
+});
