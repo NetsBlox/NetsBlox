9e123bbe05cb3e94df8a7f6597b28b9a677ac4a0
Brian Broll <brian.broll@vanderbilt.edu>
2016-12-07 13:06:30 -0600

    Moved build file to src/client/dist/. Fixes #573 (#574)
    * Moved build file to src/client/dist/. Fixes #573
    
    * WIP #573 removed build file from vcs
    
    * WIP #573 moved target dir to src/client/dist
    
    * WIP #573 Added readme for dist/

--- bin/build.js
+++ bin/build.js
@@ -2,8 +2,7 @@
 
 var fs = require('fs'),
     path = require('path'),
-    srcPath = path.join(__dirname, '..', 'src', 'client'),
-    dstPath = path.join(__dirname, '..', 'build');
+    srcPath = path.join(__dirname, '..', 'src', 'client');
 
 // Get the given js files
 var devHtml = fs.readFileSync(path.join(srcPath, 'netsblox-dev.html'), 'utf8'),
@@ -22,7 +21,7 @@ var src = srcFiles
     .map(file => fs.readFileSync(file, 'utf8'))
     .join('\n');
 
-var ugly = require("uglify-js");
+var ugly = require('uglify-js');
 
 console.log('dev src length:', src.length);
 var final_code = ugly.minify(srcFiles, {outSourceMap: path.join(srcPath, 'netsblox-build.js.map')});
@@ -30,4 +29,4 @@ var final_code = ugly.minify(srcFiles, {outSourceMap: path.join(srcPath, 'netsbl
 console.log('output length:', final_code.code.length);
 console.log('compression ratio:', 1-(final_code.code.length/src.length));
 
-fs.writeFileSync(path.join(srcPath, 'netsblox-build.js'), final_code.code);
+fs.writeFileSync(path.join(srcPath, 'dist', 'netsblox-build.js'), final_code.code);
