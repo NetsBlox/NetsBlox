6c03d453e0502ad7862301bc23bf9e65af3a3b98
Brian Broll <brian.broll@vanderbilt.edu>
2016-12-19 10:04:16 -0600

    WIP #580 updated NASA rpc

--- src/server/rpc/procedures/NASA/NASA.js
+++ src/server/rpc/procedures/NASA/NASA.js
@@ -3,68 +3,72 @@
 'use strict';
 
 var debug = require('debug'),
-	log = debug('NetsBlox:RPCManager:NASA:log'),
-	error = debug('NetsBlox:RPCManager:NASA:error'),
-	trace = debug('NetsBlox:RPCManager:NASA:trace'),
-	request = require('request'),
-	KEY = process.env.NASA_KEY,
-	APOD_URL = 'https://api.nasa.gov/planetary/apod?api_key=' + KEY,
-	MARS_URL = 'http://marsweather.ingenology.com/v1/latest/';
+    log = debug('NetsBlox:RPCManager:NASA:log'),
+    error = debug('NetsBlox:RPCManager:NASA:error'),
+    trace = debug('NetsBlox:RPCManager:NASA:trace'),
+    request = require('request'),
+    KEY = process.env.NASA_KEY,
+    APOD_URL = 'https://api.nasa.gov/planetary/apod?api_key=' + KEY,
+    MARS_URL = 'http://marsweather.ingenology.com/v1/latest/';
 
 module.exports = {
 
-	isStateless: true,
-	getPath: () => '/NASA',
-	getActions: () => ['apod', 'apodMedia', 'marsHighTemp', 'marsLowTemp', 'marsWeather'], // list of available functions for client to use
+    isStateless: true,
+    getPath: () => '/NASA',
 
-	// NASA's 'Astronomy Picture of the Day'
-	apod: function(req, res) {
-		request(APOD_URL, function(err, response, body) {
-			body = JSON.parse(body);
-			var msg = {
-				type: 'message',
-				msgType: 'Astronomy Pic of the Day',
-				dstId: req.netsbloxSocket.roleId,
-				content: {
-					date: body.date,
-					title: body.title,
-					link: body.url,
-					description: body.explanation
-				}
-			};
-			req.netsbloxSocket.send(msg);
-			return res.json(true);
-		});
-	},
+    // NASA's 'Astronomy Picture of the Day'
+    apod: function() {
+        request(APOD_URL, function(err, response, body) {
+            body = JSON.parse(body);
+            var msg = {
+                type: 'message',
+                msgType: 'Astronomy Pic of the Day',
+                dstId: this.socket.roleId,
+                content: {
+                    date: body.date,
+                    title: body.title,
+                    link: body.url,
+                    description: body.explanation
+                }
+            };
+            this.socket.send(msg);
+            return this.response.json(true);
+        });
+        return null;
+    },
 
-	// NASA's 'Astronomy Picture of the Day' media
-	apodMedia: function(req, res) {
-		request(APOD_URL, function(err, response, body) {
-			body = JSON.parse(body);
-			request.get(body.url).pipe(res);
-		});
-	},
+    // NASA's 'Astronomy Picture of the Day' media
+    apodMedia: function() {
+        request(APOD_URL, function(err, response, body) {
+            body = JSON.parse(body);
+            request.get(body.url).pipe(this.response);
+        });
+        return null;
+    },
 
-	// Latest Mars data according to MAAS
-	marsHighTemp: function(req, res) {
-		request(MARS_URL, function(err, response, body) {
-			body = JSON.parse(body);
-			return res.json(body.report.max_temp_fahrenheit);
-		});
-	},
+    // Latest Mars data according to MAAS
+    marsHighTemp: function() {
+        request(MARS_URL, function(err, response, body) {
+            body = JSON.parse(body);
+            return this.response.json(body.report.max_temp_fahrenheit);
+        });
+        return null;
+    },
 
-	marsLowTemp: function(req, res) {
-		request(MARS_URL, function(err, response, body) {
-			body = JSON.parse(body);
-			return res.json(body.report.min_temp_fahrenheit);
-		});
-	},
+    marsLowTemp: function() {
+        request(MARS_URL, function(err, response, body) {
+            body = JSON.parse(body);
+            return this.response.json(body.report.min_temp_fahrenheit);
+        });
+        return null;
+    },
 
-	marsWeather: function(req, res) {
-		request(MARS_URL, function(err, response, body) {
-			body = JSON.parse(body);
-			return res.json(body.report.atmo_opacity);
-		});
-	}
+    marsWeather: function() {
+        request(MARS_URL, function(err, response, body) {
+            body = JSON.parse(body);
+            return this.response.json(body.report.atmo_opacity);
+        });
+        return null;
+    }
 
 };
