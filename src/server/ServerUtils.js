b31c00ff901dc7594653e811e8a074226279e640
Brian Broll <brian.broll@vanderbilt.edu>
2017-01-11 13:10:27 -0600

    WIP #707 organized ServerUtils

--- src/server/ServerUtils.js
+++ src/server/ServerUtils.js
@@ -3,13 +3,16 @@
 
 var R = require('ramda'),
     assert = require('assert'),
-    path = require('path'),
-    fs = require('fs'),
     debug = require('debug'),
     info = debug('NetsBlox:API:Utils:info'),
     trace = debug('NetsBlox:API:Utils:trace'),
     error = debug('NetsBlox:API:Utils:error');
 
+var uuid = function(owner, name) {
+    return owner + '/' + name;
+};
+
+// Helpers for routes
 var serializeArray = function(content) {
     assert(content instanceof Array);
     return content.map(serialize).join(' ');
@@ -18,10 +21,6 @@ var serializeArray = function(content) {
 var serialize = function(service) {
     var pairs = R.toPairs(service);
     return encodeURI(pairs.map(R.join('=')).join('&'));
-};
-
-var uuid = function(owner, name) {
-    return owner + '/' + name;
 };
 
 var serializeRole = (project, roomName) => {
@@ -33,7 +32,6 @@ var serializeRole = (project, roomName) => {
         project))}&SourceCode=${src}`;
 };
 
-// Helper for routes
 var joinActiveProject = function(userId, room, res) {
     var serialized,
         openRole,
