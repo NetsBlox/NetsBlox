f71bef03604ae02f7548c9bd4765bb4cf168297a
Brian Broll <brian.broll@vanderbilt.edu>
2017-02-14 16:25:10 -0600

    WIP #764 minor refactors; added logs

--- src/server/rpc/procedures/g-trends/g-trends.js
+++ src/server/rpc/procedures/g-trends/g-trends.js
@@ -8,28 +8,28 @@ var debug = require('debug'),
     googleTrends = require('google-trends-api'),
     translate = require('google-translate-api'),
     error = debug('netsblox:rpc:trends:error'),
-    trace = debug('netsblox:rpc:trends:trace');
-let q = require('q');
+    trace = debug('netsblox:rpc:trends:trace'),
+    countryInfoBaseUrl = 'http://ws.geonames.org/countryCodeJSON?',
+    username = 'demo',  // TODO: real username (probably from an env var)
+    Q = require('q');
 
 TrendsRPC.byLocation = function(latitude, longitude) {
 
     // get location data eg: country, language
     // or can use geocoder package
-    let countryInfoBaseUrl = 'http://ws.geonames.org/countryCodeJSON?';
-
-    let username = 'demo';
     let url = `${countryInfoBaseUrl}radius=${100}&lat=${latitude}&lng=${longitude}&username=${username}`,
         response = this.response,
         socket = this.socket;
+
     trace('Requesting country data from ', url);
     request(url, (err, res, body) => {
         if (err) {
+            error('Could not request country data', err);
             return response.status(500).send('ERROR: ' + err);
         }
         // TODO check if body is expected - try catch
         let countryInfo = JSON.parse(body);
         trace('detected country: ', countryInfo.countryName, countryInfo, 'long', longitude, 'lat', latitude);
-        // TODO synchronized sth? promise? gonna do it this way for now
         if (typeof countryInfo.countryCode != 'undefined') {
             // TODO google does not use official country codes for trends see VN vs VE
             googleTrends.hotTrends(countryInfo.countryCode)
@@ -40,13 +40,13 @@ TrendsRPC.byLocation = function(latitude, longitude) {
                     let translatePromisesArr = trendsArr.map((val) => {
                         return translate(val,{to:'en'});
                     });
-                    return q.all(translatePromisesArr);
+                    return Q.all(translatePromisesArr);
                 })
                 .then((translatedArr) => {
                     let trendsTexts = translatedArr.map(val => val.text);
                     response.json(trendsTexts);
                 })
-                // doesn't catch some errors..
+                // doesn't catch some errors.. ?
                 .catch((err) => {
                     error(err);
                     showError(`no trends available for ${countryInfo.countryCode}`);
@@ -55,7 +55,7 @@ TrendsRPC.byLocation = function(latitude, longitude) {
         }else{
             showError('failed to detect the country.');
         }
-    }); //end of request
+    }); // end of request
 
     function showError(err){
         let msg = {
@@ -66,11 +66,11 @@ TrendsRPC.byLocation = function(latitude, longitude) {
                 array: err
             }
         };
+        // TODO: handle the error a different way (no longer using messages)
         socket.send(msg);
     }
 
-    // or null?
-    return null;
+    return null;  // explicitly return null since async
 };
 
 
