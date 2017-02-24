546ef099cdd7fb2ac1f024fd73aa0f0d0a39a98d
Hamid Zr <dellydela@gmail.com>
2017-02-24 10:27:39 -0600

    test only when there is an api

--- test/server/rpc/procedures/traffic.spec.js
+++ test/server/rpc/procedures/traffic.spec.js
@@ -1,11 +1,13 @@
-describe('traffic', function() {
-    var Traffic = require('../../../../src/server/rpc/procedures/traffic/traffic'),
-        RPCMock = require('../../../assets/mock-rpc'),
-        utils = require('../../../assets/utils'),
-        traffic = new RPCMock(Traffic);
+if (process.env.BING_TRAFFIC_KEY) {
+	describe('traffic', function() {
+	    var Traffic = require('../../../../src/server/rpc/procedures/traffic/traffic'),
+	        RPCMock = require('../../../assets/mock-rpc'),
+	        utils = require('../../../assets/utils'),
+	        traffic = new RPCMock(Traffic);
 
-    utils.verifyRPCInterfaces(traffic, [
-        ['search', ['westLongitude', 'northLatitude', 'eastLongitude', 'southLatitude']],
-        ['stop']
-    ]);
-});
+	    utils.verifyRPCInterfaces(traffic, [
+	        ['search', ['westLongitude', 'northLatitude', 'eastLongitude', 'southLatitude']],
+	        ['stop']
+	    ]);
+	});
+}
