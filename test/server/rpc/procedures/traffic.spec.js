d887f7037228ca8cb5f5b85f4f2bca4e34f3d7c0
Brian Broll <brian.broll@vanderbilt.edu>
2017-03-06 10:55:36 -0600

    WIP #603 Added test environment

--- test/server/rpc/procedures/traffic.spec.js
+++ test/server/rpc/procedures/traffic.spec.js
@@ -1,13 +1,11 @@
-if (process.env.BING_TRAFFIC_KEY) {
-	describe('traffic', function() {
-	    var Traffic = require('../../../../src/server/rpc/procedures/traffic/traffic'),
-	        RPCMock = require('../../../assets/mock-rpc'),
-	        utils = require('../../../assets/utils'),
-	        traffic = new RPCMock(Traffic);
+describe.only('traffic', function() {
+    var Traffic = require('../../../../src/server/rpc/procedures/traffic/traffic'),
+        RPCMock = require('../../../assets/mock-rpc'),
+        utils = require('../../../assets/utils'),
+        traffic = new RPCMock(Traffic);
 
-	    utils.verifyRPCInterfaces(traffic, [
-	        ['search', ['westLongitude', 'northLatitude', 'eastLongitude', 'southLatitude']],
-	        ['stop']
-	    ]);
-	});
-}
+    utils.verifyRPCInterfaces(traffic, [
+        ['search', ['westLongitude', 'northLatitude', 'eastLongitude', 'southLatitude']],
+        ['stop']
+    ]);
+});
