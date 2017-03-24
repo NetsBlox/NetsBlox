0ac97a6a7efebd64e7c14fc6c41b69e66f87a58f
hamidzr <dellydela@gmail.com>
2017-03-24 18:07:34 -0500

    #796 added test and an example

---
+++ test/server/rpc/procedures/waterwatch.spec.js
@@ -0,0 +1,12 @@
+describe('waterwatch', function() {
+    var Waterwatch = require('../../../../src/server/rpc/procedures/waterwatch/waterwatch'),
+        RPCMock = require('../../../assets/mock-rpc'),
+        utils = require('../../../assets/utils'),
+        waterwatch = new RPCMock(Waterwatch);
+
+    utils.verifyRPCInterfaces(waterwatch, [
+        ['gageHeight', ['northernLat', 'easternLong', 'southernLat', 'westernLong']],
+        ['streamFlow', ['northernLat', 'easternLong', 'southernLat', 'westernLong']],
+        ['waterHeight', ['northernLat', 'easternLong', 'southernLat', 'westernLong']]
+    ]);
+});
