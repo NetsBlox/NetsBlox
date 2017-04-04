5431c112527824c23b4a56cc4291c8365d0a314f
Brian Broll <brian.broll@vanderbilt.edu>
2017-04-04 17:39:28 -0500

    WIP #844 trying to fix tests

--- test/server/rpc/procedures/staticmap.spec.js
+++ test/server/rpc/procedures/staticmap.spec.js
@@ -1,18 +1,31 @@
-describe('staticmap', function() {
-    var StaticMap = require('../../../../src/server/rpc/procedures/static-map/static-map'),
+describe.only('staticmap', function() {
+    var StaticMap,
         RPCMock = require('../../../assets/mock-rpc'),
         utils = require('../../../assets/utils'),
-        staticmap = new RPCMock(StaticMap);
+        storage = require('../../../../src/server/storage/storage'),
+        staticmap;
 
-    utils.verifyRPCInterfaces(staticmap, [
-        ['getMap', ['latitude', 'longitude', 'width', 'height', 'zoom']],
-        ['getLongitude', ['x']],
-        ['getLatitude', ['y']],
-        ['getXFromLongitude', ['longitude']],
-        ['getYFromLatitude', ['latitude']],
-        ['maxLongitude'],
-        ['maxLatitude'],
-        ['minLongitude'],
-        ['minLatitude']
-    ]);
+    before(function(done) {
+        storage.connect()
+            .then(() => {
+                console.log('connected!');
+                StaticMap = require('../../../../src/server/rpc/procedures/static-map/static-map');
+                staticmap = new RPCMock(StaticMap);
+                done();
+            });
+    });
+
+    describe('interfaces', function() {
+        utils.verifyRPCInterfaces(staticmap, [
+            ['getMap', ['latitude', 'longitude', 'width', 'height', 'zoom']],
+            ['getLongitude', ['x']],
+            ['getLatitude', ['y']],
+            ['getXFromLongitude', ['longitude']],
+            ['getYFromLatitude', ['latitude']],
+            ['maxLongitude'],
+            ['maxLatitude'],
+            ['minLongitude'],
+            ['minLatitude']
+        ]);
+    });
 });
