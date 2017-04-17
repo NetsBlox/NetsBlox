45af0a86a99e636415488ff9d5f3f147811dc7ae
Janos Sallai <sallai.janos@gmail.com>
2017-04-17 09:27:27 -0500

    Updated starmap to support search and zoom (#874)
    * added star map object search
    
    * formatting fixes
    
    * updated example, fixed a codeclimate issue
    
    * updated netsblox star map example
    
    * star map enhancements
    
    * star map app updates
    
    * Star map fix to avoid zooming out too much

--- src/server/rpc/procedures/star-map/star-map.js
+++ src/server/rpc/procedures/star-map/star-map.js
@@ -31,20 +31,70 @@ module.exports = {
         return 360.0 * (1.0/24.0) * ( (1.0/60.0) * ( ( (1.0/60.0) * parseFloat(arcSec)) + parseFloat(arcMin)) + parseFloat(arcHour));
     },
 
-    getImage: function(right_ascension, declination, arcseconds_per_pixel, options) {
-        var width = 360;
-        var height = 480;
+    findObject: function(name) {
+        var rsp = this.response;
+        var queryUrl = `http://skyserver.sdss.org/dr13/en/tools/Resolver.ashx?name=${name}`;
+
+        trace(`Querying object by name: ${name}`);
+
+        request.get(queryUrl, function (err, res) {
+            
+            rsp.set('cache-control', 'private, no-store, max-age=0');
+
+            var lines = res.body.split(/\r?\n/);
+
+            if(lines.length < 3) {
+                rsp.status(400).send('Not found.');
+            } else {
+                trace(lines);
+                var objName = lines[0].split(':')[1].trim();
+                var ra = lines[1].split(':')[1].trim();
+                var dec = lines[2].split(':')[1].trim();
+                ra = parseFloat(ra);
+                dec = parseFloat(dec);
+                trace(`Found ${objName} at ra=${ra} dec=${dec}`);
+                rsp.status(200).json([ra, dec]);
+            }
+        });
+
+        // explicitly state that we're async
+        return null;
+    },
+
+    getImage: function(right_ascension, declination, arcseconds_per_pixel, options, width, height) {
         var rsp = this.response;
         var url;
         var scale = parseFloat(arcseconds_per_pixel);
+
+        if(!width) {
+            width = 480.0;
+        } else {
+            width = parseFloat(width);
+        }
+
+        if(!height) {
+            height = 360.0;
+        } else {
+            height = parseFloat(height);
+        }
+
+        info(`width = ${width}`);
+        info(`height = ${height}`);
+
 
         if(isNaN(scale) || scale>32.0 || scale < 1.0/128.0) {
             rsp.status(400).send('Error: arcseconds_per_pixel is out of range. Valid values are between 1/128 and 32');        
         }
         else if(!right_ascension || !declination) {
             rsp.status(400).send('Error: right_ascension and declination not specified');        
+        }
+        else if(!width || isNaN(width) || width>4096 || height < 1) {
+            rsp.status(400).send('Error: width must be between 1-4096, got ' + width);        
+        }
+        else if(!height || isNaN(height) || height>4096 || height < 1) {
+            rsp.status(400).send('Error: height must be between 1-4096, got ' + width);        
         } else {
-            url = baseUrl+`/ImgCutout/getjpeg?ra=${right_ascension}&dec=${declination}&width=${width}&height=${height}&scale=${scale}&opt=${options}`;
+            url = baseUrl+`/ImgCutout/getjpeg?ra=${right_ascension}&dec=${declination}&width=${height}&height=${width}&scale=${scale}&opt=${options}`;
 
             info(`Getting image from URL ${url}`);
 
