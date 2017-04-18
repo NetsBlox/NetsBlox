a3e6414253f6f21d525d1052ae6e4dd520405aa1
Brian Broll <brian.broll@vanderbilt.edu>
2017-04-18 12:18:49 -0500

    Added url updating on open, share/unshare, examples. Fixes #883 (#884)
    * Added url updating on open, share/unshare, examples. Fixes #883
    
    * WIP #883 Removed old comment

--- src/client/netsblox.js
+++ src/client/netsblox.js
@@ -1929,12 +1929,20 @@ NetsBloxMorph.prototype.rawLoadCloudProject = function (project, isPublic) {
             this.showMessage(localize('A new role has been created for you at ' + newRoom));
         }
     }
-    if (isPublic === 'true') {
-        window.history.pushState(newRoom, newRoom, location.pathname + '?action=present&Username=' +
-            encodeURIComponent(SnapCloud.username) +
-            '&ProjectName=' +
-            encodeURIComponent(newRoom));
+    this.updateUrlQueryString(newRoom, isPublic === 'true');
+};
+
+NetsBloxMorph.prototype.updateUrlQueryString = function (room, isPublic, isExample) {
+    var url = location.pathname;
+
+    if (isExample) {
+        url += '?action=example&ProjectName=' + encodeURIComponent(room);
+    } else if (isPublic) {
+        url += '?action=present&Username=' + encodeURIComponent(SnapCloud.username) +
+            '&ProjectName=' + encodeURIComponent(room);
     }
+
+    window.history.pushState(room, room, url);
 };
 
 // Bug reporting assistance
