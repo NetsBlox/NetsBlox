b7808cd25755fb7b6edd4d5d93fa9093eb3a99c4
Brian Broll <brian.broll@vanderbilt.edu>
2017-05-08 15:50:30 -0500

    Added lastUpdateAt and createdAt. Fixes #941 (#945)
    Added lastUpdateAt to user projects. Fixes #941

--- src/server/storage/room-store.js
+++ src/server/storage/room-store.js
@@ -20,6 +20,7 @@ class Room extends DataWrapper {
         this._logger = params.logger.fork((this._room ? this._room.uuid : this.uuid));
         this._user = params.user;
         this._room = params.room;
+        this.lastUpdateAt = Date.now();
     }
 
     fork(room) {
@@ -28,6 +29,7 @@ class Room extends DataWrapper {
             user: this._user,
             room: room,
             logger: this._logger,
+            lastUpdateAt: Date.now(),
             db: this._db
         };
         this._logger.trace('forking (' + room.uuid + ')');
@@ -163,6 +165,7 @@ class Room extends DataWrapper {
                 PublicProjectStore.update(room);
             }
         }
+        room.lastUpdateAt = Date.now();
         this._user.changed(room);
         return this._user.save()
             .then(() => {
@@ -218,6 +221,7 @@ class RoomStore {
             logger: this._logger,
             db: this._rooms,
             user: user,
+            lastUpdateAt: Date.now(),
             room: activeRoom
         });
     }
