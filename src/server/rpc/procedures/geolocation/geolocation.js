7fcb779457d1a5a1f5b8c99e2a97ea650ba88887
Hamid Zare <dellydela@gmail.com>
2017-05-12 12:01:58 -0500

    move from axios to request-promise to be consistent with NB. fix indentation

--- src/server/rpc/procedures/geolocation/geolocation.js
+++ src/server/rpc/procedures/geolocation/geolocation.js
@@ -7,7 +7,7 @@ var debug = require('debug'),
     error = debug('netsblox:rpc:trends:error'),
     CacheManager = require('cache-manager'),
     NodeGeocoder = require('node-geocoder'),
-    axios = require('axios'),
+    rp = require('request-promise'),
     trace = debug('netsblox:rpc:trends:trace');
 
 // init
@@ -77,80 +77,81 @@ let reverseGeocode = (lat, lon, response, query)=>{
 
 // geocode an address and send back the details
 GeoLocationRPC.geolocate = function (address) {
-    let response = this.response;
+  let response = this.response;
 
-    trace('Geocoding', address);
-    geocoder.geocode(address)
-      .then(function(res) {
-        trace(res);
-        response.json([res[0].latitude, res[0].longitude]);
-      })
-      .catch(function(err) {
-        error('Error in geocoding', err);
-        showError('Failed to geocode',response);
-      });
+  trace('Geocoding', address);
+  geocoder.geocode(address)
+    .then(function(res) {
+      trace(res);
+      response.json([res[0].latitude, res[0].longitude]);
+    })
+    .catch(function(err) {
+      error('Error in geocoding', err);
+      showError('Failed to geocode',response);
+    });
 
-    return null;
+  return null;
 };
 
 // reverse geocode and send back a specific detail
 GeoLocationRPC.city = function (latitude, longitude) {
-    reverseGeocode(latitude, longitude, this.response, '.city')
-    return null;
+  reverseGeocode(latitude, longitude, this.response, '.city')
+  return null;
 };
 
 
 // reverse geocode and send back a specific detail
 GeoLocationRPC.country = function (latitude, longitude) {
-    reverseGeocode(latitude, longitude, this.response, '.country')
-    return null;
+  reverseGeocode(latitude, longitude, this.response, '.country')
+  return null;
 };
 
 // reverse geocode and send back a specific detail
 GeoLocationRPC.countryCode = function (latitude, longitude) {
-    reverseGeocode(latitude, longitude, this.response, '.countryCode')
-    return null;
+  reverseGeocode(latitude, longitude, this.response, '.countryCode')
+  return null;
 };
 
 // find places near a coordinate (20 reults max)
 GeoLocationRPC.nearbySearch = function (latitude, longitude, keyword, radius) {
-    let response = this.response;
-    radius = radius || 100; // default to 100
-    trace('Doing a nearby search', latitude, longitude);
+  let response = this.response;
+  radius = radius || 100; // default to 100
+  trace('Doing a nearby search', latitude, longitude);
 
-    let requestOptions = {
-      method: 'get',
-      url: 'https://maps.googleapis.com/maps/api/place/nearbysearch/json',
-      params: {
-        location: latitude + ',' + longitude,
-        radius: radius,
-        // rankby: 'distance',
-        key: GEOCODER_API
-      }
-    };
+  let requestOptions = {
+    method: 'get',
+    uri: 'https://maps.googleapis.com/maps/api/place/nearbysearch/json',
+    qs: {
+      location: latitude + ',' + longitude,
+      radius: radius,
+      // rankby: 'distance',
+      key: GEOCODER_API
+    },
+    json: true
+  };
 
-    if (keyword) {
-      requestOptions.params.keyword = keyword;
-    }
+  if (keyword) {
+    requestOptions.params.keyword = keyword;
+  }
 
-    axios(requestOptions).then(res=>{
-      let places = res.data.results;
-      places = places.map(place => {
-        return place.name + ' (' + place.types[0] + ')'
-      })
-      response.json(places);
-    }).catch(err => {
-      error("Error in searching for places",err);
-      showError('Failed to find places',response);
+  rp(requestOptions).then(res=>{
+    let places = res.data.results;
+    places = places.map(place => {
+      return place.name + ' (' + place.types[0] + ')'
     })
+    response.json(places);
+  }).catch(err => {
+    error("Error in searching for places",err);
+    showError('Failed to find places',response);
+  })
 
-    return null;
+  return null;
 };
 
 
 
 function showError(err, response) {
-    response.json(err);
+  response.json(err);
 }
 
 module.exports = GeoLocationRPC;
