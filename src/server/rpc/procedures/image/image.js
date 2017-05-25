8ce77be611def75848310f1d8c9ae2bac98e435e
Hamid Zare <dellydela@gmail.com>
2017-05-25 16:51:13 -0500

    init commit

---
+++ src/server/rpc/procedures/image/image.js
@@ -0,0 +1,11 @@
+const ApiConsumer = require('../utils/api-consumer');
+let image = new ApiConsumer('image','');
+
+image.display = function(url){
+    this._sendImage({
+        queryString: url
+    })
+    return null;
+}
+
+module.exports = image;
