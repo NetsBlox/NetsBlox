95143cf3e50172b92fa3cd7ff4ee330d3e68db36
Brian Broll <brian.broll@vanderbilt.edu>
2017-06-08 15:03:46 -0500

    v0.15.0

--- src/client/store.js
+++ src/client/store.js
@@ -8,7 +8,7 @@ NetsBloxSerializer.prototype.constructor = NetsBloxSerializer;
 NetsBloxSerializer.uber = SnapSerializer.prototype;
 SnapSerializer.prototype.thumbnailSize = new Point(640, 480);
 
-NetsBloxSerializer.prototype.app = 'NetsBlox 0.14.2, http://netsblox.org';  // Make this version automatic
+NetsBloxSerializer.prototype.app = 'NetsBlox 0.15.0, http://netsblox.org';  // Make this version automatic
 
 function NetsBloxSerializer() {
     this.init();
