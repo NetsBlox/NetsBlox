b64937e224417ee1fb658c1611aa5d423b753475
Dung Nguyen Do <thuydung.ptnk@gmail.com>
2017-06-15 10:43:59 -0500

    Creating service for ThingSpeak. Fix #1124

---
+++ test/server/rpc/procedures/thingspeak-iot.spec.js
@@ -0,0 +1,18 @@
+describe('thingspeak-iot', function() {
+    var ThingSpeak = require('../../../../src/server/rpc/procedures/thingspeak-iot/thingspeak-iot'),
+        RPCMock = require('../../../assets/mock-rpc'),
+        utils = require('../../../assets/utils'),
+        thingspeak = new RPCMock(ThingSpeak);
+    
+    before(function() {
+        thingspeak = new RPCMock(ThingSpeak);
+    });
+    
+    describe('interfaces', function() {
+        utils.verifyRPCInterfaces(thingspeak, [
+            ['publicChannel', ['tagString']],
+            ['channelFeed', ['id', 'numResult']]
+        ]);
+    });
+    
+});
