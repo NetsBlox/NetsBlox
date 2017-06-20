53c5b2e75b4743e530660d116d3d45f03ecb4bb2
Brian Broll <brian.broll@vanderbilt.edu>
2017-06-20 15:29:08 -0500

    Added input validation for weather service. Fixes #1145

--- test/server/rpc/procedures/weather.spec.js
+++ test/server/rpc/procedures/weather.spec.js
@@ -1,4 +1,5 @@
 describe('weather', function() {
+    const assert = require('assert');
     var Weather = require('../../../../src/server/rpc/procedures/weather/weather'),
         RPCMock = require('../../../assets/mock-rpc'),
         utils = require('../../../assets/utils'),
@@ -12,4 +13,23 @@ describe('weather', function() {
         ['windAngle', ['latitude', 'longitude']],
         ['icon', ['latitude', 'longitude']]
     ]);
+
+    describe('input validation', function() {
+        it('should send error message if non-numeric inputs', function() {
+            weather.temp('hello', 'world');
+            assert(weather.response.response.includes('ERROR'));
+        });
+
+        it('should send error message if out of range lat', function() {
+            weather.temp(-91, 0);
+            assert(weather.response.response.includes('ERROR'));
+            assert(weather.response.response.includes('latitude'));
+        });
+
+        it('should send error message if out of range lng', function() {
+            weather.temp(0, -181);
+            assert(weather.response.response.includes('ERROR'));
+            assert(weather.response.response.includes('longitude'));
+        });
+    });
 });
