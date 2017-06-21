652f063a7ea0584f168b3ada19625dac8957e0e5
Hamid Zare <dellydela@gmail.com>
2017-06-21 16:21:47 -0500

    factor out updating of corgis datasets

---
+++ bin/update-datasets.js
@@ -0,0 +1,52 @@
+const fs = require('fs'),
+    osmosis = require('osmosis'),
+    exec = require('child_process').exec,
+    Q = require('q');
+
+const STORAGE_DIR = process.env.CORGIS_DIR || 'datasets/';
+
+
+// in: array of dataset names
+// out: downloads all the datasets to the predefined location
+let updateDatasets = (names) => {
+    if (!fs.existsSync(STORAGE_DIR)){
+        fs.mkdirSync(STORAGE_DIR);
+    }
+    console.log(names);
+    names.forEach(dsName => {
+        console.log('downloading for', dsName);
+        let directUrl = `https://think.cs.vt.edu/corgis/json/${dsName}/${dsName}.json?forcedownload=1`;
+        let absolutePath = STORAGE_DIR + dsName + '.json';
+        console.log('updating dataset', dsName, directUrl);
+        exec(`curl ${directUrl} -o ${absolutePath}`, (error, stdout, stderr)=>{
+        });
+    });
+};
+
+
+// finds out what datasets are available
+let discoverDatasets = () => {
+    let deferred = Q.defer();
+    let names = [];
+    osmosis
+    .get('https://think.cs.vt.edu/corgis/json/index.html')
+    .find('.media-body a')
+    .set({name: '@href'})
+    .data(dataset => {
+        names.push(dataset.name.match(/(.*)\//)[1]);
+    })
+    .done(()=>{
+        deferred.resolve(names);
+    })
+    // .log(console.log)
+    // .debug(console.log)
+    .error(deferred.reject);
+    return deferred.promise;
+};
+
+discoverDatasets().then(names => {
+    updateDatasets(names);
+});
+
+module.exports = discoverDatasets;
+module.exports = updateDatasets;
--- src/server/rpc/procedures/corgis/corgis.js
+++ src/server/rpc/procedures/corgis/corgis.js
@@ -1,41 +1,10 @@
 const ApiConsumer = require('../utils/api-consumer'),
     jsonQuery = require('json-query'),
-    exec = require('child_process').exec,
-    fs = require('fs'),
-    osmosis = require('osmosis');
+    fs = require('fs');
 
 let corgis = new ApiConsumer('corgis','');
 
 const STORAGE_DIR = process.env.CORGIS_DIR || 'datasets/';
-
-// probably a shell script would be better
-let updateDatasets = (names) => {
-    if (!fs.existsSync(STORAGE_DIR)){
-        fs.mkdirSync(STORAGE_DIR);
-    }
-    return new Promise((resolve, reject) => {
-        if (names === undefined) names = [];
-        osmosis
-        .get('https://think.cs.vt.edu/corgis/json/index.html')
-        .find('.media-body a')
-        .set({name: '@href'})
-        .data(dataset => {
-            names.push(dataset.name.match(/(.*)\//)[1]);
-        })
-        .done(()=>{
-            names.forEach(dsName => {
-                let directUrl = `https://think.cs.vt.edu/corgis/json/${dsName}/${dsName}.json?forcedownload=1`;
-                let absolutePath = STORAGE_DIR + dsName + '.json';
-                corgis._logger.trace('updating dataset', dsName, directUrl);
-                exec(`curl ${directUrl} -o ${absolutePath}`, (error, stdout, stderr)=>{
-                });
-            });
-        })
-        .log(corgis._logger.trace)
-        // .debug(corgis._logger.trace)
-        .error(reject);
-    });
-};
 
 let loadDataset = dsName => {
     let absolutePath = STORAGE_DIR + dsName + '.json';
@@ -52,8 +21,6 @@ let loadDataset = dsName => {
     });
 };
 
-
-updateDatasets();
 corgis.searchDataset = function(name,query){
     return loadDataset(name)
         .then( data => {
