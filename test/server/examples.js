32d31221064e72a443c0ac08139ce57f0d7dc8dc
Hamid Zare <dellydela@gmail.com>
2017-06-22 17:03:24 -0500

    WIP adding  tests for room creation and loading of roles from

--- test/server/examples.js
+++ test/server/examples.js
@@ -1,32 +1,67 @@
 const assert = require('assert');
-// test if the services are being extracted/identified correctly. 
-describe('testing if services are being detected and populated correctly', function() {
-    let EXAMPLES = require('../../src/server/examples');
+// test if the services are being extracted/identified correctly.
+describe('examples', function() {
+    const ROOT_DIR = '../../',
+        NBSocket = require(ROOT_DIR + 'src/server/rooms/netsblox-socket'),
+        RoomManager = require(ROOT_DIR + 'src/server/rooms/room-manager'),
+        Logger = require(ROOT_DIR + 'src/server/logger'),
+        UserStorage = require(ROOT_DIR + 'src/server/storage/users'),
+        Storage = require(ROOT_DIR + 'src/server/storage/storage'),
+        EXAMPLES = require(ROOT_DIR + 'src/server/examples');
+    describe('testing if services are being detected and populated correctly', function() {
 
-    it('test Earthquakes examples', function() {
-        assert(EXAMPLES.Earthquakes.services.includes('earthquakes'));
-        assert(EXAMPLES.Earthquakes.services.includes('staticmap'));
+        it('test Earthquakes examples', function() {
+            assert(EXAMPLES.Earthquakes.services.includes('earthquakes'));
+            assert(EXAMPLES.Earthquakes.services.includes('staticmap'));
+        });
+
+        it('test Story examples', function(){
+            assert(EXAMPLES.Story.services.includes('NPlayer'));
+        });
+
+        it('test Weather examples', function(){
+            assert(EXAMPLES.Weather.services.includes('weather'));
+        });
+
+        it('test Movies examples', function(){
+            assert(EXAMPLES.Movies.services.includes('MovieDB'));
+        });
+
+        it('test GoogleTrends examples', function(){
+        	assert(EXAMPLES.GoogleTrends.services.includes('GoogleTrends'));
+        	assert(EXAMPLES.GoogleTrends.services.includes('staticmap'));
+        });
+
+        it('test Quizzer examples', function(){
+        	assert(EXAMPLES.Quizzer.services.includes('kv-store'));
+        });
+
     });
 
-    it('test Story examples', function(){
-        assert(EXAMPLES.Story.services.includes('NPlayer'));
-    });
-
-    it('test Weather examples', function(){
-        assert(EXAMPLES.Weather.services.includes('weather'));
-    });
-
-    it('test Movies examples', function(){
-        assert(EXAMPLES.Movies.services.includes('MovieDB'));
-    });
-
-    it('test GoogleTrends examples', function(){
-    	assert(EXAMPLES.GoogleTrends.services.includes('GoogleTrends'));
-    	assert(EXAMPLES.GoogleTrends.services.includes('staticmap'));
-    });
-
-    it('test Quizzer examples', function(){
-    	assert(EXAMPLES.Quizzer.services.includes('kv-store'));
+    describe('room creation', function() {
+        const logger = new Logger('example');
+        let room;
+        before(function(done){
+            let storage = new Storage(logger);
+            storage.connect('mongodb://localhost:27017/netsbloxTest').then(()=> {
+                RoomManager.init(logger, storage);
+                let socket = new NBSocket(logger, {on: () => {}});
+                room = RoomManager.createRoom(socket, 'roomName', 'ownerId');
+                EXAMPLES.Battleship.toRoom(room).then(()=>done());
+            })
+        });
+        it('should return role from room', function(){
+            // console.log('role names:', room.getRoleNames().join());
+            assert.equal(room.getRoleNames().length, 2);
+        });
+        it.only('should get correct role data', function(done){
+            // UserStorage.get('hamid').then(console.log);
+            console.log(UserStorage.get);
+            room.getRole('player 1').then(role => {
+                console.log(role);
+                done();
+            })
+        })
     });
 
 });
