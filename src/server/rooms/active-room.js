e2a56beeb87e2bd1004018131abe2ad7b04acbdf
Hamid Zare <dellydela@gmail.com>
2017-06-22 16:56:45 -0500

    set room save should return promise

--- src/server/rooms/active-room.js
+++ src/server/rooms/active-room.js
@@ -163,8 +163,9 @@ class ActiveRoom {
 
     save() {
         if (this._project) {  // has been saved
-            this._project.save();
+            return this._project.save();
         }
+        return Q();
     }
 
     move (params) {
@@ -220,7 +221,7 @@ class ActiveRoom {
         }
         this.sockets().forEach(socket => socket.send(msg));
     }
- 
+
     sockets () {
         return R.values(this.roles)
             .filter(socket => !!socket);
