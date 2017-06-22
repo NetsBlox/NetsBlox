c0d12918eeb69fd9183905e0f232cc3f498cac09
Hamid Zare <dellydela@gmail.com>
2017-06-22 17:15:40 -0500

    lowered default searchresults limit, enabled image caching, disabled advance search

--- src/server/rpc/procedures/british-museum/british-museum.js
+++ src/server/rpc/procedures/british-museum/british-museum.js
@@ -11,6 +11,7 @@ PREFIX fts: <http://www.ontotext.com/owlim/fts#>
 PREFIX btm: <http://collection.britishmuseum.org/id/ontology/>
 PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>`;
 
+const DEFAULT_LIMIT = 5;
 
 // parser used for when calling the json endPoint
 let searchParser = resp => {
@@ -71,9 +72,9 @@ britishmuseum._htmlSearchHandler = function(html, response) {
 
 
 
-britishmuseum.search = function(label, type, material, limit) {
+britishmuseum._search = function(label, type, material, limit) {
     if (!(label || type || material)) return 'Please pass in a query';
-    limit = limit || 10;
+    limit = limit || DEFAULT_LIMIT;
     let response = this.response;
     let baseQ = [
         `SELECT DISTINCT (SAMPLE(?object) AS ?object) ?img
@@ -120,7 +121,7 @@ britishmuseum.search = function(label, type, material, limit) {
 };
 
 britishmuseum.searchByLabel = function(label, limit){
-    limit = limit || 10;
+    limit = limit || DEFAULT_LIMIT;
     let response = this.response;
 
     let simpleLabelQ = `
@@ -145,7 +146,7 @@ britishmuseum.searchByLabel = function(label, limit){
 
 
 britishmuseum.searchByType = function(type, limit){
-    limit = limit || 10;
+    limit = limit || DEFAULT_LIMIT;
     let response = this.response;
 
     let simpleTypeQ = `
@@ -174,7 +175,7 @@ britishmuseum.searchByType = function(type, limit){
 
 
 britishmuseum.searchByMaterial = function(material, limit){
-    limit = limit || 10;
+    limit = limit || DEFAULT_LIMIT;
     let response = this.response;
 
     let simpleMaterialQ = `
@@ -250,7 +251,7 @@ britishmuseum.getImage = function getImage(imageId, maxWidth, maxHeight) {
     this._sendImage({
         baseUrl: 'http://www.britishmuseum.org/collectionimages/',
         queryString: `AN${imageId.substr(0,5)}/AN${imageId}_001_l.jpg?maxwidth=${maxWidth}&maxheight=${maxHeight}`,
-        cache: false
+        cache: true
     });
 
     return null;
