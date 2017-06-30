06bbe74c00688dee573fd99b946323c63fb292cb
Dung Nguyen Do <thuydung.ptnk@gmail.com>
2017-06-30 13:13:42 -0500

    WIP #1131: Add dataset test and test for itself

---
+++ src/server/rpc/procedures/utils/dataset-test.js
@@ -0,0 +1,65 @@
+let test = {};
+
+test.getField = (input, fieldName) => {
+    let stringResult = input.reduce((acc, cur) => {
+        return acc.concat(cur.find((valuePair) => {
+            return valuePair[0] === fieldName;
+        })[1]);
+    }, []);
+    
+    let sample = stringResult[0];
+    if (parseInt(sample) && parseInt(sample).toString().length === sample.length) {
+        return stringResult.map(parseInt);
+    }
+    return stringResult;
+};
+
+test.testValidArray = (rawArray) => {
+    if (typeof rawArray === 'string' || typeof rawArray === 'number') {
+        return true;
+    }
+    if (Array.isArray(rawArray)) {
+        if (rawArray.length === 0) {
+            return false;
+        }
+        return rawArray.filter(test.testValidArray).length === rawArray.length;
+    }
+    return false;
+};
+
+test.testValidDataset = (rawDataset, xAxis, yAxis) => {
+    if (!(test.testValidArray(rawDataset))) {
+        return 'The dataset is blank or has blank or invalid value';
+    }
+    for (let i = 0; i < rawDataset.length; i++) {
+        let dataPoint = rawDataset[i];
+        if (dataPoint.length !== 2) {
+            return 'The dataset should only have two types of value';
+        }
+        if (dataPoint[0].length !== 2 || dataPoint[1].length !== 2) {
+            return 'At least one point doesn\'t have both name and value';
+        }
+        if (dataPoint[0][0] !== xAxis || dataPoint[1][0] !== yAxis) {
+            return 'At least one point has name different from xAxis or yAxis';
+        }
+    }
+    
+    let yValue = test.getField(rawDataset, yAxis);
+    if (yValue.find((value) => typeof value !== 'number') !== undefined) {
+        return 'yAxis values are not valid number';
+    }
+    return '';
+};
+
+test.testMultipleDatasets = (rawDataset, xAxis, yAxis) => {
+    let returnedMsg;
+    for (let i = 0; i < rawDataset.length; i++) {
+        returnedMsg = test.testValidDataset(rawDataset[i], xAxis, yAxis);
+        if (returnedMsg !== '') {
+            return returnedMsg;
+        }
+    }
+    return '';
+};
+
+module.exports = test;
---
+++ test/server/rpc/procedures/chart.spec.js
@@ -0,0 +1,4 @@
+
+describe('chart', function() {
+
+});
