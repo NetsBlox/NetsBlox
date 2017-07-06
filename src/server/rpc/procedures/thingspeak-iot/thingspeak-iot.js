811755ac9fe119192c33cf205bac7986d821e108
Dung Nguyen Do <thuydung.ptnk@gmail.com>
2017-07-06 12:46:10 -0500

    Remove duplicated parser

--- src/server/rpc/procedures/thingspeak-iot/thingspeak-iot.js
+++ src/server/rpc/procedures/thingspeak-iot/thingspeak-iot.js
@@ -9,48 +9,18 @@ function encodeQueryData(options) {
     return ret.join('&');
 }
 
-function getFieldName(data) {
-    let result = {};
-    for (var prop in data) {
-        if (data.hasOwnProperty(prop) && prop.match(/field\d/)) {
+let feedParser = data => {
+    let fieldMap = {};
+    let channel = data.channel;
+    for (var prop in channel) {
+        if (channel.hasOwnProperty(prop) && prop.match(/field\d/)) {
             var matchGroup = prop.match(/field\d/)[0];
-            result[matchGroup] = data[matchGroup];
+            fieldMap[matchGroup] = channel[matchGroup];
         }
     }
-    return result;
-}
-
-thingspeakIoT.searchPublicChannel = function(tagString) {
-    let queryOptions = {
-        queryString: tagString !== '' ? 'public.json?' +
-            encodeQueryData({
-                tag: encodeURIComponent(tagString),
-            }) : 'public.json',
-    };
-    let parser = data => {
-        return data.channels.map(item => {
-            return {
-                id: item.id,
-                name: item.name,
-                description: item.description,
-                latitude: item.latitude,
-                longitude: item.longitude,
-                tags: (function(data) {
-                    return data.map(tag => {
-                        return tag.name;
-                    });
-                })(item.tags),
-            };
-        });
-    };
-    return this._sendStruct(queryOptions, parser);
-};
-
-let feedParser = data => {
-    let fieldMap = getFieldName(data.channel);
     return data.feeds.map(tag => {
         let resultObj = {
-            created_at: tag.created_at,
+            Time: tag.created_at.match(/\d{2}:\d{2}/)[0],
         };
         for (let field in fieldMap) {
             if (fieldMap.hasOwnProperty(field)) {
@@ -61,13 +31,41 @@ let feedParser = data => {
     });
 };
 
+let detailParser = item => {
+    return {
+        id: item.id,
+        name: item.name,
+        description: item.description,
+        latitude: item.latitude,
+        longitude: item.longitude,
+        tags: (function(data) {
+            return data.map(tag => {
+                return tag.name;
+            });
+        })(item.tags),
+    };
+};
+
+thingspeakIoT.searchPublicChannel = function(tagString) {
+    let queryOptions = {
+        queryString: tagString !== '' ? 'public.json?' +
+            encodeQueryData({
+                tag: encodeURIComponent(tagString),
+            }) : 'public.json',
+    };
+    let parser = data => {
+        return data.channels.map(detailParser);
+    };
+    return this._sendStruct(queryOptions, parser);
+};
+
 thingspeakIoT.channelFeed = function(id, numResult) {
     let queryOptions = {
         queryString: id + '/feeds.json?' + encodeQueryData({
             results: numResult,
         }),
     };
-    return feedParser
+    return this._sendStruct(queryOptions, feedParser)
 };
 
 thingspeakIoT.privateChannelFeed = function(id, numResult, apiKey) {
@@ -88,16 +86,7 @@ thingspeakIoT.channelDetail = function(id) {
     let queryOptions = {
         queryString: id + '.json?'
     };
-    let parser = item => {
-        return {
-            id: item.id,
-            name: item.name,
-            description: item.description,
-            latitude: item.latitude,
-            longitude: item.longitude
-        };
-    };
-    return this._sendStruct(queryOptions, parser);
+    return this._sendStruct(queryOptions, detailParser);
 };
 
 module.exports = thingspeakIoT;
