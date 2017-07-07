a27ee9877ff42b1f504a8b07453d66a34762bfd4
Dung Nguyen Do <thuydung.ptnk@gmail.com>
2017-07-07 16:28:47 -0500

    Fix small problem and don't export if having no key

--- src/server/rpc/procedures/search-image/search-image.js
+++ src/server/rpc/procedures/search-image/search-image.js
@@ -1,56 +1,57 @@
-const ApiConsumer = require('../utils/api-consumer');
-const imageSearch = new ApiConsumer('imageSearch', 'https://pixabay.com/api/?');
-const KEY = process.env.PIXABAY;
-
-function parserFnGen(maxHeight) {
-    let optimalSize;
-    if (maxHeight == '' || maxHeight > 640) {
-        optimalSize = ['_640', '_640'];
-    } else if (maxHeight < 360) {
-        optimalSize = ['_640', '_180'];
-    } else {
-        optimalSize = ['_640', '_340'];
+if(!process.env.PIXABAY) {
+    console.log(
+        'Warning: environment variable PIXABAY not defined, search-image RPC will not work.');
+} else {
+    const ApiConsumer = require('../utils/api-consumer');
+    const imageSearch = new ApiConsumer('imageSearch', 'https://pixabay.com/api/?');
+    const KEY = process.env.PIXABAY;
+    const rpcUtils = require('../utils');
+    
+    function parserFnGen(maxHeight) {
+        let optimalSize;
+        if (maxHeight == '' || maxHeight > 640) {
+            optimalSize = ['_640', '_640'];
+        } else if (maxHeight < 360) {
+            optimalSize = ['_640', '_180'];
+        } else {
+            optimalSize = ['_640', '_340'];
+        }
+        return data => {
+            return data.hits.map(item => {
+                return {
+                    image_url: item.webformatURL.replace(optimalSize[0], optimalSize[1]),
+                    tags: item.tags.split(', '),
+                    type: item.type
+                };
+            });
+        };
     }
-    return data => {
-        return data.hits.map(item => {
-            return {
-                image_url: item.webformatURL.replace(optimalSize[0], optimalSize[1]),
-                tags: item.tags.split(', '),
-                type: item.type
-            };
-        });
-    };
-}
-
-function encodeQueryData(options) {
-    let ret = [];
-    for (let d in options) ret.push(encodeURIComponent(d) + '=' + encodeURIComponent(options[d]));
-    return ret.join('&');
-}
-
-function encodeQueryOptions(keywords, type, minHeight, self) {
-    if (self === 'searchAll') {
-        type = 'all';
-    } else if (self === 'searchPhoto') {
-        type = 'photo';
-    } else if (self === 'searchIllustration') {
-        type = 'illustration';
+    
+    function encodeQueryOptions(keywords, type, minHeight, self) {
+        if (self === 'searchAll') {
+            type = 'all';
+        } else if (self === 'searchPhoto') {
+            type = 'photo';
+        } else if (self === 'searchIllustration') {
+            type = 'illustration';
+        }
+        return {
+            queryString: rpcUtils.encodeQueryData({
+                key: KEY,
+                q: encodeURIComponent(keywords),
+                image_type: type,
+                safesearch: true,
+                min_height: (minHeight || 0)
+            })
+        };
     }
-    return {
-        queryString: encodeQueryData({
-            key: KEY,
-            q: encodeURIComponent(keywords),
-            image_type: type,
-            safesearch: true,
-            min_height: (minHeight || 0)
-        })
-    };
+    
+    
+    ['searchAll', 'searchPhoto', 'searchIllustration'].forEach(function (item) {
+        imageSearch[item] = function (keywords, maxHeight, minHeight) {
+            return this._sendStruct(encodeQueryOptions(keywords, null, minHeight, item), parserFnGen(maxHeight));
+        };
+    });
+    
+    module.exports = imageSearch;
 }
-
-['searchAll', 'searchPhoto', 'searchIllustration'].forEach(function (item) {
-    imageSearch[item] = (keywords, maxHeight, minHeight) => {
-        return imageSearch._sendStruct(encodeQueryOptions(keywords, null, minHeight, item), parserFnGen(maxHeight));
-    };
-});
-
-module.exports = imageSearch;
