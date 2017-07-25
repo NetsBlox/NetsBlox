2a758c5afaafc9e4c25014100e415f0f719f86f4
Hamid Zare <dellydela@gmail.com>
2017-07-25 17:04:06 -0500

    reverse Y for getYfromLatitude

--- src/server/rpc/procedures/static-map/static-map.js
+++ src/server/rpc/procedures/static-map/static-map.js
@@ -52,7 +52,7 @@ StaticMap.prototype._pixelsAt = function(lat, lon, map) {
     // new latlon in px
     let targetPx = merc.px([lon, lat], map.zoom);
     // difference in px
-    let pixelsXY = {x: (targetPx[0] - curPx[0]), y: (targetPx[1] - curPx[1])};
+    let pixelsXY = {x: (targetPx[0] - curPx[0]), y: -(targetPx[1] - curPx[1])};
     return pixelsXY;
 };
 
