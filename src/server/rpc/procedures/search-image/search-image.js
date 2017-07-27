d0f2a65f361eb7b2282b29c79718efc34c67b1a9
Hamid Zare <dellydela@gmail.com>
2017-07-27 16:33:31 -0500

    add get Image functionality to the service. Also moved the check for existance of the apiKey to a only guard exporting

--- src/server/rpc/procedures/search-image/search-image.js
+++ src/server/rpc/procedures/search-image/search-image.js
@@ -1,57 +1,60 @@
+const ApiConsumer = require('../utils/api-consumer');
+const imageSearch = new ApiConsumer('imageSearch', 'https://pixabay.com/api/?');
+const KEY = process.env.PIXABAY;
+const rpcUtils = require('../utils');
+
+function parserFnGen(maxHeight) {
+    let optimalSize;
+    if (maxHeight == '' || maxHeight > 640) {
+        optimalSize = ['_640', '_640'];
+    } else if (maxHeight < 360) {
+        optimalSize = ['_640', '_180'];
+    } else {
+        optimalSize = ['_640', '_340'];
+    }
+    return data => {
+        return data.hits.map(item => {
+            return {
+                image_url: item.webformatURL.replace(optimalSize[0], optimalSize[1]),
+                tags: item.tags.split(', '),
+                type: item.type
+            };
+        });
+    };
+}
+
+function encodeQueryOptions(keywords, type, minHeight, self) {
+    if (self === 'searchAll') {
+        type = 'all';
+    } else if (self === 'searchPhoto') {
+        type = 'photo';
+    } else if (self === 'searchIllustration') {
+        type = 'illustration';
+    }
+    return {
+        queryString: rpcUtils.encodeQueryData({
+            key: KEY,
+            q: encodeURIComponent(keywords),
+            image_type: type,
+            safesearch: true,
+            min_height: (minHeight || 0)
+        })
+    };
+}
+
+
+['searchAll', 'searchPhoto', 'searchIllustration'].forEach(function (item) {
+    imageSearch[item] = function (keywords, maxHeight, minHeight) {
+        return this._sendStruct(encodeQueryOptions(keywords, null, minHeight, item), parserFnGen(maxHeight));
+    };
+});
+
+imageSearch.getImage = function(fullPath){
+    return this._sendImage({queryString: '', baseUrl:fullPath});
+};
+
 if(!process.env.PIXABAY) {
-    console.log(
-        'Warning: environment variable PIXABAY not defined, search-image RPC will not work.');
+    console.log('Warning: environment variable PIXABAY not defined, search-image RPC will not work.');
 } else {
-    const ApiConsumer = require('../utils/api-consumer');
-    const imageSearch = new ApiConsumer('imageSearch', 'https://pixabay.com/api/?');
-    const KEY = process.env.PIXABAY;
-    const rpcUtils = require('../utils');
-    
-    function parserFnGen(maxHeight) {
-        let optimalSize;
-        if (maxHeight == '' || maxHeight > 640) {
-            optimalSize = ['_640', '_640'];
-        } else if (maxHeight < 360) {
-            optimalSize = ['_640', '_180'];
-        } else {
-            optimalSize = ['_640', '_340'];
-        }
-        return data => {
-            return data.hits.map(item => {
-                return {
-                    image_url: item.webformatURL.replace(optimalSize[0], optimalSize[1]),
-                    tags: item.tags.split(', '),
-                    type: item.type
-                };
-            });
-        };
-    }
-    
-    function encodeQueryOptions(keywords, type, minHeight, self) {
-        if (self === 'searchAll') {
-            type = 'all';
-        } else if (self === 'searchPhoto') {
-            type = 'photo';
-        } else if (self === 'searchIllustration') {
-            type = 'illustration';
-        }
-        return {
-            queryString: rpcUtils.encodeQueryData({
-                key: KEY,
-                q: encodeURIComponent(keywords),
-                image_type: type,
-                safesearch: true,
-                min_height: (minHeight || 0)
-            })
-        };
-    }
-    
-    
-    ['searchAll', 'searchPhoto', 'searchIllustration'].forEach(function (item) {
-        imageSearch[item] = function (keywords, maxHeight, minHeight) {
-            return this._sendStruct(encodeQueryOptions(keywords, null, minHeight, item), parserFnGen(maxHeight));
-        };
-    });
-    
     module.exports = imageSearch;
 }
