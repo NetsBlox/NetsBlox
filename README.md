9208cb0d37a8cd391df08e4f0e5b8fd5dc86bf82
Brian Broll <brian.broll@gmail.com>
2017-08-03 13:14:41 -0500

    WIP #1114 renamed the last pixabay things

--- README.md
+++ README.md
@@ -64,7 +64,7 @@ RPCs that are using 3rd party API's often require getting an API key from the gi
   - `BING_TRAFFIC_KEY` should be set to an API key from [Bing Traffic](https://msdn.microsoft.com/en-us/library/hh441725.aspx)
 - Twitter
   - `TWITTER_BEARER_TOKEN` should be set to an API key from Twitter
-- Image Search
+- [Pixabay](https://pixabay.com)
   - `PIXABAY` should be set to an API key from Pixabay
 
 To simplify this process (and to keep your `~/.bashrc` clean), these values can be stored in a `.env` file in the project root directory and they will be loaded into the environment on starting NetsBlox.
--- test/server/rpc/procedures/pixabay.js
+++ test/server/rpc/procedures/pixabay.js
@@ -1,12 +1,12 @@
-describe('imageSearch', function() {
-    var ImageSearch = require('../../../../src/server/rpc/procedures/pixabay/pixabay'),
+describe('Pixabay', function() {
+    var Pixabay = require('../../../../src/server/rpc/procedures/pixabay/pixabay'),
         RPCMock = require('../../../assets/mock-rpc'),
         utils = require('../../../assets/utils'),
-        imageSearch = new RPCMock(ImageSearch);
+        pixabay = new RPCMock(Pixabay);
 
-    utils.verifyRPCInterfaces(imageSearch, [
-    ['searchAll', ['keywords', 'maxHeight', 'minHeight']],
+    utils.verifyRPCInterfaces(pixabay, [
+        ['searchAll', ['keywords', 'maxHeight', 'minHeight']],
         ['searchPhoto', ['keywords', 'maxHeight', 'minHeight']],
-        ['searchIllustration', ['keywords', 'maxHeight', 'minHeight']],
+        ['searchIllustration', ['keywords', 'maxHeight', 'minHeight']]
     ]);
 });
