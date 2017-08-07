3981c2a8f718352ea3116f4988bbcf218e6b5a55
Hamid Zare <dellydela@gmail.com>
2017-08-07 16:44:36 -0500

    added a new algorithm to select stations. added new visualization for stations using googlemap WIP

--- src/server/rpc/procedures/eclipse-2017/eclipse-2017.js
+++ src/server/rpc/procedures/eclipse-2017/eclipse-2017.js
@@ -10,6 +10,10 @@ const STATIONS_COL = 'wuStations',
     READINGS_COL = 'wuReadings';
 let connection;
 
+let eclipsePath = function(){
+    return eclipsePathCenter();
+};
+
 // connect to nb database
 let dbConnect = () => {
     const mongoUri = process.env.MONGO_URI || process.env.MONGOLAB_URI || 'mongodb://localhost:27017';
@@ -19,20 +23,27 @@ let dbConnect = () => {
     return connection;
 };
 
+function nearbyStations(db, lat, lon,  maxDistance){
+    lat = parseFloat(lat);
+    lon = parseFloat(lon);
+    // find stations uptodate stations within MAX_DISTANCE
+    let closeStations = { coordinates: { $nearSphere: { $geometry: { type: "Point", coordinates: [lon, lat] }, $maxDistance: maxDistance } } };
+    closeStations.readingMedian = {$ne: null, $lte: 1800}; // lte: just to avoid getting slow stations
+    return db.collection(STATIONS_COL).find(closeStations).sort({readingMedian: 1}).toArray().then(stations => {
+        // sorted array of stations by closest first
+        console.log(`found ${stations.length} stations within ${maxDistance/1000} of ${lat}, ${lon}`);
+        return stations
+    });
+} // end of nearbyStations
+
 // OPTIMIZE can be cached based on approximate coords n time
 function closestReading(lat, lon, time){
     const MAX_DISTANCE = 25000, // in meters
         MAX_AGE = 60 * 5; // in seconds
     time = time ? new Date(time) : new Date();// should be in iso format or epoch if there is no time past it means we want the temp for now! 
-    lat = parseFloat(lat);
-    lon = parseFloat(lon);
 
     return dbConnect().then(db => {
-        // find stations uptodate stations within MAX_DISTANCE
-        let closeStations = { coordinates: { $nearSphere: { $geometry: { type: "Point", coordinates: [lon, lat] }, $maxDistance: MAX_DISTANCE } } };
-        closeStations.readingMedian = {$ne: null, $lte: MAX_AGE*10}; // lte: just to avoid getting slow stations
-        return db.collection(STATIONS_COL).find(closeStations).toArray().then(stations => {
-            // sorted array of stations by closest first
+        nearbyStations(db, lat, lon, MAX_DISTANCE).then(stations => {
             let stationIds = stations.map(station => station.pws);
             // ask mongo for updates with the timelimit and specific stations.
             // QUESTION could lookup for a single stations instead here.. will lead to more calls to the database and more promises
@@ -44,6 +55,7 @@ function closestReading(lat, lon, time){
             console.log('readings query',updatesQuery);
             return db.collection(READINGS_COL).find(updatesQuery).sort({readAt: -1, distance: 1}).toArray().then(readings => {
                 // QUESTION pick the closest or latest?!
+                // sth like pickBestStations but on readings
                 console.log('replying with ',readings);
                 return readings[0];
             });
@@ -81,7 +93,7 @@ let availableStations = function(maxReadingMedian, maxDistanceFromCenter, latitu
     });
 };
 
-let selectedStations = function(numSections, perSection){
+let selectSectionBased = function(numSections, perSection){
     numSections = parseInt(numSections);
     perSection = parseInt(perSection);
     console.log(sectionStations)
@@ -91,14 +103,40 @@ let selectedStations = function(numSections, perSection){
             process.stdout.write(section.length+'');
         })
         let stations = sections.reduce((arr,val)=> arr.concat(val));
-        return rpcUtils.jsonToSnapList(stations);
+        return stations;
     });
 }
 
+let selectPointBased = function(){
+    return dbConnect().then(db => {
+        let pointToStation = point => {
+            return nearbyStations(db, point[0], point[1], 25000 )
+                .then(stations => {
+                    return pickBestStations(stations,1)[0];
+                }).catch(console.log);
+        };
+        // TODO generate more points
+        let stationPromises = eclipsePathCenter().map(pointToStation);
+        console.log(stationPromises)
+        return Promise.all(stationPromises).then(stations => {
+            console.log(stations)
+            return stations;
+        });
+    })
+}
 
-let eclipsePath = function(){
-    return eclipsePathCenter();
-};
+let selectedStations = function(numSections, perSection){
+    return selectSectionBased(numSections, perSection).then(stations => {
+        return rpcUtils.jsonToSnapList(stations);
+    })
+}
+
+
+let selectedStations2 = function(){
+    return selectPointBased().then(stations => {
+        return rpcUtils.jsonToSnapList(stations);
+    })
+}
 
 // TODO add arg validation like openWeather
 
@@ -108,5 +146,8 @@ module.exports = {
     currentCondition,
     eclipsePath,
     availableStations,
-    selectedStations
+    selectedStations,
+    selectedStations2,
+    selectSectionBased,
+    selectPointBased
 };
