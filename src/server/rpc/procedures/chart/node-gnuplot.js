f858a3ee7f67e2f5c345c5809101668e1530cf1a
Hamid Zare <dellydela@gmail.com>
2017-08-22 15:34:51 -0500

    added a js friendly wrapper aroung gnuplot module

---
+++ src/server/rpc/procedures/chart/node-gnuplot.js
@@ -0,0 +1,101 @@
+const gnuplot = require(`gnuplot`),
+    Q = require('q'),
+    _ = require('lodash');
+
+const lineDefaults = {
+    title: undefined,
+    type: 'line',
+    points: []
+};
+
+// opts defaults
+const optsDefautls = {
+    xRange: undefined,
+    yRange: undefined,
+    title: null,
+    outputName: false
+};
+
+function pointsToInlineData(points){
+    let inlineStr = points.map(point => point.join(',')).join('\n')
+    return ' \n' + inlineStr + '\ne';
+}
+
+// if sharing a column, using can be used to select which columns relate (x & y)
+// returns a plot command string
+function dataToPlot(data){ 
+    let settings = [],
+        points = '';
+    data.forEach(line => {
+        settings.push(`"-" title "${line.title}" with ${line.type}`);
+        points += pointsToInlineData(line.points);
+    })
+    let cmdString = settings.join(', ') + points;
+    // console.log('cmd', cmdString)
+    return cmdString;
+}
+// graph.plot(`"-" title "bla" with line, "-" title "secondLine" with line \n1,2\n2,3\ne\n3,1\n1,3\ne`)
+
+let draw = (data, opts) => {
+    // apply the defaults
+    graph = gnuplot();
+    data = data.map(line => _.merge(line, lineDefaults));
+    _.merge(optsDefautls, opts);
+    graph.set(`term pngcairo`)
+        .set(`loadpath "${__dirname}"`)
+        .set(`load "xyborder.cfg"`)
+        .set(`load "grid.cfg"`)
+        .set(`title "${opts.title}"`)
+        // .set(`zeroaxis`)
+
+        // add grid
+        .set(`style line 102 lc rgb "#d6d7d9" lt 0 lw 1`)
+        .set(`grid back ls 102`)
+        // add colors
+        .set(`style line 1 lt 1 lc rgb "#1B9E77"`)  // dark teal
+        .set(`style line 2 lt 1 lc rgb "#D95F02"`)  // dark orange
+        .set(`style line 3 lt 1 lc rgb "#7570B3"`)  // dark lilac
+        .set(`style line 4 lt 1 lc rgb "#E7298A"`)  // dark magenta
+        .set(`style line 5 lt 1 lc rgb "#66A61E"`)  // dark lime green
+        .set(`style line 6 lt 1 lc rgb "#E6AB02"`)  // dark banana
+        .set(`style line 7 lt 1 lc rgb "#A6761D"`)  // dark tan
+        .set(`style line 8 lt 1 lc rgb "#666666"`)  // dark gray
+
+        // palette
+        .set(`palette maxcolors 8`)
+        .set(`palette defined ( 0 "#1B9E77", 1 "#D95F02", 2 "#7570B3", 3 "#E7298A", 4 "#66A61E", 5 "#E6AB02", 6 "#A6761D",7 "#666666" )`)
+
+        .set(`datafile separator ","`);
+
+    // if (data.length > 1) graph.set(`multiplot layout 1,2`) // this option is for drawing multiple charts
+    
+    if (opts.xRange) graph.set(`xrange [${opts.xRange.min}:${opts.xRange.max}]`)
+    if (opts.yRange) graph.set(`yrange [${opts.yRange.min}:${opts.yRange.max}]`)
+    if (opts.outputName) graph.set(`output "${opts.outputName}"`);
+
+    graph.plot(dataToPlot(data));
+
+    return graph;
+};
+
+// draw a test
+let data = [
+    {
+        type: 'line',
+        points: [[1,2],[2,3],[4,2]]
+    },
+    {
+        title: 'temp',
+        points: [[5,2],[2,3],[3,1]]
+    },
+    {
+        title: 'temp',
+        points: [[1,2],[2,1]]
+    }
+];
+
+let opts = {};
+
+module.exports = {
+    draw,
+}
