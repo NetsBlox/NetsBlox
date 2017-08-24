308eb4ac4dbbe9c50ba054bff599919ea6dbdf20
Brian Broll <brian.broll@vanderbilt.edu>
2017-08-24 11:56:37 -0500

    Added add-user command to cli. Fixes #1428

---
+++ bin/netsblox-add-user.js
@@ -0,0 +1,42 @@
+#!/usr/bin/env node
+/* eslint-disable no-console*/
+
+require('epipebomb')();  // Allow piping to 'head'
+
+const Storage = require('../src/server/storage/storage'),
+    Logger = require('../src/server/logger'),
+    Groups = require('../src/server/storage/groups'),
+    Users = require('../src/server/storage/users'),
+    logger = new Logger('netsblox:cli:projects'),
+    storage = new Storage(logger),
+    Command = require('commander').Command,
+    program = new Command();
+
+program.arguments('<group>');
+program.parse(process.argv);
+
+if (program.args.length !== 3) {
+    console.log('usage: netsblox add-user <name> <email> <password>');
+    process.exit(1);
+}
+
+// List all the groups
+const username = program.args[0];
+const email = program.args[1];
+const password = program.args[2];
+storage.connect()
+    .then(() => Users.get(username))
+    .then(existing => {
+        if (existing) throw `username "${username}" already exists`;
+
+        const user = Users.new(username, email);
+        user.password = password;
+        return user.save();
+    })
+    .then(() => console.log('user created!'))
+    .then(() => storage.disconnect())
+    .catch(err => {
+        console.error(err);
+        return storage.disconnect();
+    });
+/* eslint-enable no-console*/
