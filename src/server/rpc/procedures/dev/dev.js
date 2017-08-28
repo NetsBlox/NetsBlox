57c3608fefbe7f66eb4864d325ea002d86ccaa5e
Hamid Zare <dellydela@gmail.com>
2017-08-28 11:50:05 -0500

    removed isStateless as it is now removed and breaks things..

--- src/server/rpc/procedures/dev/dev.js
+++ src/server/rpc/procedures/dev/dev.js
@@ -1,7 +1,6 @@
 const rpcUtils = require('../utils/index');
 
 module.exports = {
-    isStateless: true,
     isSupported: () => process.env.ENV !== 'production',
     echo: function (argument){
         return rpcUtils.jsonToSnapList(argument);
