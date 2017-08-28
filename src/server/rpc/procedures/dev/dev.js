d844c0e3496fdbd7ea6bd90a63e212b712111af5
Hamid Zare <dellydela@gmail.com>
2017-08-28 11:09:22 -0500

    rename rpc to echo

--- src/server/rpc/procedures/dev/dev.js
+++ src/server/rpc/procedures/dev/dev.js
@@ -3,7 +3,7 @@ const rpcUtils = require('../utils/index');
 module.exports = {
     isStateless: true,
     isSupported: () => process.env.ENV !== 'production',
-    echoArgs: function (argument){
+    echo: function (argument){
         return rpcUtils.jsonToSnapList(argument);
     },
 };
