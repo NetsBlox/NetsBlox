8fd96f6e06ce7a3f87ba7572f320dfb485cfb504
Hamid Zare <dellydela@gmail.com>
2017-08-31 16:49:35 -0500

    added linked list tests

--- test/client/services.spec.js
+++ test/client/services.spec.js
@@ -10,16 +10,23 @@ describe('services', function() {
         });
 
         it('should serialize lists properly', function() {
-// NetsProcess.prototype.getJSFromRPCStruct = function (rpc, methodSignature) {
-            var world = driver.world();
-            var palette = driver.palette();
             var service = 'Dev',
-                rpcSignature = ['echo', ['argument']];
+                rpcSignature = ['echo', ['arg']];
             var listInput = new List([new List([2,3]),new List([3,4])]);
             var process = new NetsProcess();
             process.getJSFromRPCDropdown = (service, rpc, params)=> params;
             var rv = process.getJSFromRPCStruct(service, rpcSignature, listInput);
-            expect(rv).to.be('argument[0][0]=2&argument[0][1]=3&argument[1][0]=3&argument[1][1]=4');
+            expect(rv).to.be('arg[0][0]=2&arg[0][1]=3&arg[1][0]=3&arg[1][1]=4');
+        });
+
+        it('should serialize linked lists properly', function() {
+            var service = 'Dev',
+                rpcSignature = ['echo', ['arg']];
+            var linkedList = List.prototype.cons(2, new List([3,4,5]));
+            var process = new NetsProcess();
+            process.getJSFromRPCDropdown = (service, rpc, params)=> params;
+            var rv = process.getJSFromRPCStruct(service, rpcSignature, linkedList);
+            expect(rv).to.be('arg[0]=2&arg[1]=3&arg[2]=4&arg[3]=5');
         });
     });
 });
