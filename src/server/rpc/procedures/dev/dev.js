b307c21d423617b42265b2430468f4c8240a3fdc
Hamid Zare <dellydela@gmail.com>
2017-09-12 12:31:03 -0500

    changed 'echo' so that it doesn't go through jsonToSnapList #1521

--- src/server/rpc/procedures/dev/dev.js
+++ src/server/rpc/procedures/dev/dev.js
@@ -3,6 +3,6 @@ const rpcUtils = require('../utils/index');
 module.exports = {
     isSupported: () => process.env.ENV !== 'production',
     echo: function (argument){
-        return rpcUtils.jsonToSnapList(argument);
+        return argument;
     },
 };
