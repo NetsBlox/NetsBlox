b948d72649970447515d973cbd80875ecaf93d84
Hamid Zare <dellydela@gmail.com>
2017-09-19 09:02:53 -0500

    revert dev/dev echo rpc changes. separated the pr

--- src/server/rpc/procedures/dev/dev.js
+++ src/server/rpc/procedures/dev/dev.js
@@ -1,6 +1,8 @@
+const rpcUtils = require('../utils/index');
+
 module.exports = {
     isSupported: () => process.env.ENV !== 'production',
     echo: function (argument){
-        return argument;
+        return rpcUtils.jsonToSnapList(argument);
     },
 };
