de2decfe5d298452d295e78c99f5d54690d663d0
Hamid Zare <hamidzr@users.noreply.github.com>
2017-09-26 11:10:58 -0500

    gnuplot based charting service Fixes #1419 (#1514)
    * added collectStream func to rpcutils
    
    * added a js friendly wrapper aroung gnuplot module
    
    * added gnuplot as a charting option to chart service
    
    * added the option to set x,y labels
    
    * added gnuplot support for charting timeseries
    
    * updated chart and gnuplot for cleanup and added some tests
    
    *  fixes #1468
    
    * added a workaround for a list serialization bug on rpc input ( this should automatically be ignored once it's fixed). updated rpcUtils collectStream to accept a logger
    
    * take xLabel and yLabel fo rtimeseries data
    
    * removing chartjs files and code
    
    * updated default line title to be null,
    fixed problems with lodash merge mutating the default settings
    made graph title optional
    
    * refactored to use options datastructure instead of normal rpc inputs.
    added defaults rpc method ( to get the defaults ).
    
    * added a check for null array item
    
    * dont drop nulls
    
    * added qs lib to client to stringify user rpcs
    
    * added more tests to rpcinput and cleanedup the testfile
    
    * added a client test page with unminified js files. #1520
    
    * added kvListToJson fn. updated the reverse fn of it to stop converting
    null and undefined to array
    
    * dont add linetype and title if they don't exist
    
    * added timeseries drawing to 'draw' rpc. handling 'null' values from
    sending back default obj. removed misplaced opts.type
    
    * added default sorting for points, updated default xRange and y to []
    
    * simple xticks support
    
    * moved from custom sort fn to _.sortBy
    
    * added handling for diff input types for lines
    
    * added relative padding to charts, updated problem with setting ranges
    
    * removed timeSeries rpc
    
    * separated data preprations from genGnuData to mutate perform checks
    separately
    
    * added check for faulty inputs, simplle
    
    * added example
    
    * activated csplines smoothing
    
    * added smoothing option
    
    * updated examples with more chart options
    
    * cleanup
    
    * turn of eslint for imporeted library (qs)
    
    * more cleanup and codeclimate fixes
    
    * removed unused configs
    
    * changed chart to use _.fromPairs instead of custom kvListToJson, fixed
    gnuplot manual test
    
    * removed kvListToJson
    
    *  typo
    
    * added interface tests
    
    * removed a fixed todo. fixed a codeclimate issue
    
    * update example
    
    * removed extra logging for options
    
    * added one more style to example
    
    * added checks for input ( checking only the structure not the content)
    
    * added gnuplot to docker
    not fully tested
    
    * revert changes to services test
    
    * delete debug file for unminified resources
    
    * removed eslint command for disabling quotes
    the problem is fixed in another PR
    
    * missing semicolon
    
    * removed client test project
    
    * changed backticks to single quote whereever singlequote isn't used in
    the text
    
    * moved the example to be a client test
    
    * added support for previous chart methods. ( requires numerical xlabesl)
    
    * removed the option to choose the axis for timeSeries (defaults to x)
    rename timeOutputFormat to timeDisplayFormat
    
    * updated the client test
    
    * updated the testfile with new option names and old methods
    
    * codeclimate
    
    * parse all points to floats, throws an error if points are not numbers
    
    * WIP #1419 added libx11-dev to dockerfile for graphing

--- src/server/rpc/procedures/chart/chart.js
+++ src/server/rpc/procedures/chart/chart.js
@@ -1,150 +1,202 @@
-const ChartNode = require('chartjs-node');
-const ApiConsumer = require('../utils/api-consumer');
-const test = require('../utils/dataset-test');
-const rpcUtils = require('../utils');
-const Logger = require('../../../logger');
-const logger = new Logger('netsblox:rpc:chart');
+const ApiConsumer = require('../utils/api-consumer'),
+    rpcUtils = require('../utils'),
+    gnuPlot = require('./node-gnuplot.js'),
+    _ = require('lodash');
 
 let chart = new ApiConsumer('chart');
-let chartNode = new ChartNode(600, 600);
-let defaultColor = [
-    'rgba(74, 108, 212, 0.8)',
-    'rgba(217, 77, 17, 0.8)',
-    'rgba(207, 74, 217, 0.8)',
-    'rgba(0, 161, 120, 0.8)',
-    'rgba(143, 86, 227, 0.8)',
-    'rgba(230, 168, 34, 0.8)',
-    'rgba(4, 148, 220, 0.8)',
-    'rgba(98, 194, 19, 0.8)',
-    'rgba(243, 118, 29, 0.8)',
-    'rgba(150, 150, 15, 0.8)'
-];
 
-let defaultBlack = '#666';
-let defaultFont = 'Helvetica';
-let defaultFontSize = [18, 20];
-let defaultOption = (xAxis, yAxis, title) => {
-    return {
-        scales: {
-            xAxes: [{
-                scaleLabel: {
-                    display: true,
-                    labelString: xAxis,
-                    fontSize: defaultFontSize[0],
-                    fontFamily: defaultFont,
-                    fontColor: defaultBlack
-                }
-            }],
-            yAxes: [{
-                scaleLabel: {
-                    display: true,
-                    labelString: yAxis,
-                    fontSize: defaultFontSize[0],
-                    fontFamily: defaultFont,
-                    fontColor: defaultBlack
-                },
-                ticks: {
-                    beginAtZero:false
-                }
-            }]
-        },
-        title: {
-            display: true,
-            text: title,
-            fontSize: defaultFontSize[1],
-            padding: 25
-        },
-        gridLines: {
-            color: defaultBlack
-        },
-        tick: {
-            fontColor: defaultBlack
+const defaults = {
+    title: undefined,
+    labels: [],
+    types: [],
+    xRange: [],
+    yRange: [],
+    xLabel: undefined,
+    yLabel: undefined,
+    xTicks: undefined,
+    smooth: 'false',
+    isTimeSeries: 'false',
+    timeInputFormat: '%s',
+    timeDisplayFormat: '%H:%M'
+};
+
+// calculates data stats
+function calcRanges(lines){
+    let stats = {
+        x: {
+            min: Number.MAX_VALUE, max: -1 * Number.MAX_VALUE
+        }, 
+        y: {
+            min: Number.MAX_VALUE, max: -1 * Number.MAX_VALUE
         }
     };
-};
-
-chart._processDataset = function(dataset, yAxis, datasetTag, code, chartType) {
-    let result =  {
-        label: datasetTag,
-        data: test.getField(dataset, yAxis),
-        backgroundColor: defaultColor[code % 10],
-        borderColor: defaultColor[code % 10]
-    };
-    if (chartType === 'line') {
-        result.fill = false;
-    }
-    return result;
-};
-
-chart._processData = function(dataset, datasetTag, chartType) {
-    let data = {};
-    let xAxisTag = dataset[0][0][0];
-    let yAxisTag = dataset[0][1][0];
-    data.labels = test.getField(dataset, xAxisTag);
-    data.datasets = [chart._processDataset(dataset, yAxisTag, datasetTag, 0, chartType)];
-    return data;
-};
-
-chart._processMultipleData = function(dataset, datasetTag, chartType) {
-    let data = {};
-    let xAxisTag = dataset[0][0][0][0];
-    let yAxisTag = dataset[0][0][1][0];
-    data.labels = test.getField(dataset[0], xAxisTag);
-    data.datasets = [];
-    dataset.forEach((set, index) => {
-        data.datasets.push(chart._processDataset(set, yAxisTag, datasetTag[index], index, chartType));
+    lines.forEach(line => {
+        // min max of x
+        line = _.sortBy(line, (pt => pt[0]));
+        let {0 : xmin ,length : l, [l - 1] : xmax} = line.map(pt => pt[0]);
+        // min max of y
+        line = _.sortBy(line, (pt => pt[1]));
+        let {0 : ymin , [l - 1] : ymax} = line.map(pt => pt[1]);
+        if( xmin < stats.x.min ) stats.x.min = xmin;
+        if( xmax > stats.x.max ) stats.x.max = xmax;
+        if( ymin < stats.y.min ) stats.y.min = ymin;
+        if( ymax > stats.y.max ) stats.y.max = ymax;
     });
-    return data;
-};
+    Object.keys(stats).forEach( key => {
+        stats[key].range = stats[key].max - stats[key].min;
+    });
+    return stats;
+}
 
-chart._testDataset = function(rawArray, datasetTag) {
-    let multiple = test.isMultipleDataset(rawArray);
-    if (multiple) {
-        return test.testMultipleDatasets(rawArray, datasetTag);
-    } else {
-        return test.testValidDataset(rawArray);
+function prepareData(input) {
+    // if the input is one line convert it to appropriate format
+    if (! Array.isArray(input[0][0])){
+        chart._logger.trace('one line input detected');
+        input = [input];
     }
-};
-
-chart._drawChart = function (dataset, xAxisTag, yAxisTag, datasetTag, title, chartType) {
-    if (dataset === '') {
-        this.response.status(404).send('Dataset is blank');
-    } else {
-        let testResult = this._testDataset(dataset, datasetTag);
-        if (testResult !== '') {
-            this.response.status(404).send(testResult);
-        } else {
-            let data;
-            if (test.isMultipleDataset(dataset)) {
-                data = chart._processMultipleData(dataset, datasetTag, chartType);
-            } else {
-                data = chart._processData(dataset, datasetTag, chartType);
-            }
-            let chartOptions =  {
-                type: chartType,
-                data: data,
-                options: defaultOption(xAxisTag, yAxisTag, title)
-            };
-        
-            return chartNode.drawChart(chartOptions).then(() => {
-                return chartNode.getImageBuffer('image/png');
-            }).then((imageBuffer) => {
-                rpcUtils.sendImageBuffer(this.response, imageBuffer);
-            }).catch(err => {
-                logger.warn(`chart drawing failed: ${err}`);
-                this.response.status(404).send('Error with service');
-            });
+    input = input.map( line => {
+        if (!Array.isArray(line)) {
+            chart._logger.warn('input is not an array!', line);
+            throw 'chart input is not an array';
         }
+        line.map(pt => {
+            let [x,y] = pt;
+            if (!Array.isArray(pt)) {
+                chart._logger.warn('input is not an array!', pt);
+                throw 'all input points should be in [x,y] form.';
+            }
+            pt[0] = parseFloat(pt[0]);
+            pt[1] = parseFloat(pt[1]);
+            if ( !x || !y || isNaN(x) || isNaN(y) ) throw 'all [x,y] pairs should be numbers';
+            return pt;
+        });
+        return line;
+    });
+    return input;
+}
+
+
+// generate gnuplot friendly line objects
+function genGnuData(lines, lineTitles, lineTypes, smoothing){
+    return lines.map((pts, idx) => {
+        pts = _.sortBy(pts, (pt => pt[0]));
+        let lineObj = {points: pts};
+        if (lineTypes) lineObj.type = lineTypes[idx];
+        if (lineTitles) lineObj.title = lineTitles[idx];
+        if (smoothing) lineObj.smoothing = 'csplines';
+        return lineObj;
+    });
+}
+
+chart.draw = function(lines, options){
+    options = _.fromPairs(options);
+    Object.keys(options).forEach(key => {
+        if (options[key] === 'null' || options[key] === ''){
+            delete options[key];
+        }
+    });
+    options = _.merge({}, defaults, options || {});
+
+    // prepare and check for errors in data
+    try {
+        lines = prepareData(lines);
+    } catch (e) {
+        this.response.status(500).send(e);
+        return null;
     }
-};
-//
-chart.drawBarChart = function(dataset, xAxisTag, yAxisTag, datasetTag, title) {
-    return this._drawChart(dataset, xAxisTag, yAxisTag, datasetTag, title, 'bar');
+
+    let stats = calcRanges(lines);
+    this._logger.info('data stats:', stats);
+    const relativePadding = {
+        x: stats.x.range * 0.05,
+        y: stats.y.range * 0.05
+    };
+    let data = genGnuData(lines, options.labels, options.types, options.smooth === 'true');
+    let opts = {title: options.title, xLabel: options.xLabel, yLabel: options.yLabel};
+    opts.xRange = {min: stats.x.min - relativePadding.x, max: stats.x.max + relativePadding.x};
+    opts.yRange = {min: stats.y.min - relativePadding.y, max: stats.y.max + relativePadding.y};
+    if (options.xRange.length === 2) opts.xRange = {min: options.xRange[0], max: options.xRange[1]};
+    if (options.yRange.length === 2) opts.yRange = {min: options.yRange[0], max: options.yRange[1]};
+    if (options.isTimeSeries == 'true') {
+        opts.timeSeries = {
+            axis: 'x',
+            inputFormat: options.timeInputFormat,
+            outputFormat: options.timeDisplayFormat
+        };
+    }
+    
+    // if a specific number of ticks are requested
+    if (options.xTicks) {
+        let tickStep = (stats.x.max - stats.x.min)/options.xTicks;
+        opts.xTicks = [stats.x.min, tickStep, stats.x.max];
+    }
+    
+    this._logger.trace('charting with options', opts);
+
+    try {
+        var chartStream =  gnuPlot.draw(data, opts);
+    } catch (e) {
+        this.response.status(500).send('error in drawing the plot. bad input.');
+        return null;
+    }
+
+    return rpcUtils.collectStream(chartStream).then( buffer => {
+        rpcUtils.sendImageBuffer(this.response, buffer, this._logger);
+    }).catch(this._logger.error);
 };
 
-chart.drawLineChart = function(dataset, xAxisTag, yAxisTag, datasetTag, title) {
-    return this._drawChart(dataset, xAxisTag, yAxisTag, datasetTag, title, 'line');
+chart.drawLineChart = function(dataset, xAxisTag, yAxisTag, datasetTag, title){
+    let lines = [];
+
+    // testMultipleDataset credit to Dung
+    let isMultipleDataset = rawArray => {
+        let numLayers = (rawArray) => {
+            if (typeof rawArray !== 'object') {
+                return 0;
+            }
+            return numLayers(rawArray[0]) + 1;
+        };
+
+        return numLayers(rawArray) === 4;
+    };
+
+    if (!isMultipleDataset(dataset)){
+        this._logger.trace('oneline input detected');
+        dataset = [dataset];
+    }
+
+    dataset.forEach(line => {
+        line = line.map(pt => {
+            let newPt = [];
+            newPt.push(pt[0][1]);
+            newPt.push(pt[1][1]);
+            return newPt;
+        });
+        lines.push(line);
+    });
+
+    // account for list or string datasettag
+    if (!Array.isArray(datasetTag)){
+        datasetTag = [datasetTag];
+    }
+
+    let opts = {
+        xLabel: xAxisTag,
+        yLabel: yAxisTag,
+        title: title,
+        smooth: true,
+        labels: datasetTag
+    };
+
+    return chart.draw.call(this, lines, _.toPairs(opts));
 };
 
+chart.drawBarChart = function(dataset, xAxisTag, yAxisTag, datasetTag, title){
+    return chart.drawLineChart.apply(this, arguments);
+};
+
+chart.defaultOptions = function(){
+    return rpcUtils.jsonToSnapList(defaults);
+};
 
 module.exports = chart;
---
+++ src/server/rpc/procedures/chart/node-gnuplot.js
@@ -0,0 +1,99 @@
+const gnuplot = require('gnuplot'),
+    _ = require('lodash');
+
+const lineDefaults = {
+    title: '',
+    type: 'lines',
+    smoothing: undefined,
+    points: []
+};
+
+// opts defaults
+const optsDefaults = {
+    xRange: undefined,
+    yRange: undefined,
+    xLabel: undefined,
+    yLabel: undefined,
+    xTicks: undefined,
+    timeSeries: {
+        axis: undefined,
+        inputFormat: undefined,
+        outputFormat: undefined
+    },
+    title: null,
+    outputName: false
+};
+
+function pointsToInlineData(points){
+    let inlineStr = points.map(point => point.join(',')).join('\n');
+    return ' \n' + inlineStr + '\ne';
+}
+
+// if sharing a column, using can be used to select which columns relate (x & y)
+// returns a plot command string
+function dataToPlot(data){ 
+    let settings = [],
+        points = '';
+    data.forEach(line => {
+        let smoothing = line.smoothing ? `smooth ${line.smoothing}` : '';
+        settings.push(`'-' using 1:2 title '${line.title}' with ${line.type} ${smoothing}`);
+        points += pointsToInlineData(line.points);
+    });
+    let cmdString = settings.join(', ') + points;
+    // console.log('cmd', cmdString)
+    return cmdString;
+}
+// graph.plot(`'-' title 'bla' with line, '-' title 'secondLine' with line \n1,2\n2,3\ne\n3,1\n1,3\ne`)
+
+let draw = (data, opts) => {
+    // apply the defaults
+    let graph = gnuplot();
+    data = data.map(line => _.merge({}, lineDefaults, line));
+    opts = _.merge({}, optsDefaults, opts);
+    graph.set('term pngcairo')
+        .set(`loadpath '${__dirname}'`)
+        .set(`load 'xyborder.cfg'`)
+        .set(`load 'grid.cfg'`)
+        // .set(`zeroaxis`)
+
+        // add grid
+        .set(`style line 102 lc rgb '#d6d7d9' lt 0 lw 1`)
+        .set('grid back ls 102')
+        // add colors
+        .set(`style line 1 lt 1 lc rgb '#1B9E77'`)  // dark teal
+        .set(`style line 2 lt 1 lc rgb '#D95F02'`)  // dark orange
+        .set(`style line 3 lt 1 lc rgb '#7570B3'`)  // dark lilac
+        .set(`style line 4 lt 1 lc rgb '#E7298A'`)  // dark magenta
+        .set(`style line 5 lt 1 lc rgb '#66A61E'`)  // dark lime green
+        .set(`style line 6 lt 1 lc rgb '#E6AB02'`)  // dark banana
+        .set(`style line 7 lt 1 lc rgb '#A6761D'`)  // dark tan
+        .set(`style line 8 lt 1 lc rgb '#666666'`)  // dark gray
+        // palette
+        .set('palette maxcolors 8')
+        .set(`palette defined ( 0 '#1B9E77', 1 '#D95F02', 2 '#7570B3', 3 '#E7298A', 4 '#66A61E', 5 '#E6AB02', 6 '#A6761D',7 '#666666' )`)
+
+        .set(`datafile separator ','`);
+
+    // if (data.length > 1) graph.set(`multiplot layout 1,2`) // this option is for drawing multiple charts
+    
+    if (opts.title) graph.set(`title '${opts.title}'`);
+    if (opts.xRange) graph.set(`xrange [${opts.xRange.min}:${opts.xRange.max}]`);
+    if (opts.yRange) graph.set(`yrange [${opts.yRange.min}:${opts.yRange.max}]`);
+    if (opts.outputName) graph.set(`output '${opts.outputName}'`);
+    if (opts.xLabel) graph.set(`xlabel '${opts.xLabel}'`);
+    if (opts.yLabel) graph.set(`ylabel '${opts.yLabel}'`);
+    if (opts.xTicks) graph.set(`xtics ${opts.xTicks.join(', ')}`);
+    if (opts.timeSeries.axis) {
+        graph.set(`${opts.timeSeries.axis}data time`);
+        graph.set(`timefmt '${opts.timeSeries.inputFormat}'`);
+        graph.set(`format ${opts.timeSeries.axis} '${opts.timeSeries.outputFormat}'`);
+    }
+
+    graph.plot(dataToPlot(data),{end: true});
+
+    return graph;
+};
+
+module.exports = {
+    draw,
+};
