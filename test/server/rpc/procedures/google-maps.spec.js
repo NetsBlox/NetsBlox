830ec01910db62ac6b094bbc1484ce27df0e2f42
Hamid Zare <dellydela@gmail.com>
2017-10-12 11:17:40 -0500

    added tests for calculating distance

--- test/server/rpc/procedures/google-maps.spec.js
+++ test/server/rpc/procedures/google-maps.spec.js
@@ -1,6 +1,7 @@
 describe('staticmap', function() {
     var StaticMap = require('../../../../src/server/rpc/procedures/google-maps/google-maps'),
         RPCMock = require('../../../assets/mock-rpc'),
+        assert = require('assert'),
         utils = require('../../../assets/utils'),
         staticmap = new RPCMock(StaticMap);
 
@@ -31,4 +32,16 @@ describe('staticmap', function() {
             ['minLatitude']
         ]);
     });
+
+    describe('getDistance', function() {
+        it('should calculate distance in meters (string input)', function(){
+            let distance = staticmap.getDistance('36', '-86', '36', '-87');
+            assert.deepEqual(distance, 90163);
+        });
+        it('should calculate distance in meters (integer input)', function(){
+            let distance = staticmap.getDistance(36, -86, 36, -87);
+            assert.deepEqual(distance, 90163);
+        });
+    });
+
 });
