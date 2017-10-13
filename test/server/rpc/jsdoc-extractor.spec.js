da6d90429408c8edf322144943278e95d4df6611
Hamid Zare <dellydela@gmail.com>
2017-10-13 10:02:21 -0500

    added unit tests

---
+++ test/server/rpc/jsdoc-extractor.spec.js
@@ -0,0 +1,30 @@
+const assert = require('assert'),
+    utils = require('../../assets/utils.js'),
+    jp = utils.reqSrc('rpc/jsdoc-extractor.js');
+
+describe('jsdoc-extractor', () => {
+
+    describe.only('fnFinder', () => {
+        let testText = `let doStuff = a => a*2;
+    function doStuff(){}
+    GoogleMap.doStuff = function
+    GoogleMap.doStuff = (asdf) =>`;
+
+        let testLines = testText.split('\n');
+
+        it('should support multiline', () => {
+            assert.deepEqual(jp._findFn(testLines[2]), 'doStuff');
+        });
+
+        it('should find let fn = ()', () => {
+            let line = 'let reverseGeocode = (lat, lon, response, query)=>{';
+            assert.deepEqual(jp._findFn(line), 'reverseGeocode');
+        });
+
+        it('should fiind obj.ojb = function', () => {
+            let line = '    GeoLocationRPC.geolocate = function (address) {';
+            assert.deepEqual(jp._findFn(line), 'geolocate');
+        });
+    });
+
+});
