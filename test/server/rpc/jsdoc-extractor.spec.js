a3f977c22b49aa7582135e48dc211d7e0fd43eb2
Hamid Zare <dellydela@gmail.com>
2017-10-17 12:00:09 -0500

    added tests for simplifying

--- test/server/rpc/jsdoc-extractor.spec.js
+++ test/server/rpc/jsdoc-extractor.spec.js
@@ -4,12 +4,23 @@ const assert = require('assert'),
 
 describe('jsdoc-extractor', () => {
 
+    let comment = `
+    /**
+     * this is the description
+     * next line of description
+     * @param {string} address target address
+     * @param {number} limit the results limit
+     * @param {Object} options
+     * @returns {string}
+     * @name associatedFnName
+     */
+    `;
+
     describe('fnFinder', () => {
         let testText = `let doStuff = a => a*2;
     function doStuff(){}
     GoogleMap.doStuff = function
     GoogleMap.doStuff = (asdf) =>`;
-
         let testLines = testText.split('\n');
 
         it('should support multiline', () => {
@@ -29,6 +40,41 @@ describe('jsdoc-extractor', () => {
         it('should find obj.obj = function', () => {
             let line = '    GeoLocationRPC.geolocate = function (address) {';
             assert.deepEqual(jp._findFn(line), 'geolocate');
+        });
+    });
+
+
+    describe('parsing', () => {
+
+        let metadata = jp._parseSource(comment)[0];
+
+        it('should parse jsdoc comments', () => {
+            assert.deepEqual(metadata.parsed.tags[1].name, 'limit');
+        }); 
+
+        it('should simplify the metadata', () => {
+            let simpleMetadata = jp._simplify(metadata.parsed);
+            assert.deepEqual(simpleMetadata, {
+                description: metadata.parsed.description,
+                args: [
+                    {
+                        name: 'address',
+                        type: 'string',
+                        description: 'target address'
+                    },
+                    {
+                        name: 'limit',
+                        type: 'number',
+                        description: 'the results limit'
+                    },
+                    {
+                        name: 'options',
+                        type: 'Object',
+                        description: null
+                    }
+                ],
+                returns: {type: 'string', description: null}
+            });
         });
     });
 
