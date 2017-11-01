d764e85b629b7949990e91a84a583afde48e54b7
Brian Broll <brian.broll@vanderbilt.edu>
2017-11-01 15:10:48 -0500

    WIP #1633 added test verifying only es5 on the client

---
+++ test/server/build.js
@@ -0,0 +1,26 @@
+describe('build', function() {
+    describe('es5 client js', function() {
+        let fs = require('fs'),
+            path = require('path'),
+            srcPath = path.join(__dirname, '..', '..', 'src', 'client'),
+            ugly = require('uglify-js');
+
+        // Get the given js files
+        let devHtml = fs.readFileSync(path.join(srcPath, 'netsblox-dev.html'), 'utf8'),
+            re = /text\/javascript" src="(.*)">/,
+            match = devHtml.match(re),
+            srcFiles = [];
+
+        while (match) {
+            srcFiles.push(match[1]);
+            devHtml = devHtml.substring(match.index + match[0].length);
+            match = devHtml.match(re);
+        }
+
+        srcFiles.forEach(filename => {
+            it(`should use es5 in ${filename}`, function() {
+                ugly.minify(path.join(srcPath, filename)).code;
+            });
+        });
+    });
+});
