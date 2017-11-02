3b576ce1c633b4deb1d11c3964d3a7ef8f246132
Brian Broll <brian.broll@vanderbilt.edu>
2017-11-02 16:15:35 -0500

    WIP #1637 Added seeding projects collection

--- test/fixtures/index.js
+++ test/fixtures/index.js
@@ -1,15 +1,39 @@
-// TODO: seed the database with the fixtures
+// seed the database with the fixtures
 const users = require('./users');
+const projects = require('./projects');
 const Q = require('q');
 const hash = require('../../src/common/sha512').hex_sha512;
+const ActiveRoom = require('../../src/server/rooms/active-room');
+const PublicProjects = require('../../src/server/storage/public-projects');
+const Logger = require('../../src/server/logger');
+const logger = new Logger('netsblox:test:fixtures');
 
 function seed(storage) {
-    // TODO: Add the users
+    // Add the users and the projects from the respective files!
     return Q.all(users.map(data => {  // create the users
         let user = storage.users.new(data.username, data.email);
         user.hash = hash(data.password);
         return user.save();
-    }));
+    }))
+    .then(Q.all(projects.map(data => {
+        // Load the project
+        let project = null;
+        const room = new ActiveRoom(logger, data.name, data.owner);
+        const userData = {
+            username: data.owner,
+            roleId: data.activeRole
+        };
+
+        // create, set role data, publish!
+        return storage.projects.new(userData, room)
+            .then(_project => project = _project)
+            .then(() => project.setRoles(data.roles))
+            .then(() => {
+                if (data.Public) {
+                    return PublicProjects.publish(project);
+                }
+            });
+    })));
 }
 
 module.exports.init = seed;
---
+++ test/fixtures/projects.js
@@ -0,0 +1,63 @@
+/*
+ * "owner","name","roles" are required. Everything else is optional. Defaults:
+ *
+ *   transient: false
+ *   Public: false
+ *   collaborators: []
+ *
+ */
+const _ = require('lodash');
+const utils = require('../../src/server/server-utils');
+const fs = require('fs');
+const path = require('path');
+const PROJECT_DATA_DIR = path.join(__dirname, 'projects');
+
+function loadProjectData(project) {
+    const projectDir = path.join(PROJECT_DATA_DIR, project.projectData);
+    let roleNames = fs.readdirSync(projectDir)
+        .map(name => name.replace(/-(src|media)\.xml$/, ''));
+
+    project.roles = {};
+
+    roleNames = _.uniq(roleNames);
+    project.roles = roleNames.map(name => {
+        let src = fs.readFileSync(path.join(projectDir, `${name}-src.xml`), 'utf8');
+        let media = fs.readFileSync(path.join(projectDir, `${name}-media.xml`), 'utf8');
+        return {
+            ProjectName: name,
+            SourceCode: src,
+            Media: media,
+            SourceSize: src.length,
+            MediaSize: media.length,
+            Thumbnail: utils.xml.thumbnail(src),
+            Updated: new Date(),
+            Public: false,
+            Notes: ''
+        };
+    });
+
+    delete project.projectData;
+    return project;
+}
+
+function addDefaults(project) {
+    project.collaborators = project.collaborators || [];
+    project.transient = !!project.transient;
+    project.Public = !!project.Public;
+    project.name = project.name || project.projectData;
+
+    loadProjectData(project);
+
+    project.activeRole = project.activeRole || Object.keys(project.roles).pop();
+
+    return project;
+}
+
+module.exports = [
+    {
+        owner: 'brian',
+        name: 'PublicProject',
+        Public: true,
+        projectData: 'drawing'
+    }
+].map(addDefaults);
