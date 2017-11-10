945bcbb6f1244a4c9cb5f5b6fa377434462fe38d
Hamid Zare <dellydela@gmail.com>
2017-11-09 18:06:30 -0600

    allow sessionglue headers

--- src/server/server.js
+++ src/server/server.js
@@ -61,7 +61,7 @@ Server.prototype.configureRoutes = function() {
     this.app.use(function(req, res, next) {
         res.header('Access-Control-Allow-Origin', '*');
         res.header('Access-Control-Allow-Credentials', true);
-        res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+        res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, SESSIONGLUE');
         next();
     });
 
