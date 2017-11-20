e62f65c0ba75941d431f2845882b4e6dc552c028
Brian Broll <brian.broll@vanderbilt.edu>
2017-11-20 11:56:29 -0600

    WIP Added timestamp to sent messages

--- src/server/storage/messages.js
+++ src/server/storage/messages.js
@@ -9,9 +9,11 @@ Messages.init = (_logger, db) => {
 };
 
 Messages.save = message => {
-    logger.trace(`saving message: ${JSON.stringify(message, null, 2)}`);
     // TODO: store the given message in the database
     // It may be better to batch the writes...
+    message.time = Date.now();
+
+    logger.trace(`saving message: ${JSON.stringify(message, null, 2)}`);
     return collection.save(message);
 };
 
