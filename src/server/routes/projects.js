747a1e69838eaaf0165cd1ea0f6b4d5e25ba600f
Brian Broll <brian.broll@gmail.com>
2017-12-20 06:20:42 -0600

    Replaced raw image data with url in project list. Fixes #1780

--- src/server/routes/projects.js
+++ src/server/routes/projects.js
@@ -54,7 +54,7 @@ var setProjectPublic = function(name, user, value) {
 };
 
 // Select a preview from a project (retrieve them from the roles)
-var getPreview = function(project) {
+var getProjectInfo = function(project) {
 
     const roles = Object.keys(project.roles).map(k => project.roles[k]);
     const preview = {
@@ -80,6 +80,16 @@ var getPreview = function(project) {
     preview.Public = project.Public;
     preview.Owner = project.owner;
     return preview;
+};
+
+var getProjectMetadata = function(project) {
+    let metadata = getProjectInfo(project);
+    metadata.Thumbnail = `/api/projects/${project.owner}/${project.name}/thumbnail`;
+    return metadata;
+};
+
+var getProjectThumbnail = function(project) {
+    return getProjectInfo(project).Thumbnail;
 };
 
 ////////////////////// Project Helpers //////////////////////
@@ -277,7 +287,7 @@ module.exports = [
                                 trace(`found shared project list (${projects.length}) ` +
                                     `for ${username}: ${projects.map(proj => proj.name)}`);
 
-                                const previews = projects.map(getPreview);
+                                const previews = projects.map(getProjectMetadata);
                                 const names = JSON.stringify(previews.map(preview =>
                                     preview.ProjectName));
 
@@ -316,7 +326,7 @@ module.exports = [
                                 trace(`found project list (${projects.length}) ` +
                                     `for ${username}: ${projects.map(proj => proj.name)}`);
 
-                                const previews = projects.map(getPreview);
+                                const previews = projects.map(getProjectMetadata);
                                 info(`Projects for ${username} are ${JSON.stringify(
                                     previews.map(preview => preview.ProjectName)
                                 )}`
@@ -545,15 +555,15 @@ module.exports = [
             return Projects.getRawProject(req.params.owner, name)
                 .then(project => {
                     if (project) {
-                        const preview = getPreview(project);
-                        if (!preview || !preview.Thumbnail) {
+                        const thumbnail = getProjectThumbnail(project);
+                        if (!thumbnail) {
                             const err = `could not find thumbnail for ${name}`;
                             this._logger.error(err);
                             return res.status(400).send(err);
                         }
                         this._logger.trace(`Applying aspect ratio for ${req.params.owner}'s ${name}`);
                         return applyAspectRatio(
-                            preview.Thumbnail,
+                            thumbnail,
                             aspectRatio
                         ).then(buffer => {
                             this._logger.trace(`Sending thumbnail for ${req.params.owner}'s ${name}`);
