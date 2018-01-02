b5c6d86b703a9bdc4f122a413edc2d8bf568e80a
Brian Broll <brian.broll@gmail.com>
2018-01-02 08:55:18 -0600

    WIP #1795 added room/role tests

---
+++ test/unit/client/room.spec.js
@@ -0,0 +1,167 @@
+/*global driver*/
+describe('room', function() {
+    describe('roles', function() {
+        describe('new', function() {
+            const name = 'newRoleName';
+            before(done => {
+                driver.reset(() => {
+                    // Select the room tab
+                    driver.addBlock('forward').accept(() => {
+                        driver.selectTab('Room');
+                        let btn = driver.ide().spriteEditor.addRoleBtn;
+
+                        // Click on the plus icon
+                        driver.click(btn);
+                        driver.keys(name);
+                        let dialog = driver.dialog();
+                        dialog.ok();
+
+                        // wait for it to show up
+                        let room = driver.ide().room;
+                        driver.waitUntil(() => {
+                            return room.roles[name];
+                        }, found => {
+                            if (found) return done();
+                            done(`could not create role "${name}"`);
+                        });
+                    });
+                });
+            });
+
+            describe('moveToRole', function() {
+                before(function() {
+                    driver.moveToRole(name);
+                });
+
+                it('should be able to move to new role', function(done) {
+                    // wait for the project name to change
+                    driver.waitUntil(() => {
+                        return driver.ide().projectName === name;
+                    }, found => {
+                        if (found) return done();
+                        done(`could move to role "${name}"`);
+                    });
+                });
+
+                it('should be an empty role', function(done) {
+                    // Check for existing blocks
+                    driver.waitUntil(() => {
+                        return !driver.ide().currentSprite.scripts.children.length;
+                    }, foundNoBlocks => {
+                        if (foundNoBlocks) return done();
+                        done(`did not load empty role"${name}"`);
+                    });
+                });
+            });
+        });
+
+        describe('rename', function() {
+            const newName = 'newRoleName';
+            before(done => {
+                driver.reset(() => {
+                    driver.selectTab('Room');
+
+                    const roleName = driver.ide().projectName;
+                    const role = driver.ide().room.roleLabels[roleName];
+
+                    // rename the role
+                    driver.click(role._label._roleLabel);
+                    driver.keys(newName);
+                    driver.dialog().ok();
+                    done();
+                });
+            });
+
+            it('should change role name in room tab', function(done) {
+                driver.waitUntil(() => {
+                    return driver.ide().room.roleLabels[newName];
+                }, passed => {
+                    if (passed) return done();
+                    done('role did not change names');
+                });
+            });
+        });
+
+        describe('duplicate', function() {
+            before(done => {
+                driver.reset(() => {
+                    driver.addBlock('forward').accept(() => {
+                        driver.selectTab('Room');
+
+                        const roleName = driver.ide().projectName;
+                        const role = driver.ide().room.roleLabels[roleName];
+
+                        // duplicate the role
+                        driver.click(role._label);
+                        const dupBtn = driver.dialog().buttons.children
+                            .find(btn => btn.action === 'createRoleClone');
+                        driver.click(dupBtn);
+                        done();
+                    });
+                });
+            });
+
+            it('should create a new role', function(done) {
+                driver.waitUntil(() => {
+                    const roleNames = Object.keys(driver.ide().room.roleLabels);
+                    return roleNames.length === 2;
+                }, passed => {
+                    if (passed) return done();
+                    done('new role did not appear');
+                });
+            });
+
+            it('should contain the same blocks', function(done) {
+                const roleName = driver.ide().projectName;
+                const roleNames = Object.keys(driver.ide().room.roleLabels);
+                const newRoleName = roleNames.find(name => name !== roleName);
+                const currentBlockCount = driver.ide().currentSprite.scripts.children.length;
+
+                // Move to the role and check the blocks
+                driver.moveToRole(newRoleName);
+                driver.waitUntil(() => {
+                    return driver.ide().currentSprite.scripts.children.length === currentBlockCount;
+                }, passed => {
+                    if (passed) return done();
+                    done('role does not contain expected blocks');
+                });
+            });
+        });
+
+        describe('remove', function() {
+            const newRoleName = 'testRole';
+
+            before(function(done) {
+                driver.newRole(newRoleName);
+
+                driver.waitUntil(() => {
+                    return driver.ide().room.roleLabels[newRoleName];
+                }, passed => {
+                    if (!passed) return done('new role did not show up');
+
+                    const role = driver.ide().room.roleLabels[newRoleName];
+                    // duplicate the role
+                    driver.click(role._label);
+                    const delBtn = driver.dialog().buttons.children
+                        .find(btn => btn.action === 'deleteRole');
+
+                    driver.click(delBtn);
+                    done();
+                });
+            });
+
+            it('should remove the role', function(done) {
+                driver.waitUntil(() => {
+                    const roleNames = Object.keys(driver.ide().room.roleLabels);
+                    return roleNames.includes(newRoleName);
+                }, passed => {
+                    if (passed) return done();
+                    done('could not remove new role');
+                });
+            });
+        });
+    });
+
+    it('should be able to rename the project', function() {
+    });
+});
