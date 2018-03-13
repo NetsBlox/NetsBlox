25a31f35cf04810098cc7b019c7d60a4606aa51e
Brian Broll <brian.broll@vanderbilt.edu>
2018-03-13 18:52:58 -0500

    WIP #1708 added tests for login/logout

--- test/unit/server/routes/project.js
+++ test/unit/server/routes/project.js
@@ -1,5 +1,6 @@
 describe('project routes', function() {
     const utils = require('../../../assets/utils');
+    const hash = require('../../../../src/common/sha512').hex_sha512;
     const assert = require('assert');
     const supertest = require('supertest');
     let port = 8393,
@@ -69,5 +70,40 @@ describe('project routes', function() {
                 .expect(200)
                 .end(done);
         });
+    });
+
+    describe('login/logout', function() {
+
+        it('should support login', function(done) {
+            const user = 'brian';
+            const password = hash('secretPassword');
+
+            api.post(`/`)
+                .send({__u: user, __h: password, remember: true})
+                .expect(res => res.text.startsWith('Service'))
+                .end(err => done(err));
+        });
+
+        it('should support logout', function(done) {
+            const user = 'brian';
+            const password = hash('secretPassword');
+
+            api.post(`/`)
+                .send({__u: user, __h: password, remember: true})
+                .expect(res => res.text.startsWith('Service'))
+                .end(err => {
+                    if (err) return done(err);
+                    // Logout
+                    api.post(`/logout`)
+                        .expect(res => {  // check that the cookie is reset
+                            const setCookieCmd = res.header['set-cookie']
+                                .find(cmd => cmd.includes('netsblox-cookie=;'));
+
+                            assert(setCookieCmd);
+                        })
+                        .end(err => done(err));
+                });
+        });
+
     });
 });
