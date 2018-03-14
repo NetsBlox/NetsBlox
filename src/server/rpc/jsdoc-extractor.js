fda8792e77a7134d34bae4531d5c035efdeeea96
Brian Broll <brian.broll@vanderbilt.edu>
2018-03-14 15:57:46 -0500

    Filter out service annotations from rpc ann's. Fixes #1914

--- src/server/rpc/jsdoc-extractor.js
+++ src/server/rpc/jsdoc-extractor.js
@@ -73,25 +73,27 @@ function parseSource(source, searchScope) {
         .map(block => block.parsed.description)
         .pop();
 
-    const rpcDocs = blocks.filter(block => {
-        let linesToSearch = lines.slice(block.endLine, block.endLine + searchScope);
-        let fnName;
-        // if @name is set just use that and save a few cycles
-        let nameTag = block.parsed.tags.find(tag => tag.title === 'name');
-        if (nameTag) {
-            fnName  = nameTag.name;
-            logger.info('fn name set through @name', fnName);
-        } else {
-            fnName = findFn(linesToSearch);
-            if (!fnName){
-                logger.warn(`can't associate ${block.lines} with any function. # Fix it at line ${block.beginLine}, column ${block.column}`);
-                return false;
+    const rpcDocs = blocks
+        .filter(block => !block.parsed.tags.find(tag => tag.title === 'service'))
+        .filter(block => {
+            let linesToSearch = lines.slice(block.endLine, block.endLine + searchScope);
+            let fnName;
+            // if @name is set just use that and save a few cycles
+            let nameTag = block.parsed.tags.find(tag => tag.title === 'name');
+            if (nameTag) {
+                fnName  = nameTag.name;
+                logger.info('fn name set through @name', fnName);
+            } else {
+                fnName = findFn(linesToSearch);
+                if (!fnName){
+                    logger.warn(`can't associate ${block.lines} with any function. # Fix it at line ${block.beginLine}, column ${block.column}`);
+                    return false;
+                }
+                block.parsed.tags.push({title: 'name', name: fnName, description: null});
             }
-            block.parsed.tags.push({title: 'name', name: fnName, description: null});
-        }
-        block.fnName = fnName;
-        return true;
-    });
+            block.fnName = fnName;
+            return true;
+        });
 
     return {
         description,
