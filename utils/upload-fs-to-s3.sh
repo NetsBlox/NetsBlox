5e0f95a542bd6a7bd7011b120ab196fdf973f13b
Brian Broll <brian.broll@vanderbilt.edu>
2018-03-16 08:22:03 -0500

    Diff the existing and total files for new files to upload. Closes #1927 (#1928)

--- utils/upload-fs-to-s3.sh
+++ utils/upload-fs-to-s3.sh
@@ -2,6 +2,20 @@
 #
 # Requires awscli to be installed and configured.
 
-for id in $(node list-blob-objects.js); do
+BLOB_BACKEND=fs
+node list-blob-objects.js > fs-objects.txt
+echo "Found $(wc -l fs-objects.txt) objects in the fs blob"
+
+BLOB_BACKEND=s3
+node list-blob-objects.js > s3-objects.txt
+echo "Found $(wc -l s3-objects.txt) objects in the s3 blob"
+
+grep -v -F -x -f fs-objects.txt s3-objects.txt > new-objects.txt
+echo "Found $(wc -l new-objects.txt) new objects to upload to s3"
+
+for id in $(cat new-objects.txt); do
     aws s3api put-object --bucket $S3_BUCKET --key $id --body $NETSBLOX_BLOB_DIR/${id:0:2}/${id:2}
 done
+
+echo "Removing temp files..."
+rm s3-objects.txt fs-objects.txt new-objects.txt
