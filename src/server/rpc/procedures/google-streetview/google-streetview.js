a3595a722c0ce9ae6a0e35163cd4fa8159ab6d17
Gordon Stein <gstein@ltu.edu>
2018-04-18 13:42:06 -0400

    Improved Street View RPC cache and types

--- src/server/rpc/procedures/google-streetview/google-streetview.js
+++ src/server/rpc/procedures/google-streetview/google-streetview.js
@@ -10,7 +10,7 @@
 var key = process.env.GOOGLE_MAPS_KEY;
 
 const ApiConsumer = require('../utils/api-consumer');
-const GoogleStreetView = new ApiConsumer('google-streetview', 'https://maps.googleapis.com/maps/api/streetview?',{cache: {ttl: 5*60}});
+const GoogleStreetView = new ApiConsumer('google-streetview', 'https://maps.googleapis.com/maps/api/streetview?',{cache: {ttl: 7*24*60*60}});
 
 GoogleStreetView.isSupported = () => {
     if(!key){
@@ -23,30 +23,30 @@ GoogleStreetView.isSupported = () => {
 
 /**
  * Get Street View image of a location using coordinates
+ * @param {Latitude} latitude Latitude coordinate of location
+ * @param {Longitude} longitude Longitude coordinate of location
  * @param {Number} width Width of image
  * @param {Number} height Height of image
- * @param {Number} latitude Latitude coordinate of location
- * @param {Number} longitude Longitude coordinate of location
  * @param {Number} fieldofview Field of View of image
  * @param {Number} heading Heading of view
  * @param {Number} pitch Pitch of view
  * @returns {Image} Image of requested location with specified size and orientation
  */
-GoogleStreetView.getViewFromLatLong = function(width, height, latitude, longitude, fieldofview, heading, pitch) {
+GoogleStreetView.getViewFromLatLong = function(latitude, longitude, width, height, fieldofview, heading, pitch) {
     return this._sendImage({queryString: `size=${width}x${height}&location=${latitude},${longitude}&fov=${fieldofview}&heading=${heading}&pitch=${pitch}&key=${key}`, method: 'GET'});
 };
 
 /**
  * Get Street View image of a location from a location string
+ * @param {String} address Address or Name of location
  * @param {Number} width Width of image
  * @param {Number} height Height of image
- * @param {String} address Address or Name of location
  * @param {Number} fieldofview Field of View of image
  * @param {Number} heading Heading of view
  * @param {Number} pitch Pitch of view
  * @returns {Image} Image of requested location with specified size and orientation
  */
-GoogleStreetView.getViewFromAddress = function(width, height, location, fieldofview, heading, pitch) {
+GoogleStreetView.getViewFromAddress = function(location, width, height, fieldofview, heading, pitch) {
     return this._sendImage({queryString: `size=${width}x${height}&location=${location}&fov=${fieldofview}&heading=${heading}&pitch=${pitch}&key=${key}`, method: 'GET'});
 };
 
