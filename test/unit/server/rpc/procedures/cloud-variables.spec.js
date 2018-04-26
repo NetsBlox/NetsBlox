3cb0574258996e15c376210aa196f0320ba1b8a7
Brian Broll <brian.broll@vanderbilt.edu>
2018-04-26 14:22:50 -0500

    WIP #1542 added tests for cloud variable behavior

--- test/unit/server/rpc/procedures/cloud-variables.spec.js
+++ test/unit/server/rpc/procedures/cloud-variables.spec.js
@@ -3,6 +3,7 @@ describe('cloud-variables', function() {
     const CloudVariables = utils.reqSrc('rpc/procedures/cloud-variables/cloud-variables');
     const RPCMock = require('../../../../assets/mock-rpc');
     const cloudvariables = new RPCMock(CloudVariables);
+    const assert = require('assert');
 
     utils.verifyRPCInterfaces(cloudvariables, [
         ['getVariable', ['name', 'password']],
@@ -13,4 +14,88 @@ describe('cloud-variables', function() {
         ['deleteUserVariable', ['name']],
     ]);
 
+    let counter = 0;
+    function newVar() {
+        return `var${counter++}`;
+    }
+
+    before(() => utils.reset());
+
+    describe('public', function() {
+
+        it('should get/set variables', function() {
+            const name = newVar();
+            return cloudvariables.setVariable(name, 'world')
+                .then(() => cloudvariables.getVariable(name))
+                .then(result => assert.equal(result, 'world'));
+        });
+
+        it('should delete variables', function() {
+            const name = newVar();
+            return cloudvariables.setVariable(name, 'world')
+                .then(() => cloudvariables.deleteVariable(name))
+                .then(() => cloudvariables.getVariable(name))
+                .catch(err => assert(err.message.includes('not found')));
+        });
+
+        it('should not get/set variables w/ bad password', function() {
+            const name = newVar();
+            return cloudvariables.setVariable(name, 'world', 'password')
+                .then(() => cloudvariables.getVariable(name))
+                .catch(err => assert(err.message.includes('password')));
+        });
+
+        it('should not set variables w/ bad password', function() {
+            const name = newVar();
+            return cloudvariables.setVariable(name, 'world', 'password')
+                .then(() => cloudvariables.setVariable(name, 'asdf'))
+                .catch(err => assert(err.message.includes('password')));
+        });
+
+        it('should not delete variables w/ bad password', function() {
+            const name = newVar();
+            return cloudvariables.setVariable(name, 'world', 'password')
+                .then(() => cloudvariables.deleteVariable(name))
+                .catch(err => assert(err.message.includes('password')));
+        });
+    });
+
+    describe('user', function() {
+        beforeEach(function() {
+            cloudvariables.socket.loggedIn = true;
+            cloudvariables.socket.username = 'brian';
+        });
+
+        it('should get/set variables', function() {
+            const name = newVar();
+            return cloudvariables.setUserVariable(name, 'world')
+                .then(() => cloudvariables.getUserVariable(name))
+                .then(result => assert.equal(result, 'world'));
+        });
+
+        it('should delete variables', function() {
+            const name = newVar();
+            return cloudvariables.setUserVariable(name, 'world')
+                .then(() => cloudvariables.deleteUserVariable(name))
+                .then(() => cloudvariables.getUserVariable(name))
+                .catch(err => assert(err.message.includes('not found')));
+        });
+
+        it('should not set other user variables', function() {
+            const name = newVar();
+            return cloudvariables.setUserVariable(name, 'world')
+                .then(() => {
+                    cloudvariables.socket.username = 'hamid';
+                    return cloudvariables.getUserVariable(name);
+                })
+                .catch(err => assert(err.message.includes('not found')));
+        });
+
+        it('should not delete variables w/ bad password', function() {
+            const name = newVar();
+            return cloudvariables.setUserVariable(name, 'world')
+                .then(() => cloudvariables.deleteUserVariable(name))
+                .catch(err => assert(err.message.includes('password')));
+        });
+    });
 });
