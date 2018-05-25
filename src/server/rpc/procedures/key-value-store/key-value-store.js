0c293910512e63eef18ac556f9cbc71ac0db1ca1
Brian Broll <brian.broll@gmail.com>
2018-05-24 20:05:33 -0500

    Sanitize key name in KeyValueStore. Fixes #2058

--- src/server/rpc/procedures/key-value-store/key-value-store.js
+++ src/server/rpc/procedures/key-value-store/key-value-store.js
@@ -18,145 +18,156 @@ var saveStore = function(store) {
     return Storage.save(NAME, store);
 };
 
-var KeyValueStore = {
+const validateKeys = function(keys) {
+    const validName = /^[\w _()-]+$/;
+    keys.forEach(key => {
+        if (!validName.test(key)) {
+            throw new Error(`Invalid key: ${key}`);
+        }
+    });
+};
 
-    get: function(key) {
-        var keys = getKeys(key),
-            response = this.response,
-            i = 0;
+const KeyValueStore = {};
 
-        logger.trace(`getting key "${key}"`);
-        getStore()
-            .then(result => {
-                while (result && i < keys.length) {
-                    result = result[keys[i]];
-                    if (!result) {
-                        logger.warn(`invalid key: ${key} (get)`);
-                        return response.json(false);
-                    }
-                    i++;
-                }
+KeyValueStore.get = function(key) {
+    var keys = getKeys(key),
+        response = this.response,
+        i = 0;
 
-                if (typeof result === 'object') {
-                    if (Array.isArray(result)) {
-                        return response.json(result);
-                    }
-                    logger.warn(`invalid key: ${key} (get) -> key is an object`);
+    logger.trace(`getting key "${key}"`);
+    getStore()
+        .then(result => {
+            while (result && i < keys.length) {
+                result = result[keys[i]];
+                if (!result) {
+                    logger.warn(`invalid key: ${key} (get)`);
                     return response.json(false);
                 }
+                i++;
+            }
 
-                logger.trace(`retrieved value: ${key} -> ${result}`);
-                return response.json(result);
-            })
-            .catch(err => {
-                logger.error(`Could not retrieve key ${keys[0]}: ${err}`);
-                return response.json(false);
-            });
-
-        return null;
-    },
-
-    put: function(key, value) {
-        var keys = getKeys(key),
-            response = this.response;
-
-        logger.trace(`Looking up key "${key}"`);
-        getStore()
-            .then(store => {
-                var result = store,
-                    i = 0;
-
-                while (result && i < keys.length-1) {
-                    if (!result[keys[i]]) {  // create nonexistent keys
-                        result[keys[i]] = {};
-                    }
-                    result = result[keys[i]];
-                    i++;
+            if (typeof result === 'object') {
+                if (Array.isArray(result)) {
+                    return response.json(result);
                 }
-
-                result[keys[i]] = value;
-
-                logger.trace(`about to save ${JSON.stringify(store)}`);
-                return saveStore(store);
-            })
-            .then(result => {
-                logger.trace(`set "${key}" to "${value}"`);
-                return response.json(result);
-            })
-            .catch(err => {
-                logger.error(`Could not save key "${key}": ${err}`);
+                logger.warn(`invalid key: ${key} (get) -> key is an object`);
                 return response.json(false);
-            });
+            }
 
-        return null;
-    },
+            logger.trace(`retrieved value: ${key} -> ${result}`);
+            return response.json(result);
+        })
+        .catch(err => {
+            logger.error(`Could not retrieve key ${keys[0]}: ${err}`);
+            return response.json(false);
+        });
 
-    delete: function(key) {
-        var keys = getKeys(key),
-            response = this.response,
-            i = 0;
+    return null;
+};
 
-        getStore()
-            .then(result => {
-                while (result && i < keys.length-1) {
-                    result = result[keys[i]];
-                    if (!result) {
-                        logger.warn(`invalid key: ${key} (delete)`);
-                        return response.json(false);
-                    }
-                    i++;
+KeyValueStore.put = function(key, value) {
+    // Validate the variable names
+    // TODO
+    var keys = getKeys(key),
+        response = this.response;
+
+    validateKeys(keys);
+    logger.trace(`Looking up key "${key}"`);
+    getStore()
+        .then(store => {
+            var result = store,
+                i = 0;
+
+            while (result && i < keys.length-1) {
+                if (!result[keys[i]]) {  // create nonexistent keys
+                    result[keys[i]] = {};
                 }
+                result = result[keys[i]];
+                i++;
+            }
 
-                if (typeof result !== 'object') {
+            result[keys[i]] = value;
+
+            logger.trace(`about to save ${JSON.stringify(store)}`);
+            return saveStore(store);
+        })
+        .then(result => {
+            logger.trace(`set "${key}" to "${value}"`);
+            return response.json(result);
+        })
+        .catch(err => {
+            logger.error(`Could not save key "${key}": ${err}`);
+            return response.json(false);
+        });
+
+    return null;
+};
+
+KeyValueStore.delete = function(key) {
+    var keys = getKeys(key),
+        response = this.response,
+        i = 0;
+
+    getStore()
+        .then(result => {
+            while (result && i < keys.length-1) {
+                result = result[keys[i]];
+                if (!result) {
                     logger.warn(`invalid key: ${key} (delete)`);
                     return response.json(false);
                 }
+                i++;
+            }
 
-                delete result[keys[i]];
-                logger.trace(`successfully removed key ${key}`);
-                return saveStore(result);
-            })
-            .then(() => response.json(true))
-            .catch(e => logger.error(`deleting ${key} failed: ${e}`));
+            if (typeof result !== 'object') {
+                logger.warn(`invalid key: ${key} (delete)`);
+                return response.json(false);
+            }
 
-        return null;
-    },
+            delete result[keys[i]];
+            logger.trace(`successfully removed key ${key}`);
+            return saveStore(result);
+        })
+        .then(() => response.json(true))
+        .catch(e => logger.error(`deleting ${key} failed: ${e}`));
 
-    parent: function(key) {
-        var keys = getKeys(key);
+    return null;
+};
 
-        if (keys.length) {
-            keys.pop();
-        }
-        
-        return '/' + keys.join(SEP);
-    },
+KeyValueStore.parent = function(key) {
+    var keys = getKeys(key);
 
-    child: function(key) {
-        var keys = getKeys(key),
-            response = this.response,
-            i = 0;
-
-        getStore()
-            .then(result => {
-                while (result && i < keys.length) {
-                    result = result[keys[i]];
-                    if (typeof result !== 'object') {
-                        logger.warn(`invalid key: "${key}" (child)`);
-                        return response.json([]);
-                    }
-                    i++;
-                }
-                return response.json(
-                    Object.keys(result)
-                        .sort()
-                        .map(k => key + '/' + k)
-                );
-            })
-            .catch(e => logger.error(`getting children failed: ${e}`));
-
-        return null;
+    if (keys.length) {
+        keys.pop();
     }
+    
+    return '/' + keys.join(SEP);
+};
+
+KeyValueStore.child = function(key) {
+    var keys = getKeys(key),
+        response = this.response,
+        i = 0;
+
+    getStore()
+        .then(result => {
+            while (result && i < keys.length) {
+                result = result[keys[i]];
+                if (typeof result !== 'object') {
+                    logger.warn(`invalid key: "${key}" (child)`);
+                    return response.json([]);
+                }
+                i++;
+            }
+            return response.json(
+                Object.keys(result)
+                    .sort()
+                    .map(k => key + '/' + k)
+            );
+        })
+        .catch(e => logger.error(`getting children failed: ${e}`));
+
+    return null;
 };
 
 KeyValueStore.COMPATIBILITY = {
