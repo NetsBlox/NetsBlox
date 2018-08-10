4134f12a0f607879e6709d7875e48a8d00832230
Hamid Zare <dellydela@gmail.com>
2018-08-10 11:07:35 -0500

    improved memberIsnew middleware conditions

--- src/server/routes/middleware.js
+++ src/server/routes/middleware.js
@@ -188,12 +188,22 @@ let isValidMember = async function(req, res, next) {
 // requires a validmember
 let memberIsNew = async function(req, res, next) {
     // TODO add optional bypass
+    let issues = [];
     let userId = req.params.userId;
     let user = await server.storage.users.getById(userId);
     // condition #1: must have no saved or transient project
     let projects = await user.getAllRawProjects();
-    if (projects.length !== 0) return res.status(403).send('user is not new');
+    if (projects.length !== 0) issues.push('user has projects');
 
+    // condition #2: account age
+    let age = (new Date().getTime() - user.createdAt.getTime()) / 60000 ; // in minutes
+    const AGE_LIMIT_MINUTES = 60 * 24 * 7; // a week
+    if (age > AGE_LIMIT_MINUTES) issues.push('you cannot change an old account');
+
+    if (issues.length > 0) {
+        let msg = issues.join('& ');
+        return res.status(403).send(msg);
+    }
     next();
 };
 
