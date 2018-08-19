bbe44b7c7f0da8b25b488e808a410b6c1a332036
Brian Broll <brian.broll@vanderbilt.edu>
2018-08-19 15:09:53 -0500

    Dont rely on the ws conn for public role id. Fixes #2219 (#2220)
    * Dont rely on the ws conn for public role id. Fixes #2219
    
    * WIP added more logs and fixed linting issues

--- src/server/rpc/procedures/public-roles/public-roles.js
+++ src/server/rpc/procedures/public-roles/public-roles.js
@@ -2,16 +2,29 @@
 'use strict';
 
 const logger = require('../utils/logger')('public-roles');
+const Projects = require('../../../storage/projects');
 const PublicRoles = {};
 
 /**
  * Get the public role ID for the current role.
  */
 PublicRoles.getPublicRoleId = function() {
-    return this.socket.getPublicId()
-        .catch(err => {
-            logger.error(`Could not get public role ID for ${this.socket.uuid}: ${err}`);
-            throw err;
+    const {projectId, roleId, clientId} = this.caller;
+    return Projects.getRawProjectById(projectId)
+        .then(metadata => {
+            if (!metadata) {
+                logger.warn(`cannot get public id for ${clientId} project ${projectId} not found.`);
+                throw new Error('Project not found. Has it been deleted?');
+            }
+
+            if (!metadata.roles[roleId]) {
+                logger.warn(`cannot get public id for ${clientId} role ${roleId} at ${projectId} not found.`);
+                throw new Error('Role not found. Has it been deleted?');
+            }
+
+            const roleName = metadata.roles[roleId].ProjectName;
+            const {name, owner} = metadata;
+            return `${roleName}@${name}@${owner}`;
         });
 };
 
