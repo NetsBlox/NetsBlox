58159b19d0f1d5a23eba59bf7285eca69160f44a
Brian Broll <brian.broll@vanderbilt.edu>
2018-08-19 13:22:52 -0500

    Updated logger creation in API Consumer. Fixes #2215 (#2216)
    * Updated logger creation in API Consumer. Fixes #2215
    
    * WIP minor refactor
    
    * WIP renamed factory method

--- src/server/rpc/procedures/utils/api-consumer.js
+++ src/server/rpc/procedures/utils/api-consumer.js
@@ -1,4 +1,4 @@
-const Logger = require('../../../logger'),
+const newLogger = require('./logger'),
     CacheManager = require('cache-manager'),
     fsStore = require('cache-manager-fs'),
     fs = require('fs'),
@@ -24,7 +24,7 @@ class ApiConsumer {
         },opts);
         if (!fs.existsSync(opts.cache.path)) fs.mkdirSync(opts.cache.path);
         this._baseUrl = baseUrl;
-        this._logger = new Logger('netsblox:rpc:'+this._name);
+        this._logger = newLogger(this._name);
         // setup api endpoint
         this.COMPATIBILITY = {
             path: this._name
