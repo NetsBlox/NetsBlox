b64b37fa30970861a9593e8f2b863b256b13acd9
Hamid Zare <hamidzr@users.noreply.github.com>
2018-10-17 10:00:29 -0500

    support for using external SMTP server for outgoing email. Fixes #2254 (#2255)
    * if available use the configured smtp server for sending  email
    
    * just init transporterOpts

--- src/server/mailer.js
+++ src/server/mailer.js
@@ -1,5 +1,28 @@
-var nodemailer = require('nodemailer'),
-    transporter = nodemailer.createTransport();
+var nodemailer = require('nodemailer');
+
+let transporterOpts;
+
+const hasSecureSMTPConf = () => {
+    const vars = ['HOST', 'PORT', 'SECURE', 'USER', 'PASS'];
+    vars.forEach(envVar => {
+        if (!process.env.hasOwnProperty('SMTP_' + envVar)) return false;
+    });
+    return true;
+};
+
+if (hasSecureSMTPConf()) {
+    transporterOpts = {
+        host: process.env.SMTP_HOST,
+        port: process.env.SMTP_PORT,
+        secure: process.env.SMTP_SECURE, // true for 465, false for other ports
+        auth: {
+            user: process.env.SMTP_USER,
+            pass:  process.env.SMTP_PASS
+        }
+    };
+}
+
+const transporter = nodemailer.createTransport(transporterOpts);
 
 const getSenderDomain = function(host) {
     return host.replace(/^\./, '');  // remove leading '.'
