444ce091244327f4e8b678073282cfbac0df00b2
Hamid Zare <dellydela@gmail.com>
2018-11-17 15:21:06 -0600

    fixed project export issue (old activeroom)

--- bin/netsblox-export-project.js
+++ bin/netsblox-export-project.js
@@ -2,14 +2,12 @@
 require('epipebomb')();  // Allow piping to 'head'
 
 var Command = require('commander').Command,
-    Storage = require('../src/server/storage/storage'),
     Logger = require('../src/server/logger'),
     Projects = require('../src/server/storage/projects'),
-    ActiveRoom = require('../src/server/rooms/active-room'),
     logger = new Logger('netsblox:cli:export'),
     fs = require('fs'),
-    storage = new Storage(logger),
-    program = new Command();
+    program = new Command(),
+    runWithStorage = require('./utils').runWithStorage;
 
 program
     .arguments('<owner> <project>')
@@ -21,29 +19,22 @@ if (program.args.length !== 2) {
 }
 
 var [owner, name] = program.args;
-storage.connect()
-    .then(() => {
-        logger.trace(`About to get project: ${owner} ${name}`);
 
-        return Projects.get(owner, name);
-    })
-    .then(project => {
-        if (!project) {
-            console.error(`project not found`);
-            return storage.disconnect();
-        }
-        logger.trace('About to export project');
-        return ActiveRoom.fromStore(logger, project);
-    })
-    .then(room => room.serialize())
-    .then(xml => {
-        var filename = `${owner}-${name}.xml`;
-        fs.writeFileSync(filename, xml);
-        console.log(`exported project to ${filename}`);
-    })
-    .then(() => storage.disconnect())
-    .catch(err => {
-        console.error(err);
-        return storage.disconnect();
-    });
-/* eslint-enable no-console*/
+async function exportProject(owner, projectName) {
+    logger.trace(`About to get project: ${owner} ${projectName}`);
+    const project = await Projects.get(owner, projectName);
+    if (!project) {
+        console.error(`project not found`);
+        return;
+    }
+    logger.trace('About to export project');
+    console.log('loaded project');
+    const xml = await project.toXML();
+    const filename = `${owner}-${projectName}.xml`;
+    fs.writeFileSync(filename, xml);
+    console.log(`exported project to ${filename}`);
+}
+
+
+runWithStorage(exportProject, [owner, name]);
+// /* eslint-enable no-console*/
