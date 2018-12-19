ea01529c99adf0093d7afedef33d7dccfd73c57e
Hamid Zare <dellydela@gmail.com>
2018-12-19 10:14:30 -0600

    updated the test script to spawn and handle the server if needed

--- bin/test-client.sh
+++ bin/test-client.sh
@@ -2,6 +2,18 @@
 
 # runs the client tests in a headless chrome
 
-# TODO make sure the server is up and running before starting the client tests
+# check if server is currently running: get the pid
+server_pid=$(netstat -nltp 2>/dev/null | grep 8080 | head -n 1 | grep -Eo '[0-9]+/node' | cut -d '/' -f 1)
 
-npx mocha-chrome http://localhost:8080/test --chrome-flags '["--no-sandbox"]'
+# only start and stop the server if it's not up already
+if [ -z $server_pid ]; then
+  echo "starting the server..."
+  npm start >/dev/null &
+  # TODO loop while server hasn't started listening yet
+  sleep 5
+  npx mocha-chrome http://localhost:8080/test --chrome-flags '["--no-sandbox"]'
+  kill %1
+else
+  echo "server is already up with pid '$server_pid'"
+  npx mocha-chrome http://localhost:8080/test --chrome-flags '["--no-sandbox"]'
+fi
