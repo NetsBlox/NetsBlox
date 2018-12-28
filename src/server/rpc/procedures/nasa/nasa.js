9c5aca8ab0356808b0cb2ec7c6d9e2f568df6bf2
Hamid Zare <dellydela@gmail.com>
2018-12-28 14:43:42 -0600

    WIP updating service to use promises (better error handling)

--- src/server/rpc/procedures/nasa/nasa.js
+++ src/server/rpc/procedures/nasa/nasa.js
@@ -8,6 +8,7 @@
 'use strict';
 
 var request = require('request'),
+    axios = require('axios'),
     KEY = process.env.NASA_KEY,
     APOD_URL = 'https://api.nasa.gov/planetary/apod?api_key=' + KEY,
     MARS_URL = 'http://marsweather.ingenology.com/v1/latest/';
@@ -36,45 +37,28 @@ module.exports = {
         return null;
     },
 
-    // NASA's 'Astronomy Picture of the Day' media
-    apodMedia: function() {
-        var response = this.response;
-
-        request(APOD_URL, function(err, res, body) {
-            body = JSON.parse(body);
-            request.get(body.url).pipe(response);
-        });
-        return null;
+    /**
+     * NASA's 'Astronomy Picture of the Day' media
+     * @returns {String}
+     */
+    apodMedia: async function() {
+        let { data: body } = await axios.get(APOD_URL);
+        return body.url;
     },
 
     // Latest Mars data according to MAAS
-    marsHighTemp: function() {
-        var response = this.response;
-
-        request(MARS_URL, function(err, res, body) {
-            body = JSON.parse(body);
-            return response.json(body.report.max_temp_fahrenheit);
-        });
-        return null;
+    marsHighTemp: async function() {
+        let { data: body } = await axios.get(MARS_URL);
+        return body.report.max_temp_fahrenheit;
     },
 
-    marsLowTemp: function() {
-        var response = this.response;
-
-        request(MARS_URL, function(err, res, body) {
-            body = JSON.parse(body);
-            return response.json(body.report.min_temp_fahrenheit);
-        });
-        return null;
+    marsLowTemp: async function() {
+        let { data: body } = await axios.get(MARS_URL);
+        return body.report.min_temp_fahrenheit;
     },
 
-    marsWeather: function() {
-        var response = this.response;
-
-        request(MARS_URL, function(err, res, body) {
-            body = JSON.parse(body);
-            return response.json(body.report.atmo_opacity);
-        });
-        return null;
+    marsWeather: async function() {
+        let { data: body } = await axios.get(MARS_URL);
+        return body.report.atmo_opacity;
     }
 };
