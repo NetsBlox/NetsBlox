73938268b8128a41f66a5d8941929b18e0edede4
Hamid Zare <dellydela@gmail.com>
2019-01-14 12:50:40 -0600

    better vantage control

--- bin/netsblox-start.js
+++ bin/netsblox-start.js
@@ -6,12 +6,12 @@ require('dotenv').load({
 });
 
 var express = require('express'),
-    app = express(),
-    port,
-    vport;
+    app = express();
 
-port = process.env.PORT || 8080;
-vport = process.env.VANTAGE_PORT || 1234;
+const PORT = process.env.PORT || 8080;
+const VANTAGE_PORT = process.env.VANTAGE_PORT || 1234;
+const ENV = process.env.ENV;
+const isDevMode = ENV !== 'production';
 
 app.use(express.static(__dirname + '/client/'));
 
@@ -19,11 +19,20 @@ app.get('/', function(req, res) {
     res.redirect('/snap.html');
 });
 
+
+function isVantageEnabled() {
+    const VANTAGE_ENABLED = process.env.VANTAGE_ENABLED === 'true';
+    if (ENV === 'production') return false;
+    if (VANTAGE_ENABLED !== undefined) return VANTAGE_ENABLED;
+    if (VANTAGE_PORT) return true;
+    return isDevMode;
+}
+
 // Set the group manager
 var opts = {
-    port: port,
-    vantagePort: vport,
-    vantage: process.env.ENV !== 'production'
+    port: PORT,
+    vantagePort: VANTAGE_PORT,
+    vantage: isVantageEnabled(),
 };
 
 var Server = require('../src/server/server'),
