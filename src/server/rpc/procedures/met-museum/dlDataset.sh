b58fa6fdd1c4ebf926ebdb0b4e313516b6236c9b
Hamid Zare <dellydela@gmail.com>
2019-01-16 17:33:26 -0600

    renamed and chaged the target for metobjects dataset

---
+++ src/server/rpc/procedures/met-museum/dlDataset.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+wget https://media.githubusercontent.com/media/metmuseum/openaccess/master/MetObjects.csv -O /tmp/metobjects.csv
--- src/server/rpc/procedures/met-museum/dlSet.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-wget https://media.githubusercontent.com/media/metmuseum/openaccess/master/MetObjects.csv -O metobjects.csv
