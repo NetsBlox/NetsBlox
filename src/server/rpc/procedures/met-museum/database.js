b267f39425f968521a154044e0493fa3f00d80cc
Hamid Zare <dellydela@gmail.com>
2019-01-17 15:38:00 -0600

    added search rpcs

--- src/server/rpc/procedures/met-museum/database.js
+++ src/server/rpc/procedures/met-museum/database.js
@@ -1,21 +1,21 @@
 const mongoose = require('mongoose');
 const fs = require('fs');
 
-const MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost/nb-services';
+const MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost/netsblox';
 
 mongoose.connect(MONGO_URI, { useNewUrlParser: true });
 
 
 const schemaDef = {};
-const headers = fs.readFileSync('./metobjects.headers', {encoding: 'utf8'})
+const headers = fs.readFileSync(__dirname + '/metobjects.headers', {encoding: 'utf8'})
     .trim()
     .split(',');
 headers.forEach(attr => {
-  schemaDef[attr] = String;
-})
+    schemaDef[attr] = String;
+});
 
 const schema = new mongoose.Schema(schemaDef);
 
-const MetObject = mongoose.model('met-objects', schema);
+const MetObject = mongoose.model('netsblox:service:metmuseum', schema);
 
 module.exports = MetObject;
--- src/server/rpc/procedures/met-museum/met-museum.js
+++ src/server/rpc/procedures/met-museum/met-museum.js
@@ -3,10 +3,46 @@
  * @service
  */
 
+const fs = require('fs');
+const MetObject = require('./database.js');
 const ApiConsumer = require('../utils/api-consumer');
 const { JSDOM } = require('jsdom');
-const MetMuseum = new ApiConsumer('MetMuseum', 'https://www.metmuseum.org/art/collection/search', {cache: {ttl: 5*60}});
+const MetMuseum = new ApiConsumer('metmuseum', 'https://www.metmuseum.org/art/collection/search', {cache: {ttl: 5*60}});
 
+
+
+const headers = fs.readFileSync(__dirname + '/metobjects.headers', {encoding: 'utf8'})
+    .trim()
+    .split(',');
+
+MetMuseum.fields = function() {
+    return headers;
+};
+
+
+/**
+ * searches the met museum!
+ * @param {String} field blah
+ * @param {String} query blah
+ * @param {Number=} limit blah
+ * @returns {Array} results
+ */
+MetMuseum.search = async function(field, query, limit=10) {
+    if (!headers.find(attr => attr === field)) throw new Error('bad field name');
+    limit = Math.min(limit, 50); // limit the max requested documents
+
+    let dbQuery = {};
+    dbQuery[field] = new RegExp(`.*${query}.*`, 'i');
+
+    let res = await MetObject.find(dbQuery).limit(limit);
+    res = res.map(rec => {
+        delete rec._doc._id;
+        delete rec._doc.__v;
+        return rec._doc;
+    });
+
+    return res;
+};
 
 
 /**
