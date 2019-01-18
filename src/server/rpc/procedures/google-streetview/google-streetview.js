c50766d8dfe26d6847d1a0750afc623fe02c8ace
Hamid Zare <hamidzr@users.noreply.github.com>
2019-01-18 16:29:00 -0600

    streetview metadata rpcs fixes #2332 (#2333)
    * renamed mismatching jsdoc block param name
    
    * change the base endpoint url to allow more flexibility
    
    * added metadata information rpcs
    
    * shortened the helpdoc; renamed metadata to info
    
    * added a simplified rpc to check for availability of the imagery
    
    * removed width and height from info params
    
    * removed latlong from the rpc name
    
    * added availability check using address

--- src/server/rpc/procedures/google-streetview/google-streetview.js
+++ src/server/rpc/procedures/google-streetview/google-streetview.js
@@ -10,7 +10,7 @@
 var key = process.env.GOOGLE_MAPS_KEY;
 
 const ApiConsumer = require('../utils/api-consumer');
-const GoogleStreetView = new ApiConsumer('google-streetview', 'https://maps.googleapis.com/maps/api/streetview?',{cache: {ttl: 7*24*60*60}});
+const GoogleStreetView = new ApiConsumer('google-streetview', 'https://maps.googleapis.com/maps/api/streetview',{cache: {ttl: 7*24*60*60}});
 
 GoogleStreetView.isSupported = () => {
     if(!key){
@@ -19,6 +19,23 @@ GoogleStreetView.isSupported = () => {
         /* eslint-enable no-console*/
     }
     return !!key;
+};
+
+
+/**
+ * Get Street View image of a location using coordinates
+ * @param {Latitude} latitude Latitude coordinate of location
+ * @param {Longitude} longitude Longitude coordinate of location
+ * @param {BoundedNumber<1,2000>} width Width of image
+ * @param {BoundedNumber<1,2000>} height Height of image
+ * @param {BoundedNumber<1,120>} fieldofview Field of View of image, maximum of 120
+ * @param {BoundedNumber<0,360>} heading Heading of view
+ * @param {BoundedNumber<-90,90>} pitch Pitch of view, 90 to point up, -90 to point down
+ * @returns {Image} Image of requested location with specified size and orientation
+ * @deprecated
+ */
+GoogleStreetView.getViewFromLatLong = function(latitude, longitude, width, height, fieldofview, heading, pitch) {
+    return this._sendImage({queryString: `?size=${width}x${height}&location=${latitude},${longitude}&fov=${fieldofview}&heading=${heading}&pitch=${pitch}&key=${key}`, method: 'GET'});
 };
 
 /**
@@ -32,13 +49,14 @@ GoogleStreetView.isSupported = () => {
  * @param {BoundedNumber<-90,90>} pitch Pitch of view, 90 to point up, -90 to point down
  * @returns {Image} Image of requested location with specified size and orientation
  */
-GoogleStreetView.getViewFromLatLong = function(latitude, longitude, width, height, fieldofview, heading, pitch) {
-    return this._sendImage({queryString: `size=${width}x${height}&location=${latitude},${longitude}&fov=${fieldofview}&heading=${heading}&pitch=${pitch}&key=${key}`, method: 'GET'});
+GoogleStreetView.getView = function(latitude, longitude, width, height, fieldofview, heading, pitch) {
+    return this._sendImage({queryString: `?size=${width}x${height}&location=${latitude},${longitude}&fov=${fieldofview}&heading=${heading}&pitch=${pitch}&key=${key}`, method: 'GET'});
 };
+
 
 /**
  * Get Street View image of a location from a location string
- * @param {String} address Address or Name of location
+ * @param {String} location Address or Name of location
  * @param {BoundedNumber<1,2000>} width Width of image
  * @param {BoundedNumber<1,2000>} height Height of image
  * @param {BoundedNumber<1,120>} fieldofview Field of View of image, maximum of 120
@@ -47,8 +65,82 @@ GoogleStreetView.getViewFromLatLong = function(latitude, longitude, width, heigh
  * @returns {Image} Image of requested location with specified size and orientation
  */
 GoogleStreetView.getViewFromAddress = function(location, width, height, fieldofview, heading, pitch) {
-    return this._sendImage({queryString: `size=${width}x${height}&location=${location}&fov=${fieldofview}&heading=${heading}&pitch=${pitch}&key=${key}`, method: 'GET'});
+    return this._sendImage({queryString: `?size=${width}x${height}&location=${location}&fov=${fieldofview}&heading=${heading}&pitch=${pitch}&key=${key}`, method: 'GET'});
 };
+
+
+/**
+ * Get Street View metadata of a location using coordinates.
+ * Status explanation: "OK": No errors occurred.
+ * "ZERO_RESULTS": No image could be found near the provided location.
+ * "NOT_FOUND": The location provided could not be found.
+ * @param {Latitude} latitude Latitude coordinate of location
+ * @param {Longitude} longitude Longitude coordinate of location
+ * @param {BoundedNumber<1,120>} fieldofview Field of View of image, maximum of 120
+ * @param {BoundedNumber<0,360>} heading Heading of view
+ * @param {BoundedNumber<-90,90>} pitch Pitch of view, 90 to point up, -90 to point down
+ * @returns {Object} Metadata infromation about the requested Street View.
+ */
+GoogleStreetView.getInfo = function(latitude, longitude, width, height, fieldofview, heading, pitch) {
+    const queryOpts = {
+        queryString: `/metadata?location=${latitude},${longitude}&fov=${fieldofview}&heading=${heading}&pitch=${pitch}&key=${key}`
+    };
+    const parserFn = resp => resp; // explicitly do nothing
+    return this._sendStruct(queryOpts, parserFn);
+};
+
+/**
+ * Get Street View metadata of a location using a location query.
+ * Status explanation: "OK": No errors occurred.
+ * "ZERO_RESULTS": No image could be found near the provided location.
+ * "NOT_FOUND": The location provided could not be found.
+ * @param {String} location Address or Name of location
+ * @param {BoundedNumber<1,120>} fieldofview Field of View of image, maximum of 120
+ * @param {BoundedNumber<0,360>} heading Heading of view
+ * @param {BoundedNumber<-90,90>} pitch Pitch of view, 90 to point up, -90 to point down
+ * @returns {Object} Metadata infromation about the requested Street View.
+ */
+GoogleStreetView.getInfoFromAddress = function(location, width, height, fieldofview, heading, pitch) {
+    const queryOpts = {
+        queryString: `/metadata?location=${location}&fov=${fieldofview}&heading=${heading}&pitch=${pitch}&key=${key}`
+    };
+    const parserFn = resp => resp; // explicitly do nothing
+    return this._sendStruct(queryOpts, parserFn);
+};
+
+/**
+ * Check for availability of imagery at a location using coordinates
+ * @param {Latitude} latitude Latitude coordinate of location
+ * @param {Longitude} longitude Longitude coordinate of location
+ * @param {BoundedNumber<1,120>} fieldofview Field of View of image, maximum of 120
+ * @param {BoundedNumber<0,360>} heading Heading of view
+ * @param {BoundedNumber<-90,90>} pitch Pitch of view, 90 to point up, -90 to point down
+ * @returns {Boolean}
+ */
+GoogleStreetView.isAvailable = function(latitude, longitude, fieldofview, heading, pitch) {
+    const queryOpts = {
+        queryString: `/metadata?location=${latitude},${longitude}&fov=${fieldofview}&heading=${heading}&pitch=${pitch}&key=${key}`
+    };
+    const parserFn = resp => resp.status === 'OK';
+    return this._sendStruct(queryOpts, parserFn);
+};
+
+/**
+ * Check for availability of imagery at a location using an address
+ * @param {String} location Address or Name of location
+ * @param {BoundedNumber<1,120>} fieldofview Field of View of image, maximum of 120
+ * @param {BoundedNumber<0,360>} heading Heading of view
+ * @param {BoundedNumber<-90,90>} pitch Pitch of view, 90 to point up, -90 to point down
+ * @returns {Boolean}
+ */
+GoogleStreetView.isAvailableFromAddress = function(location, fieldofview, heading, pitch) {
+    const queryOpts = {
+        queryString: `/metadata?location=${location}&fov=${fieldofview}&heading=${heading}&pitch=${pitch}&key=${key}`
+    };
+    const parserFn = resp => resp.status === 'OK';
+    return this._sendStruct(queryOpts, parserFn);
+};
+
 
 GoogleStreetView.serviceName = 'GoogleStreetView';
 
