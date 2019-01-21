988905c098e2560278df9b9d426a43058efa4086
Hamid Zare <dellydela@gmail.com>
2019-01-21 16:00:31 -0600

    Revert "if the attachment is too big try compressing the contents"
    This reverts commit ccd312b8e4fa0fbc72c94df372418db6552bc133.

--- src/server/bug-reporter.js
+++ src/server/bug-reporter.js
@@ -1,7 +1,5 @@
 const mailer = require('./mailer');
 const Logger = require('./logger');
-const Buffer = require('buffer').Buffer;
-const zlib = require('zlib');
 const logger = new Logger('netsblox:bug-reporter');
 const Users = require('./storage/users');
 const version = require('./server-utils').version;
@@ -145,13 +143,6 @@ BugReporter.prototype.getUserEmail = function(username) {
         });
 };
 
-const strSize = s => {
-    const bytes = s => {
-        return ~-encodeURI(s).split(/%..|./).length;
-    };
-    return bytes(JSON.stringify(s)) / 1e+6; // in ~ mb
-};
-
 BugReporter.prototype.reportBug = function(subject, body, data) {
     // email this to the maintainer
     // Add server version?
@@ -169,16 +160,7 @@ BugReporter.prototype.reportBug = function(subject, body, data) {
     if (this.maintainer) {
         subject += ` (${version})`;
         body += `\n\nNetsBlox Server ${version}`;
-
-        const contentStr = JSON.stringify(data.content);
-        if (strSize(contentStr) > 5) {
-            logger.trace('bug report content too big, compressing..');
-            let input = new Buffer(contentStr);
-            data.content = zlib.deflate(input);
-        } else {
-            data.content = contentStr;
-        }
-
+        data.content = JSON.stringify(data.content);
         const mailOpts = {
             from: 'bug-reporter',
             to: this.maintainer,
