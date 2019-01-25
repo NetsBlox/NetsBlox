bf049eb1c1631d69d7f218d5cb246b3ace26fe49
Hamid Zare <hamidzr@users.noreply.github.com>
2019-01-25 11:30:59 -0600

    skip middlewares for preflight OPTIONS fixes #2166 (#2198)
    * preflight OPTIONS should passthrough middleswares
    
    * factored out route loading to shorten createRouter fn (codeclimate)

--- src/server/server.js
+++ src/server/server.js
@@ -330,13 +330,9 @@ Server.prototype.stop = function(done) {
     this._server.close(done);
 };
 
-Server.prototype.createRouter = function() {
-    var router = express.Router({mergeParams: true}),
-        logger = this._logger.fork('api'),
-        routes;
-
-    // Load the routes from routes/
-    routes = fs.readdirSync(path.join(__dirname, 'routes'))
+// Load the routes from routes/ dir
+function loadRoutes(logger) {
+    const routes = fs.readdirSync(path.join(__dirname, 'routes'))
         .filter(name => path.extname(name) === '.js')  // Only read js files
         .filter(name => name !== 'middleware.js')  // ignore middleware file
         .map(name => __dirname + '/routes/' + name)  // Create the file path
@@ -345,6 +341,14 @@ Server.prototype.createRouter = function() {
             return require(filePath);
         })  // Load the routes
         .reduce((prev, next) => prev.concat(next), []);  // Merge all routes
+    return routes;
+}
+
+Server.prototype.createRouter = function() {
+    var router = express.Router({mergeParams: true}),
+        logger = this._logger.fork('api');
+
+    const routes = loadRoutes(logger);
 
     middleware.init(this);
 
@@ -356,7 +360,10 @@ Server.prototype.createRouter = function() {
         // Add the middleware
         if (api.middleware) {
             logger.trace(`adding "${method}" to ${api.URL}`);
-            var args = api.middleware.map(name => middleware[name]);
+            var args = api.middleware.map(name => (req, res, next) => {
+                if (req.method === 'OPTIONS') return next();
+                return middleware[name](req, res, next);
+            });
             args.unshift(api.URL);
             router.use.apply(router, args);
         }
