4df75a08b13e7c7abcfe0c5f99f9c4c14508336a
Hamid Zare <dellydela@gmail.com>
2019-01-25 12:08:59 -0600

    removed nolonger necessary options skips

--- src/server/routes/middleware.js
+++ src/server/routes/middleware.js
@@ -128,7 +128,6 @@ function login(req, res) {
 
 var isLoggedIn = function(req, res, next) {
     logger.trace(`checking if logged in ${Object.keys(req.cookies)}`);
-    if (req.method === 'OPTIONS') return next();
     tryLogIn(req, res, (err, success) => {
         if (err) {
             return next(err);
@@ -219,7 +218,6 @@ var setUser = function(req, res, next) {
 
 // check group write access
 let isGroupOwner = function(req, res, next) {
-    if (req.method === 'OPTIONS') return next();
     let groupId = req.params.id;
     Groups.get(groupId)
         .then( group => {
