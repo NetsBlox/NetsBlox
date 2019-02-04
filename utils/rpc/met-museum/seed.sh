df8b9346c7af91436d34404ebbf10db6fe849f82
Hamid Zare <dellydela@gmail.com>
2019-02-04 15:27:18 -0600

    moving metmuseum to rely on csv-toolset

--- utils/rpc/met-museum/seed.sh
+++ utils/rpc/met-museum/seed.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-./src/server/rpc/procedures/met-museum/dlDataset.sh
-node ./src/server/rpc/procedures/met-museum/prepare.js /tmp/metobjects.csv
+node ./src/server/rpc/procedures/met-museum/seed.js
