83d4c663a93eed57fc7b1e8b435e06324a9e5b25
Hamid Zare <dellydela@gmail.com>
2019-02-05 17:37:18 -0600

    added ispublic attribute; make user properties required

--- src/server/rpc/procedures/roboscape/database.js
+++ src/server/rpc/procedures/roboscape/database.js
@@ -2,13 +2,14 @@ const getServiceStorage = require('../../advancedStorage');
 
 const schemaDef = {
     robotId: {type: String, required: true},
+    isPublic: {type: Boolean, default: false},
     owner: {type: String, required: true}, // username of the owner
     ownedAt: {type: Date, required: true}, // last date the user proved ownership of the robot
     users: [ // users that ever had access to the robot
         {
-            username: String,
-            hasAccess: Boolean,
-            updatedAt: Date,
+            username: {type: String, required: true},
+            hasAccess: {type: Boolean, required: true},
+            updatedAt: {type: Date, required: true},
         }
     ]
 };
