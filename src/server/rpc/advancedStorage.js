cbd2e448c8ac86ff8edb81be5e5cfea1f0232bdc
Hamid Zare <dellydela@gmail.com>
2019-02-05 13:55:25 -0600

    SEP changed the default advanced storage URI to be consistent with other db connections. added logger

--- src/server/rpc/advancedStorage.js
+++ src/server/rpc/advancedStorage.js
@@ -1,11 +1,14 @@
 const mongoose = require('mongoose');
+const Logger = require('../logger');
+const logger = new Logger('netsblox:storage:services');
 const { isValidServiceName } = require('./procedures/utils');
-const MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost/netsblox';
+const MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost:27017/admin'; // probably should mention netsblox instead of admin
 mongoose.connect(MONGO_URI, { useNewUrlParser: true });
 
 // this could probably have a better name
 // creates a mongoose model for a service
 function getServiceStorage(serviceName, schemaDef) {
+    logger.trace(`setting up storage for "${serviceName}"`);
     const schema = new mongoose.Schema(schemaDef);
     if (!isValidServiceName(serviceName)) throw new Error('invalid service name');
     const model = mongoose.model(`netsblox:services:${serviceName}`, schema);
