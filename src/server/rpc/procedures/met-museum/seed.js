9d948178456ea39cccf58dbea792d682aee8c21d
Hamid Zare <dellydela@gmail.com>
2019-02-06 15:24:05 -0600

    renamed csv-toolset to csv loader

--- src/server/rpc/procedures/met-museum/seed.js
+++ src/server/rpc/procedures/met-museum/seed.js
@@ -1,6 +1,6 @@
 #!/bin/env node
 
-const seeder = require('../utils/csv-toolset');
+const seeder = require('../utils/csv-loader');
 const MetMuseumCol = require('./database');
 
 seeder(MetMuseumCol, {url: 'https://media.githubusercontent.com/media/metmuseum/openaccess/master/MetObjects.csv'});
