a11a1fbed100b458c99dbe04cf8d950873f72beb
Hamid Zare <dellydela@gmail.com>
2019-02-06 14:09:56 -0600

    WIP creating roboscape routes for the dashboard and app registration

--- src/server/rpc/procedures/roboscape/routes.js
+++ src/server/rpc/procedures/roboscape/routes.js
@@ -3,15 +3,67 @@
 'use strict';
 
 const logger = require('../utils/logger')('roboscape:routes');
+const RoboscapeCol = require('./database'); // roboscape model
 
-module.exports = [
-    {
-        Parameters: '',
-        Method: 'get',
-        URL: 'golab/hamidoo',
-        middleware: [],
-        Handler: function(req, res) {
-            res.send('hola ');
+const genCrudRoutes = (model, baseEndpoint) => {
+    const routes = [
+        { // create
+            URL: '',
+            Parameters: '',
+            Method: 'post',
+            note: 'create',
+            // middleware: ['isLoggedIn'],
+            Handler: function(req, res) {
+                const newEntry = req.body;
+                return model.create(newEntry);
+            }
+        },
+
+        { // read many
+            URL: '',
+            Method: 'get',
+            // middleware: ['isLoggedIn'],
+            Handler: function(req, res) {
+                let query = res.locals.query || {};
+                logger.log('getting all robots', query);
+                return model.find(query);
+            }
+        },
+
+        { // read one
+            URL: '/:_id',
+            Method: 'get',
+            Handler: function(req, res) {
+                const { _id } = req.params;
+                return model.findById(_id);
+            }
+        },
+
+        { // update
+            URL: '/:_id',
+            Method: 'put', // FIXME patch?
+            Handler: function(req, res) {
+                const changedEntry = req.body;
+                return model.update({ _id: req.params._id }, { $set: changedEntry });
+            }
+        },
+
+        { // read one
+            URL: '/:_id',
+            Method: 'delete',
+            Handler: function(req, res) {
+                return model.remove({ _id: req.params._id });
+            }
         }
-    },
-];
+
+    ]
+        .map(r => {
+            r.URL = baseEndpoint + r.URL;
+            return r;
+        });
+    return routes;
+};
+
+const roboRoutes = genCrudRoutes(RoboscapeCol, 'roboscape/robots');
+
+module.exports = roboRoutes;
