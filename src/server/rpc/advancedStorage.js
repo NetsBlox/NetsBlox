63b97c796bb17399c222ee2151081182b098e526
Hamid Zare <dellydela@gmail.com>
2019-02-06 14:07:35 -0600

    SEP rename servicename in storage to collectionName

--- src/server/rpc/advancedStorage.js
+++ src/server/rpc/advancedStorage.js
@@ -7,11 +7,11 @@ mongoose.connect(MONGO_URI, { useNewUrlParser: true });
 
 // this could probably have a better name
 // creates a mongoose model for a service
-function getServiceStorage(serviceName, schemaDef) {
-    logger.trace(`setting up storage for "${serviceName}"`);
+function getServiceStorage(collectionName, schemaDef) {
+    logger.trace(`setting up storage for "${collectionName}"`);
     const schema = new mongoose.Schema(schemaDef);
-    if (!isValidServiceName(serviceName)) throw new Error('invalid service name');
-    const model = mongoose.model(`netsblox:services:${serviceName}`, schema);
+    if (!isValidServiceName(collectionName)) throw new Error('invalid service name');
+    const model = mongoose.model(`netsblox:services:${collectionName}`, schema);
     return model;
 }
 
