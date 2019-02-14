d25264035232ebf0798ba9c7ea9852e0571478e3
Hamid Zare <dellydela@gmail.com>
2019-02-14 12:53:17 -0600

    avoid rebinding consolelogs

--- src/server/logger.js
+++ src/server/logger.js
@@ -6,15 +6,18 @@ var levels = ['trace', 'info', 'log', 'debug', 'warn', 'error'],
 // set which debug levels to send to stderr, the rest will go to stdout
 const STDERR = ['warn', 'error'];
 
+const consolelog = console.log.bind(console),
+    consoleerror = console.error.bind(console);
+
 var Logger = function(name) {
     this._name = name;
     /* eslint-disable no-console*/
     levels.forEach(lvl => {
         this[lvl] = debug(`${name}:${lvl}`); // WARN the debug functions won't go out of scope (debug.instances) on their own
         if (STDERR.find(item => item === lvl)) {
-            this[lvl].log = console.error.bind(console);
+            this[lvl].log = consoleerror;
         } else { // send to stdout
-            this[lvl].log = console.log.bind(console);
+            this[lvl].log = consolelog;
         }
     });
     /* eslint-enable no-console*/
