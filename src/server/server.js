4268b134ebdd544727f2df2204b99265ca2d78d7
Hamid Zare <dellydela@gmail.com>
2019-02-14 13:57:24 -0600

    explicityly set santioned http verbs cors. adds put

--- src/server/server.js
+++ src/server/server.js
@@ -66,6 +66,7 @@ Server.prototype.configureRoutes = function() {
         res.header('Access-Control-Allow-Origin', req.get('origin'));
         res.header('Access-Control-Allow-Credentials', true);
         res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, SESSIONGLUE');
+        res.header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS, PUT, PATCH, DELETE');
         next();
     });
 
