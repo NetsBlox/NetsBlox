3cab172eae1e3b704debcba3486a5bb11f17853d
Hamid Zare <dellydela@gmail.com>
2019-02-18 12:46:32 -0600

    SEP Squashed new logger #2373:
    commit 054ce55865db8eb65b1e0cb191a9a1a5be5fd631
    Author: Gordon Stein <gstein@ltu.edu>
    Date:   Mon Feb 18 11:47:25 2019 -0600
    
        code climate
    
    commit f906a421a9300ef2126840d8a8bce93cfba96f67
    Author: Gordon Stein <gstein@ltu.edu>
    Date:   Mon Feb 18 11:44:57 2019 -0600
    
        Logging filter now based on each level
    
    commit b59daa911ffacf4334c64a62735a89a740d4fcfe
    Author: Gordon Stein <gstein@ltu.edu>
    Date:   Mon Feb 18 11:34:56 2019 -0600
    
        Other color check
    
    commit c77663a8f1a9f3d6d72f85fe4fd3aead6efd894f
    Author: Gordon Stein <gstein@ltu.edu>
    Date:   Mon Feb 18 11:33:45 2019 -0600
    
        different color support check
    
    commit 7dc1b20cef8ab04b414fb64819623edd95032ead
    Author: Gordon Stein <gstein@ltu.edu>
    Date:   Fri Feb 15 15:26:28 2019 -0600
    
        Color enable/disable feature to match debug's options
    
    commit 03e98936c59fa6f6f7eb70e96be754da73991e7c
    Author: Gordon Stein <gstein@ltu.edu>
    Date:   Fri Feb 15 15:21:21 2019 -0600
    
        Add a newline that was bugging me
    
    commit 7fe3dd38f84ad2d666bb2d8c90315671b59e0b60
    Author: Gordon Stein <gstein@ltu.edu>
    Date:   Fri Feb 15 15:18:45 2019 -0600
    
        Color hashing from package
    
    commit 2a2faedc1e5ba541d34f57b4342673bd4c691444
    Author: Gordon Stein <gstein@ltu.edu>
    Date:   Fri Feb 15 15:14:42 2019 -0600
    
        Better way to do it
    
    commit 0b7666d0a649c814ecbda513a0ded9a053aa52ad
    Author: Gordon Stein <gstein@ltu.edu>
    Date:   Fri Feb 15 15:13:07 2019 -0600
    
        Start work on unique colors
    
    commit bf6896f87312eb79cb08661b2321ba76f4a0c1dd
    Author: Gordon Stein <gstein@ltu.edu>
    Date:   Fri Feb 15 14:37:00 2019 -0600
    
        Remove debug statement
    
    commit cf1610c4e0ab7f15820d56710aae5ca009857dbc
    Author: Gordon Stein <gstein@ltu.edu>
    Date:   Fri Feb 15 14:34:59 2019 -0600
    
        Determine if enabled at creation time instead of log time
    
    commit d5fbf51c66dc0f529d83221c65d3174c02f27033
    Author: Gordon Stein <gstein@ltu.edu>
    Date:   Fri Feb 15 14:22:13 2019 -0600
    
        Code climate
    
    commit d9fd71709a84a817f666e2ed583a7d106e0a0fe9
    Author: Gordon Stein <gstein@ltu.edu>
    Date:   Fri Feb 15 14:20:02 2019 -0600
    
        Add date options, rename levels object
    
    commit b9b5855936bc252d1f73cfa4278fd6a10b7e4644
    Author: Gordon Stein <gstein@ltu.edu>
    Date:   Fri Feb 15 14:16:41 2019 -0600
    
        Reduce redundant arrays
    
    commit e5b918fb4e1923c0d24cf58262311d0411924379
    Author: Gordon Stein <gstein@ltu.edu>
    Date:   Fri Feb 15 14:13:22 2019 -0600
    
        Moved to modern class
    
    commit bd1189e89fc67c44902457e9e3b4003083f39790
    Author: Gordon Stein <gstein@ltu.edu>
    Date:   Fri Feb 15 14:07:38 2019 -0600
    
        Make names and skips actually static vars
    
    commit 52c6419ee35a80ecfa055d42d381579df79f7674
    Author: Gordon Stein <gstein@ltu.edu>
    Date:   Fri Feb 15 14:06:41 2019 -0600
    
        Change color lookup to functions and add default color option
    
    commit 5c541f34cc3561878f84951fdb019853f369aac0
    Author: Gordon Stein <gstein@ltu.edu>
    Date:   Fri Feb 15 14:03:53 2019 -0600
    
        upgrade chalk
    
    commit d519c83c4eb1cc7b071eaae3b088306de576620f
    Author: Gordon Stein <gstein@ltu.edu>
    Date:   Thu Feb 14 14:05:55 2019 -0600
    
        Quote change
    
    commit a2bbe51d6241cae89e7b32eafca47d3e48913688
    Author: Gordon Stein <gstein@ltu.edu>
    Date:   Thu Feb 14 14:04:37 2019 -0600
    
        Code climate
    
    commit afc5c3d5116753b26f8309ec1bfbb3d85e7e7802
    Author: Gordon Stein <gstein@ltu.edu>
    Date:   Thu Feb 14 13:59:24 2019 -0600
    
        Remove debug package, update chalk
    
    commit 2735e45c32c1a7fdd63c61243cf95c997d5193f5
    Author: Gordon Stein <gstein@ltu.edu>
    Date:   Thu Feb 14 13:56:58 2019 -0600
    
        Filter added
    
    commit 562b23475fbf0c9d1533c0b1689ee35c96241b79
    Author: Gordon Stein <gstein@ltu.edu>
    Date:   Thu Feb 14 13:39:16 2019 -0600
    
        Basic logging levels working

--- src/server/logger.js
+++ src/server/logger.js
@@ -1,27 +1,172 @@
 'use strict';
 
-var levels = ['trace', 'info', 'log', 'debug', 'warn', 'error'],
-    debug = require('debug');
+const chalk = require('chalk'),
+    moment = require('moment'),
+    ColorHash = require('color-hash');
+
+// NOP color option to use default
+const nullcolor = (output) => output;
+
+// Stores logging levels and associated output colors
+const LEVELS = {
+    'trace': {
+        bg: nullcolor,
+        fg: nullcolor
+    }, 
+    'info': {
+        bg: nullcolor,
+        fg: nullcolor
+    },
+    'log': {
+        bg: nullcolor,
+        fg: nullcolor
+    },
+    'debug': {
+        bg: nullcolor,
+        fg: nullcolor
+    },
+    'warn':  {
+        bg: chalk.bgYellow,
+        fg: chalk.black
+    },
+    'error': {
+        bg: chalk.bgRed,
+        fg: chalk.white
+    }
+};
 
 // set which debug levels to send to stderr, the rest will go to stdout
 const STDERR = ['warn', 'error'];
 
-var Logger = function(name) {
-    this._name = name;
-    /* eslint-disable no-console*/
-    levels.forEach(lvl => {
-        this[lvl] = debug(`${name}:${lvl}`);
-        if (STDERR.find(item => item === lvl)) {
-            this[lvl].log = console.error.bind(console);
-        } else { // send to stdout
-            this[lvl].log = console.log.bind(console);
-        }
-    });
-    /* eslint-enable no-console*/
-};
+// Get format from env variable
+const dateformat = process.env.DEBUG_DATE || 'MM.DD HH:mm:ss',
+    useColors = (process.env.DEBUG_COLORS || true) == true;
 
-Logger.prototype.fork = function(name) {
-    return new Logger([this._name, name].join(':'));
-};
+// For hashing names to colors
+const colorHash = new ColorHash({lightness: 0.75});
+
+class Logger {
+    /**
+     * Create a new logger with a given namespace
+     * @param {String} name Name/namespace for this Logger 
+     */
+    constructor(name) {
+        this._name = name;
+
+        Object.keys(LEVELS).forEach(lvl => {
+            if(this._shouldBeEnabled(lvl))
+            {
+                this[lvl] = this._log.bind(this, lvl);
+            } else {
+                this[lvl] = this._nop;
+            }
+        });
+
+        // Create color from string hash if available
+        if(chalk.supportsColor.has256 === true){
+            let hash = colorHash.hex(this._name);
+            this._colorize = chalk.hex(hash);
+        }
+    }
+
+    /**
+     * Determine if this logger matches the requested filters.
+     * @param {String=} level Level of logging to check for filter match, undefined for no level.
+     */
+    _shouldBeEnabled(level = undefined) {
+
+        let tempName = this._name;
+
+        // Add logging level to name
+        if(level !== undefined)
+        {
+            tempName += ':' + level;
+        }
+
+        // Allow skips
+        if(Logger.skips.some((skip) => skip.test(tempName)))
+        {
+            return false;
+        }
+    
+        // Allow whitelist
+        if(Logger.names.length > 0){
+            if(!Logger.names.some((name) => name.test(tempName)))
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    /**
+     * Used for loggers not actually sending messages out.
+     * @param {String} message Message being ignored
+     */
+    _nop(message){
+        return message;
+    }
+
+    /**
+     * Prints a message to the correct stream based on logging level
+     * @param {String} level Logging level for message
+     * @param {String} content Message to print 
+     */
+    _log(level, content) {
+        /* eslint-disable no-console*/
+        // Determine which output to use
+        let logFunc = STDERR.find(lvl => level === lvl)? console.error : console.log;
+        logFunc = logFunc.bind(console);
+        /* eslint-enable no-console*/
+    
+        // Prevent issues if no color available
+        let tags = `${dateformat != ''? moment().format(dateformat) + ' ' : ''}${this._name}:${level}`;
+
+        if(!useColors)
+        {
+            logFunc(tags + ' ' + content);
+        }
+        else if(chalk.supportsColor.has256 === true){
+            // Color from hash of name
+            logFunc(LEVELS[level].bg(LEVELS[level].fg(tags)) + ' ' + this._colorize(content));
+        } else {
+            // Default to only colors for labels
+            logFunc(LEVELS[level].bg(LEVELS[level].fg(tags)) + ' ' + content);
+        }
+    }
+
+    /**
+     * Create a new logger at one level deeper in the same namespace
+     * @param {String} name 
+     */
+    fork(name) {
+        return new Logger([this._name, name].join(':'));
+    }
+}
+
+// Inspired by debug package https://github.com/visionmedia/debug/
+// Namespaces to require a match from 
+Logger.names = [];
+// Namespaces to hide if matching
+Logger.skips = [];
+
+// Get input from env variable
+let namespaces = process.env.DEBUG || '';
+
+let split = namespaces.split(/[\s,]+/);
+
+// Set up filters 
+split.forEach((s) => {
+    if (s){
+        namespaces = s.replace(/\*/g, '.*?');
+
+        if (namespaces[0] === '-') {
+            Logger.skips.push(new RegExp('^' + namespaces.substr(1) + '$'));
+        } else {
+            Logger.names.push(new RegExp('^' + namespaces + '$'));
+        }
+    }
+});
 
 module.exports = Logger;
