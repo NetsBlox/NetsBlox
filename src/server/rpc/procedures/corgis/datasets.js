b3c3399abaf45ea26540cbc3f324038a1363cb2b
Hamid Zare <dellydela@gmail.com>
2019-02-19 13:29:21 -0600

    parser for dataset metadata + raw dataset info

---
+++ src/server/rpc/procedures/corgis/datasets.js
@@ -0,0 +1,59 @@
+const fse = require('fse');
+const path = require('path');
+
+// reads from a text file containing dataset descriptions in the following format
+
+/*
+url
+title
+desc
+tags
+
+url
+title
+desc
+tags
+*/
+
+const STORAGE_DIR = process.env.CORGIS_DIR || 'datasets/';
+
+const readAvailableDatasetNames = () => {
+    let files = fse.readdirSync(STORAGE_DIR);
+    files = files.map(fName => fName.replace('.json', ''));
+    return files;
+};
+
+const downloadedSets = readAvailableDatasetNames();
+
+function parseDatasetsInfo() {
+    let file = fse.readFileSync(path.join(__dirname, 'datasets.list'), 'utf8');
+
+    let datasetsMetadata = file
+        .split('\n\n')
+        .map(group => {
+            group = group
+                .split('\n');
+            let [url, title, description, tags] = group;
+
+            //get the id
+            let urlPieces = url.split('/');
+            let id = urlPieces[urlPieces.length - 1].replace('.html', '');
+
+            return {
+                id,
+                url,
+                title,
+                description,
+                tags: tags.split(', '),
+                isAvailable: downloadedSets.includes(id),
+            };
+        });
+    return datasetsMetadata;
+}
+
+// console.log(parseDatasetsInfo().filter(it => it.isAvailable).map(it => it.title).length);
+
+module.exports = {
+    parseDatasetsInfo,
+    downloadedSets,
+};
