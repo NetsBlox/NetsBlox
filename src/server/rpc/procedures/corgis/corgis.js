c9ce73a9e3d46fb450d7263e0f5a916d94bff3c1
Hamid Zare <dellydela@gmail.com>
2019-02-19 13:33:18 -0600

    added basic rpcs for datasets with calls to get metadata and url for them

--- src/server/rpc/procedures/corgis/corgis.js
+++ src/server/rpc/procedures/corgis/corgis.js
@@ -1,15 +1,16 @@
 const ApiConsumer = require('../utils/api-consumer'),
     jsonQuery = require('json-query'),
-    fs = require('fs');
+    corgiDatasets = require('./datasets.js'),
+    fse = require('fse');
 
-let corgis = new ApiConsumer('corgis','');
+let corgis = new ApiConsumer('Corgis','');
 
 const STORAGE_DIR = process.env.CORGIS_DIR || 'datasets/';
 
 let loadDataset = dsName => {
     let absolutePath = STORAGE_DIR + dsName + '.json';
     return new Promise((resolve, reject) => {
-        fs.readFile(absolutePath, 'utf8', function (err,data) {
+        fse.readFile(absolutePath, 'utf8', function (err,data) {
             if (err) {
                 reject(err);
             }
@@ -17,32 +18,46 @@ let loadDataset = dsName => {
                 data = JSON.parse(data);
                 resolve(data);
             }
-        }); // end of fs.readFile
+        }); // end of fse.readFile
     });
 };
+
+const datasetsMetadata = corgiDatasets.parseDatasetsInfo();
+
 
 /**
  * Search CORGIS' datasets as provided by https://think.cs.vt.edu/corgis/
  * @param {String} name dataset name
- * @param {String=} query json search query
+ * @param {String=} query search query in JSON
  * @returns {Object} search results
  */
 
-corgis.searchDataset = function(name,query){
-    return loadDataset(name)
-        .then( data => {
-            this._logger.trace(data.length, 'results loaded');
-            if (!query) {
-                let sample = data[0];
-                return this._createSnapStructure(sample);
-            }
-            let queryRes = jsonQuery(query,{data}).value;
-            this._logger.trace(queryRes);
-            this.response.send(this._createSnapStructure(queryRes));
-        })
-        .catch(this._logger.error);
+corgis.searchDataset = async function(name, query){
+    const data = await loadDataset(name);
+    this._logger.trace(data.length, 'results loaded');
+    if (!query) {
+        let sample = data[0];
+        return this._createSnapStructure(sample);
+    }
+    let queryRes = jsonQuery(query,{data}).value;
+    this._logger.trace(queryRes);
+    this.response.send(this._createSnapStructure(queryRes));
 };
 
+corgis.allDatasets = function() {
+    return datasetsMetadata;
+};
+
+corgis.availableDatasets = function() {
+    return datasetsMetadata.filter(it => it.isAvailable);
+};
+
+
+datasetsMetadata.forEach(ds => {
+    corgis[ds.id] = function(query) {
+        return corgis.searchDataset(ds.id, query);
+    };
+});
 
 
 corgis.isSupported = function() {
