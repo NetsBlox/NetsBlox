aec96aa89b9852b2a980f23ab0c1063cf9a5f3b5
Gordon Stein <gstein@ltu.edu>
2019-05-30 12:49:21 -0500

    Automatically load csv file if db is empty

--- src/server/rpc/procedures/paleo-climate/paleo-climate.js
+++ src/server/rpc/procedures/paleo-climate/paleo-climate.js
@@ -2,10 +2,69 @@
  * access to NOAA paleo
  * @service
  */
-
-const PaleoObject = require('./database.js');
 const DBConsumer = require('../utils/db-consumer');
+const getServiceStorage = require('../../advancedStorage');
+const seeder = require('../utils/csv-loader');
+const path = require('path');
+
+const schemaDef = {
+    core: String,
+    datatype: String,
+    year: String,
+    value: String
+};
+
+const PaleoObject = getServiceStorage ('paleo', schemaDef);
 const paleo = new DBConsumer('paleo', PaleoObject);
+
+const featuredFields = [
+    'core',
+    'datatype',
+    'year',
+    'value'
+];
+
+paleo._genRPCs(featuredFields);
+
+function seed() {
+    const opts = {
+        url: undefined, 
+        filePath:  path.join(__dirname, 'composite.csv'),
+        recParser:function(aRecord) { // optional
+            if (isNaN(parseInt(aRecord.year))){
+                paleo._logger.warn(aRecord);
+                return null; 
+            }else{
+                aRecord.year = parseInt(aRecord.year); 
+            } 
+            if (isNaN(parseFloat(aRecord.value))) {
+                return null; 
+            }else{
+                aRecord.CO2Concentration = parseFloat(aRecord.value); 
+            }
+            if (isNaN(parseFloat(aRecord.Error))){
+                delete aRecord.Error;
+            }else{
+                aRecord.Error = parseFloat(aRecord.Error); 
+            } 
+            return aRecord;
+        }
+    };
+
+    paleo._logger.warn(opts);
+  
+    //make rec parser funciton and then call this with seeder 
+    seeder(PaleoObject, opts);
+}
+
+// Test for existing data
+paleo.searchByCore('Dome C').then(result =>
+{
+    if(result.length === 0){
+        paleo._logger.warn('No Paleo Climate RPC data found, attempting to load from composite.csv');
+        seed();
+    }
+});
 
 //get list of fields in database
 paleo.fields = function() {
@@ -20,27 +79,6 @@ paleo.dataTypes = function() {
     return ['Oxygen', 'Carbon Dioxide']; 
 }; 
 
-//returns lists of the value of the data asked for
-paleo.iceCoreData = function(datatype,core){	// blank query gives total list
-    return (this._iceCoreData(datatype, core));
-};
-
-//returns lists of years associated with data values asked for	
-paleo.iceCoreDataYear = function(datatype,core){	// blank query gives total list
-    return (this._iceCoreDataYear(datatype, core));
-};
-
-// returns a sepecific piece of data 
-paleo.dataSearch = function(datatype,core,year){	// blank query gives total list
-    return (this._dataSearch(datatype, core, year));
-};
-			
-paleo.yearRange = function(startyear, endyear, datatype, core){	// blank query gives total list
-    return (this._yearRange(startyear, endyear, datatype, core));
-};
-
-paleo.yearRangeYr = function(startyear, endyear, datatype, core){	// blank query gives total list
-    return (this._yearRangeYr(startyear, endyear, datatype, core));
-};
+paleo.serviceName = 'PaleoClimate';
 
 module.exports = paleo;
