94d592a7021eb4604d19d5d37678a1235a9648e4
ihabibi <serkhail@gmail.com>
2019-06-06 15:03:34 -0500

    modified getPPM RPC to display ppm of all months in selected year if month not specified

--- src/server/rpc/procedures/hawaii-co2-data/hawaii-co2-data.js
+++ src/server/rpc/procedures/hawaii-co2-data/hawaii-co2-data.js
@@ -1,5 +1,6 @@
 const lineReader = require('line-reader');
 const path = require('path');
+const logger = require('../utils/logger')('hawaii-co2-data');
 
 const table = [
     ['Year', 'Month', 'Interpolated ppm', 'Seasonally adjusted ppm']
@@ -30,7 +31,7 @@ co2service.getInterpolated = function(){
             table1.push(localArr);
         }});
     return table1;
-    };
+};
 
 co2service.getSeasonal = function(){
     lineReader.eachLine(path.join(__dirname,'co2_mm_mlo.txt'), function (line) {
@@ -44,7 +45,7 @@ co2service.getSeasonal = function(){
             table2.push(localArr);
         }});
     return table2;
-    };
+};
 co2service.getWhole = function(){
     lineReader.eachLine(path.join(__dirname,'co2_mm_mlo.txt'), function (line) {
         if (line.startsWith('#')) {
@@ -60,22 +61,58 @@ co2service.getWhole = function(){
     return table;
 };
 
-/**
- *returns the specified ppm value after inputting the year, month, and type of data
- * @param {BoundedNumber<1958,2019>} year - year between 1958 and 2019 to return the selected ppm of
- * @param {BoundedNumber<1,12>} month - numerical value of the month to return the selected ppm of
- * @param {string} type - select which type of data to be returned, either "seasonal" or "interpolated"
- * @returns {string} ppm value - the selected type of ppm that matches the inputted year and month
- */
 co2service.getPPM = function(year, month, type) {
-    let result = this.getWhole(type);
     let ppm = '';
-    result.forEach(function (value) {
-        if (value[0] === year.toString() && value[1] === month.toString())
-            ppm = value[2];
-    });
+    const ppmTable = [[year, month, ppm]];
+    let result = this.getWhole(type);
+    let result1 = this.getInterpolated(type);
+    let result2 = this.getSeasonal(type);
+    if (month === ''){
+        return (new Promise(function(resolve, reject) {
+            lineReader.eachLine(path.join(__dirname,'co2_mm_mlo.txt'), function (line) {
+                if (line.startsWith(year)) {
+                    if (type.toUpperCase() === 'SEASONAL') {
+                        ppm = line.substring(50, 56);
+                        let year = line.substring(0,4);
+                        let month = line.substring(4,8).trim();
+                        let localArr = [year, month, ppm];
+                        ppmTable.push(localArr);
+                    } else if (type.toUpperCase() === 'INTERPOLATED') {
+                        ppm = line.substring(38, 44);
+                        let year = line.substring(0,4);
+                        let month = line.substring(4,8).trim();
+                        let localArr = [year, month, ppm];
+                        ppmTable.push(localArr);
+                    }
+                    logger.debug(ppmTable);
+                }
+            }, function(err) {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve();
+                }
+            });
+        })).then(function() {
+            return ppmTable;
+        });
+
+    } else {
+        result.forEach(function (value) {
+            if (value[0] === year.toString() && value[1] === month.toString())
+                if (type.toUpperCase() === 'INTERPOLATED') {
+                    ppm = value[2];
+                }
+                else if (type.toUpperCase() === 'SEASONAL'){
+                    ppm = value[3];
+                }
+        });
+    }
     return ppm;
 };
+//pass
+
+
 /**
  *displays the different message options that can be entered into the text field parameters of related RPCs
  * @returns {array} array - shows the different values that can be inputted into the "text" fields of the various related RPCs
