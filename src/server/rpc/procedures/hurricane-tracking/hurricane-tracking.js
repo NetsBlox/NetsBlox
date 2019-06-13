ed3046f564b3646efc92b2ad37f55eeac623c952
ihabibi <serkhail@gmail.com>
2019-06-13 10:18:47 -0500

    added new RPC functions for displaying tables

--- src/server/rpc/procedures/hurricane-tracking/hurricane-tracking.js
+++ src/server/rpc/procedures/hurricane-tracking/hurricane-tracking.js
@@ -2,11 +2,14 @@ const lineReader = require('line-reader');
 const path = require('path');
 
 const table = [
-    ['year', 'month', 'day', 'time', 'recordID', 'status', 'latitude', 'longitude', 'maxWind', 'minPressure']
+    ['name', 'year', 'month', 'day', 'time', 'recordID', 'status', 'latitude', 'longitude', 'maxWind', 'minPressure']
 ];
+
+let name = '';
+
 lineReader.eachLine(path.join(__dirname,'hurdat2-1851-2018-051019.txt'), function (line, last) {
     if (line.startsWith('AL')){
-        //pass
+        name = line.substring(19, 28).trim();
     } else {
         let year = line.substring(0, 4);
         let month = line.substring(4,6);
@@ -14,16 +17,16 @@ lineReader.eachLine(path.join(__dirname,'hurdat2-1851-2018-051019.txt'), functio
         let time = line.substring(10,14);
         let recordID = line.substring(15,17);
         let status = line.substring(18,21);
-        let latitude = line.substring(23,28);
-        let longitude = line.substring(31,36);
+        let latitude = line.substring(23,27);
+        let longitude = '-' + line.substring(31,35);
         let maxWind = line.substring(39,41);
         let minPressure = line.substring(43,47).trim();
-        let data = [year, month, day, time, recordID, status, latitude, longitude, maxWind, minPressure];
+        let data = [name, year, month, day, time, recordID, status, latitude, longitude, maxWind, minPressure];
         table.push(data);
     }
-    /* if (last){
-         return table;
-     } */
+     if (last){
+         console.log(table);
+     }
 });
 
 const hurricaneTracker = {};
@@ -34,7 +37,55 @@ hurricaneTracker.parseTable = function(){
     return table;
 };
 
+hurricaneTracker.getHurricane = function(text){
+    let parsedTable = [table[0]];
+    for (let i = 1; i < table.length; i++) {
+        if (table[i].includes(text.toUpperCase())) {
+            parsedTable.push(table[i]);
+        }
+    }
+    if (parsedTable.length === 1) {
+        return 'Invalid Hurricane Name';
+    }
+    else {
+        return parsedTable;
+    }
+};
 
+hurricaneTracker.getYearList = function(date){
+    let parsedTable = [table[0]];
+    for (let i = 1; i < table.length; i++) {
+        if (table[i].includes(date)) {
+            parsedTable.push(table[i]);
+        }
+    }
+    if (parsedTable.length === 1) {
+        return 'Invalid Year';
+    }
+    else {
+        return parsedTable;
+    }
+};
+
+hurricaneTracker.getLatitude = function(name){
+    for (let i = 1; i < table.length; i++) {
+        if (table[i].includes(name)) {
+            let row = table[i];
+            return row[7];
+        }
+    }
+    return 'Invalid name';
+};
+
+hurricaneTracker.getLongitude = function(name){
+    for (let i = 1; i < table.length; i++) {
+        if (table[i].includes(name)) {
+            let row = table[i];
+            return row[8];
+        }
+    }
+    return 'Invalid name';
+};
 /*hurricaneTracker.parseTable2 = function(){
     const table2 = [
         ['year', 'month', 'day', 'time', 'recordID', 'status', 'latitude', 'longitude', 'maxWind', 'minPressure']
