6158299797410c8f0b28906a73ac13705797a94c
Brady, Corey Edison <corey.brady@vanderbilt.edu>
2019-06-19 21:37:15 -0500

    remove commented out lines from trying to get HTTP formats correct

--- src/server/rpc/procedures/badgeserver/badgeserver.js
+++ src/server/rpc/procedures/badgeserver/badgeserver.js
@@ -14,7 +14,7 @@ const BadgeConsumer = new ApiConsumer('BadgeServer', 'https://gallery.app.vander
  * @param {String} groupid  The group that you want to join.
  */
 BadgeConsumer.join = function ( groupid ){
-    let body = ''; //`group-id=${encodeURI(groupid)}`;
+    let body = '';
     return this._sendAnswer({queryString: '/badgerstate/join/'+groupid+'/', method: 'POST',
         headers: {
             'Content-Type' : 'application/x-www-form-urlencoded',}, body: body})
@@ -28,7 +28,6 @@ BadgeConsumer.join = function ( groupid ){
  * @param {String} groupid  The group whose participants you want to list.
  */
 BadgeConsumer.participants = function ( groupid ){
-    //let body = `group-id=${encodeURI(theid)}`;
     let body = '';
     return this._sendAnswer({queryString: '/badgerstate/participants/'+groupid+'/', method: 'GET',
         headers: {
@@ -44,8 +43,7 @@ BadgeConsumer.participants = function ( groupid ){
  * @param {String} thedata  The arbitrary data you want to store in that bucket.
  */
 BadgeConsumer.adddata = function ( group, bucket, thedata ){
-    //let body = `group-id=${encodeURI(group)}&bucket-id=${encodeURI(bucket)}&data=${encodeURI(thedata)}`;
-    let body = ''; //`group-id=${encodeURI('123456789')}&bucket-id=${encodeURI('07cbee79-b3f3-4c0a-a59a-b11be55d7602')}&data=${encodeURI('12345')}`;
+    let body = '';
     return this._sendAnswer({queryString: '/badgerstate/data/'+group+'/'+bucket+'/'+thedata, method: 'POST',
         headers: {
             'Content-Type' : 'application/x-www-form-urlencoded',}, body: body})
@@ -61,7 +59,7 @@ BadgeConsumer.adddata = function ( group, bucket, thedata ){
  * @param {String} n  The number of records to get; 0 for all.
  */
 BadgeConsumer.getdata = function ( group, bucket, n ){
-    let body = '';//`group-id=${encodeURI(group)}&bucket-id=${encodeURI(bucket)}&n=${encodeURI(n)}`;
+    let body = '';
     return this._sendAnswer({queryString: '/badgerstate/data/'+group+'/'+bucket+'/'+n, method: 'GET',
         headers: {
             'Content-Type' : 'application/x-www-form-urlencoded',}, body: body})
@@ -76,7 +74,7 @@ BadgeConsumer.getdata = function ( group, bucket, n ){
  * @param {String} signal  The arbitrary data you want to store in that bucket.
  */
 BadgeConsumer.setsignal = function ( group, bucket, signal ){
-    let body = ''; //`signal=${encodeURI(signal)}`;
+    let body = '';
     return this._sendAnswer({queryString: '/badgerstate/signal/'+group+'/'+bucket+'/'+signal, method: 'POST',
         headers: {
             'Content-Type' : 'application/x-www-form-urlencoded',}, body: body})
@@ -90,7 +88,7 @@ BadgeConsumer.setsignal = function ( group, bucket, signal ){
  * @param {String} bucket  The 'bucket' whose data you're adding to (the participant).
  */
 BadgeConsumer.getsignal = function ( group, bucket ){
-    let body = ''; //`bucket-id=${encodeURI(bucket)}`;
+    let body = ''; 
     return this._sendAnswer({queryString: '/badgerstate/signal/'+group+'/'+bucket, method: 'GET',
         headers: {
             'Content-Type' : 'application/x-www-form-urlencoded',}, body: body})
