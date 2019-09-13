6f6292e836cd50c85e259d8c1e21be39b35cbc97
Brian Broll <brian.broll@gmail.com>
2019-09-13 11:38:55 -0500

    Improved help msg for getIceCoreNames. Closes #2451

--- src/server/rpc/procedures/paleo-climate/paleo-climate.js
+++ src/server/rpc/procedures/paleo-climate/paleo-climate.js
@@ -154,7 +154,7 @@ PaleoClimate._getColumnData = async function(core, datatype, startyear='', endye
 };
 
 /**
- * Get all valid names of ice cores
+ * Get names of ice cores with data available.
  * @returns {Array}
  */
 PaleoClimate.getIceCoreNames = function() {
