8c7399f8c652b7666c660ce0631e8b15b0e58467
Brian Broll <brian.broll@gmail.com>
2019-10-21 10:59:35 -0500

    WIP update interface tests for HawaiiCO2Data

--- test/unit/server/rpc/procedures/hawaii-co2-data.spec.js
+++ test/unit/server/rpc/procedures/hawaii-co2-data.spec.js
@@ -1,13 +1,8 @@
 describe('hawaii-co2-data', function() {
     const utils = require('../../../../assets/utils');
-    var HawaiiCO2Service = utils.reqSrc('rpc/procedures/hawaii-co2-data/hawaii-co2-data'),
-        RPCMock = require('../../../../assets/mock-rpc'),
-        co2 = new RPCMock(HawaiiCO2Service);
 
-    utils.verifyRPCInterfaces(co2, [
-        ['getInterpolated', []],
-        ['getSeasonal', []],
-        ['getWhole', []],
-        ['getPPM', ['year', 'month', 'type']],
+    utils.verifyRPCInterfaces('HawaiiCO2Data', [
+        ['getCarbonDioxideData', ['startyear', 'endyear']],
+        ['getCO2TrendData', ['startyear', 'endyear']],
     ]);
 });
