fadfe2c8bbd2227ea5af572889423e0b5198098f
Brian Broll <brian.broll@gmail.com>
2019-11-14 15:17:05 -0600

    Split composite ice core into individual ice cores. Closes #2595

--- src/server/rpc/procedures/ice-core-data/data/antarctic-co2-composite/index.js
+++ src/server/rpc/procedures/ice-core-data/data/antarctic-co2-composite/index.js
@@ -2,6 +2,33 @@ const fs = require('fs');
 const path = require('path');
 const assert = require('assert');
 
+function getCoreName(yearBP) {
+    assert(
+        yearBP > -52 && yearBP < 805669,
+        `Year ${yearBP} outside expected range.`
+    );
+
+    if (yearBP < 2000) {
+        return 'Law';
+    } else if (yearBP < 11000) {
+        return 'Dome C';
+    } else if (yearBP < 22000) {
+        return 'WAIS';
+    } else if (yearBP < 40000) {
+        return 'Siple Dome';
+    } else if (yearBP < 60000) {
+        return 'TALDICE';
+    } else if (yearBP < 115000) {  // FIXME
+        return 'EDML';  // TODO: This *could* be Dome C
+    } else if (yearBP < 155000) {
+        return 'Dome C';
+    } else if (yearBP < 393000) {
+        return 'Vostok';
+    } else {
+        return 'Dome C';
+    }
+}
+
 const dataFile = path.join(__dirname, 'antarctica2015co2composite.txt');
 const lines = fs.readFileSync(dataFile, 'utf8').split('\n');
 
@@ -17,11 +44,13 @@ while (!dataRegex.test(lines[lines.length-1])) {
 const records = lines
     .map(line => {
         const [yearsBefore1950, value] = line.split('\t');
-        const core = 'Antarctic Composite CO2';
+        const core = getCoreName(parseFloat(yearsBefore1950));
         const year = 1950 - yearsBefore1950;
         return {core, year, datatype: 'Carbon Dioxide', value};
     });
 
 const EXPECTED_RECORD_COUNT = 1901;
 assert.equal(records.length, EXPECTED_RECORD_COUNT);
-module.exports = records;
+
+const CORE_NAMES = ['Law', 'Dome C', 'WAIS', 'Vostok'];
+module.exports = records.filter(record => CORE_NAMES.includes(record.core));
