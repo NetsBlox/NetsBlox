2b2e66e7d1d3b6673ffab3aec64d80fe3c421d8a
Brian Broll <brian.broll@gmail.com>
2020-01-10 08:19:44 -0600

    Change rpc lib endpoint to avoid collisions w/ services. Fix #2634 (#2635)
    * WIP rename rpc/libs/RPC to rpc/libs/LIBS
    
    * Change rpc lib endpoint to avoid collisions w/ services. Fix #2634
    
    * WIP update client code
    
    * WIP fixed linting issues

--- src/server/server.js
+++ src/server/server.js
@@ -154,37 +154,33 @@ Server.prototype.configureRoutes = async function() {
         (req, res, next) => middleware.tryLogIn(req, res, next, true),
         RPCManager.router
     );
-    var RPC_ROOT = path.join(__dirname, 'rpc', 'libs'),
-        RPC_INDEX = fs.readFileSync(path.join(RPC_ROOT, 'RPC'), 'utf8')
-            .split('\n')
-            .filter(line => {
-                var parts = line.split('\t'),
-                    deps = parts[2] ? parts[2].split(' ') : [],
-                    displayName = parts[1];
+    const RPC_ROOT = path.join(__dirname, 'rpc', 'libs');
+    const RPC_INDEX = fs.readFileSync(path.join(RPC_ROOT, 'LIBS'), 'utf8')
+        .split('\n')
+        .filter(line => {
+            const parts = line.split('\t');
+            const deps = parts[2] ? parts[2].split(' ') : [];
+            const displayName = parts[1];
 
-                // Check if we have loaded the dependent rpcs
-                for (var i = deps.length; i--;) {
-                    if (!RPCManager.isServiceLoaded(deps[i])) {
-                        // eslint-disable-next-line no-console
-                        console.log(`Service ${displayName} not available because ${deps[i]} is not loaded`);
-                        return false;
-                    }
+            // Check if we have loaded the dependent rpcs
+            for (let i = deps.length; i--;) {
+                if (!RPCManager.isServiceLoaded(deps[i])) {
+                    // eslint-disable-next-line no-console
+                    console.log(`Service ${displayName} not available because ${deps[i]} is not loaded`);
+                    return false;
                 }
-                return true;
-            })
-            .map(line => line.split('\t').splice(0, 2).join('\t'))
-            .join('\n');
+            }
+            return true;
+        })
+        .map(line => line.split('\t').splice(0, 2).join('\t'))
+        .join('\n');
 
-    this.app.get('/rpc/:filename', (req, res) => {
-        var RPC_ROOT = path.join(__dirname, 'rpc', 'libs');
-
-        // IF requesting the RPC file, filter out unsupported rpcs
-        if (req.params.filename === 'RPC') {
+    this.app.get('/servicelibs/:filename', (req, res) => {
+        if (req.params.filename === 'SERVICELIBS') {
             res.send(RPC_INDEX);
         } else {
             res.sendFile(path.join(RPC_ROOT, req.params.filename));
         }
-
     });
 
     this.app.get('/Examples/EXAMPLES', (req, res) => {
