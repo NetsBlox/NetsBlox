cb7b306c5f19ad6cd28757043ef30fede96ffcd8
Brian Broll <brian.broll@gmail.com>
2020-01-22 09:39:30 -0600

    WIP Add buffered channel for communication

--- src/server/services/remote-client.js
+++ src/server/services/remote-client.js
@@ -1,29 +1,51 @@
-const {SendMessage, SendMessageToRoom} = require('./messages');
+const {SendMessage, SendMessageToRoom, SendMessageToRole} = require('./messages');
 const {Dealer} = require('zeromq');
 const sender = new Dealer();
 sender.connect('tcp://127.0.0.1:1235');
-// TODO: Make a queue?
 
+class QueuedSender {
+    constructor() {
+        this.sending = false;
+        this.waiting = [];
+    }
+
+    trySend(data) {
+        if (this.sending) {
+            this.waiting.push(data);
+        } else {
+            this.send(data);
+        }
+    }
+
+    async send(data) {
+        this.sending = true;
+        await sender.send(data);
+        if (this.waiting.length) {
+            this.send(this.waiting.shift());
+        }
+        this.sending = false;
+    }
+}
+
+const channel = new QueuedSender();
 class RemoteClient {
-    constructor(projectId, roleId, clientId) {
+    constructor(projectId, roleId, clientId, username) {
         this.projectId = projectId;
         this.clientId = clientId;
         this.roleId = roleId;
+        this.username = username;
     }
 
     async sendMessage(type, contents={}) {
-        await sender.send(new SendMessage(this.clientId, type, contents));
+        await channel.trySend(new SendMessage(this.clientId, type, contents));
     }
 
     async sendMessageToRole(roleId, type, contents={}) {
+        await channel.trySend(new SendMessageToRole(this.projectId, roleId, type, contents));
     }
 
     async sendMessageToRoom(type, contents={}) {
-        await sender.send(new SendMessageToRoom(this.projectId, type, contents));
-    }
-
-    send(type, contents) {
-        throw new Error('send not implemented for RemoteClient');
+        await channel.trySend(new SendMessageToRoom(this.projectId, type, contents));
     }
 }
 
