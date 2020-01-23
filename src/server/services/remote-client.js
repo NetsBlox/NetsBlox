12bbddff0fab8fd1a8f5b0e3828f77611bdd53e3
Brian Broll <brian.broll@gmail.com>
2020-01-23 11:24:35 -0600

    WIP Add default api port

--- src/server/services/remote-client.js
+++ src/server/services/remote-client.js
@@ -1,7 +1,7 @@
 const {SendMessage, SendMessageToRoom, SendMessageToRole} = require('./messages');
 const {Dealer} = require('zeromq');
 const sender = new Dealer();
-sender.connect('tcp://127.0.0.1:' + process.env.NETSBLOX_API_PORT);
+sender.connect('tcp://127.0.0.1:' + (process.env.NETSBLOX_API_PORT || '1357'));
 
 class QueuedSender {
     constructor() {
