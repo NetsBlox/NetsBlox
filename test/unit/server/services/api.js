dd2b16df204d7b71f709cca6d9a748729b57d33a
Brian Broll <brian.broll@gmail.com>
2020-01-24 13:50:37 -0600

    Ensure using the valid service name in RPC calls. Fixes #2643 (#2644)
    * Ensure using the valid service name in RPC calls. Fixes #2643
    
    * WIP Updated tests

--- test/unit/server/services/api.js
+++ test/unit/server/services/api.js
@@ -12,42 +12,47 @@ describe('services-api', function() {
     describe('validateRPCRequest', function() {
         it('should return 404 if service not found', function() {
             const response = new MockResponse();
-            const request = new MockRequest('Dev??', 'echo');
-            const isValid = ServicesAPI.validateRPCRequest(request, response);
+            const serviceName = 'Dev??';
+            const request = new MockRequest(serviceName, 'echo');
+            const isValid = ServicesAPI.validateRPCRequest(serviceName, request, response);
             assert(!isValid, 'RPC request falsey reported as valid');
             assert.equal(response.code, 404);
         });
 
         it('should return 404 if RPC not found (valid service)', function() {
             const response = new MockResponse();
-            const request = new MockRequest('Dev', 'unknown');
-            const isValid = ServicesAPI.validateRPCRequest(request, response);
+            const serviceName = 'Dev';
+            const request = new MockRequest(serviceName, 'unknown');
+            const isValid = ServicesAPI.validateRPCRequest(serviceName, request, response);
             assert(!isValid, 'RPC request falsely reported as valid');
             assert.equal(response.code, 404);
         });
 
         it('should return 400 if missing uuid', function() {
             const response = new MockResponse();
-            const request = new MockRequest('PublicRoles', 'getPublicRoleId');
+            const serviceName = 'PublicRoles';
+            const request = new MockRequest(serviceName, 'getPublicRoleId');
             delete request.query.uuid;
-            const isValid = ServicesAPI.validateRPCRequest(request, response);
+            const isValid = ServicesAPI.validateRPCRequest(serviceName, request, response);
             assert(!isValid, 'RPC request falsely reported as valid');
             assert.equal(response.code, 400);
         });
 
         it('should return 400 if missing uuid', function() {
             const response = new MockResponse();
-            const request = new MockRequest('PublicRoles', 'getPublicRoleId');
+            const serviceName = 'PublicRoles';
+            const request = new MockRequest(serviceName, 'getPublicRoleId');
             delete request.query.projectId;
-            const isValid = ServicesAPI.validateRPCRequest(request, response);
+            const isValid = ServicesAPI.validateRPCRequest(serviceName, request, response);
             assert(!isValid, 'RPC request falsely reported as valid');
             assert.equal(response.code, 400);
         });
 
         it('should return true if valid RPC', function() {
             const response = new MockResponse();
-            const request = new MockRequest('PublicRoles', 'getPublicRoleId');
-            const isValid = ServicesAPI.validateRPCRequest(request, response);
+            const serviceName = 'PublicRoles';
+            const request = new MockRequest(serviceName, 'getPublicRoleId');
+            const isValid = ServicesAPI.validateRPCRequest(serviceName, request, response);
             assert(isValid, response.response);
             assert.equal(response.code, undefined);
         });
