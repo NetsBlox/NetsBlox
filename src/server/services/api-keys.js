c8d6b55892f13cc46ce533e4e03369bcd15f0a46
Brian Broll <brian.broll@gmail.com>
2020-02-13 14:10:23 -0600

    WIP very rough code pushed to basically just sync to laptop

---
+++ src/server/services/api-keys.js
@@ -0,0 +1,22 @@
+// TODO: Manage API keys
+// TODO: Ensure the user is authenticated?
+class APIKeys {
+
+    async initialize(mongoURI) {
+        // TODO: Connect to the database
+    }
+
+    async list(username) {
+        // TODO
+    }
+
+    async create(service, username, key) {
+        // TODO
+    }
+
+    async delete(service, username) {
+        // TODO
+    }
+}
+
+module.exports = APIKeys;
