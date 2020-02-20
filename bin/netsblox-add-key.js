dc765abe852ece282e8f662a0d00d89513809801
Brian Broll <brian.broll@gmail.com>
2020-02-20 10:41:40 -0600

    Add group support to custom API keys. Closes #2664 (#2665)

--- bin/netsblox-add-key.js
+++ bin/netsblox-add-key.js
@@ -5,10 +5,15 @@ require('epipebomb')();  // Allow piping to 'head'
 const {Command} = require('commander');
 const program = new Command();
 const Storage = require('../src/server/storage/storage');
+const Groups = require('../src/server/storage/groups');
 const ApiKeys = require('../src/server/services/api-keys');
 const { runWithStorage } = require('./utils');
 
-program.arguments('<username> <key> <value>');
+program
+    .arguments('<username> <key> <value>')
+    .option('-g, --groups <groups>', 'groups using the given key')
+    .option('-a, --all-groups', 'set default key for the given user\'s groups');
+
 program.parse(process.argv);
 if (program.args.length !== 3) {
     /* eslint-disable no-console */
@@ -20,8 +25,15 @@ if (program.args.length !== 3) {
 const [username, key, value] = program.args;
 
 async function addKey() {
+    const groupNames = program.groups ? program.groups.split(',') : [];
+    const allGroups = await Groups.findAllUserGroups(username);
+    const groups = groupNames
+        .map(name => allGroups.find(g => g.name === name));
+    const groupIds = groups.map(group => group.getId());
+    const isGroupDefault = !!program.allGroups;
+
     ApiKeys.init(Storage._db);
-    await ApiKeys.create(username, key, value);
+    await ApiKeys.create(username, key, value, isGroupDefault, groupIds);
 }
 
 runWithStorage(addKey);
--- src/server/services/api-keys.js
+++ src/server/services/api-keys.js
@@ -1,4 +1,6 @@
 const Logger = require('../logger');
+const Users = require('../storage/users');
+const assert = require('assert');
 class APIKeys {
     async init(db, logger) {
         this.collection = db.collection('api-keys');
@@ -7,26 +9,78 @@ class APIKeys {
 
     async get(username, apiKey) {
         const {name} = apiKey;
-        const userKey = await this.collection.findOne({name, username});
+        const userKey = await this.getUserKey(username, name);
         if (userKey) {
             return apiKey.withValue(userKey.value);
         }
-        // TODO: Check for apiKeys for the group?
+
+        const groupKey = await this.getGroupKey(username, name);
+        if (groupKey) {
+            return apiKey.withValue(groupKey.value);
+        }
         return apiKey;
     }
 
-    async list(username) {
-        return this.collection.find({username}, {_id: 0}).toArray();
+    async getUserKey(owner, type) {
+        const userKeys = await this.collection.find({owner, type}, {_id: 0}).toArray();
+        return this.getLeastSharedKey(userKeys);
     }
 
-    async create(username, name, value) {
-        const filter = {username, name};
+    getKeyPriority(key) {
+        if (key.isGroupDefault) {
+            return Infinity;
+        }
+        return key.groups.length;
+    }
+
+    getLeastSharedKey(keys) {
+        keys.sort((key1, key2) => {
+            const p1 = this.getKeyPriority(key1);
+            const p2 = this.getKeyPriority(key2);
+            return p1 < p2 ? -1 : 1;
+        });
+        return keys.shift();
+    }
+
+    async getGroupKey(username, type) {
+        const user = await Users.get(username);
+        const group = user ? await user.getGroup() : null;
+        if (group) {
+            const owner = group.getOwner();
+            const isGroupDefault = {owner, type, isGroupDefault: true};
+            const isKeyForGroup = {type, groups: group.getId()};
+            const query = {$or: [isGroupDefault, isKeyForGroup]};
+            const keys = await this.collection.find(query, {_id: 0}).toArray();
+            return this.getLeastSharedKey(keys);
+        }
+    }
+
+    async list(username) {
+        return this.collection.find({owner: username}, {_id: 0}).toArray();
+    }
+
+    async create(owner, type, value, isGroupDefault=false, groups=[]) {
+        const filter = {owner, type, isGroupDefault, groups};
         const update = {$set: {value}};
         return this.collection.updateOne(filter, update, {upsert: true});
     }
 
-    async delete(username, name) {
-        return this.collection.deleteOne({username, name});
+    async setUsage(owner, value, isGroupDefault, groups) {
+        const filter = {owner, value};
+        const update = {$set: {isGroupDefault, groups}};
+        assert(
+            typeof isGroupDefault === 'boolean',
+            `isGroupDefault must be a boolean! Received ${isGroupDefault}`
+        );
+        assert(
+            groups instanceof Array,
+            `groups must be an Array! Received ${groups}`
+        );
+        return this.collection.updateOne(filter, update);
+    }
+
+    async delete(owner, type, isGroupDefault=false, groups=[]) {
+        return this.collection.deleteOne({owner, type, isGroupDefault, groups});
     }
 
     async all() {
