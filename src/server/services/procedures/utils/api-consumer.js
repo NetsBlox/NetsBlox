43c580b941558a0599532794074c84b9138f4c95
Brian Broll <brian.broll@gmail.com>
2020-03-03 09:42:16 -0600

    Add optional cacheKey in queryOptions. Closes #2692 (#2695)
    * Add optional cacheKey in queryOptions. Closes #2692
    
    * WIP Accept non-strings for cacheKey

--- src/server/services/procedures/utils/api-consumer.js
+++ src/server/services/procedures/utils/api-consumer.js
@@ -46,6 +46,7 @@ class ApiConsumer extends NBService {
     /**
         queryOptions = {
             url,
+            cacheKey,
             path,
             queryString,
             baseUrl,
@@ -104,6 +105,10 @@ class ApiConsumer extends NBService {
     }
 
     _getCacheKey(queryOptions){
+        if (queryOptions.cacheKey) {
+            return typeof queryOptions.cacheKey === 'string' ? queryOptions.cacheKey :
+                JSON.stringify(queryOptions.cacheKey);
+        }
         let parameters = [];
         parameters.push(queryOptions.method || 'GET');
         const fullUrl = this._getFullUrl(queryOptions);
