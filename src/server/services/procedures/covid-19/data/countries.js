cb6a25697cccc88f02032dde8585a4b8fee62cb0
Brian Broll <brian.broll@gmail.com>
2020-03-16 12:43:01 -0500

    Add aliases for rep of congo and DRC. Closes #2721 (#2726)

--- src/server/services/procedures/covid-19/data/countries.js
+++ src/server/services/procedures/covid-19/data/countries.js
@@ -2,7 +2,8 @@ module.exports = {
     US: ['United States', 'United States of America', 'USA'],
     'Korea, South': ['South Korea', 'S. Korea'],
     'Korea, North': ['North Korea', 'N. Korea'],
-    'Congo (Kinshasa)': ['Congo', 'Democratic Republic of the Congo'],
+    'Congo (Kinshasa)': ['Democratic Republic of the Congo'],
+    'Congo (Brazzaville)': ['Republic of the Congo'],
     'United Arab Emirates': ['UAE'],
     'United Kingdom': ['UK'],
 };
