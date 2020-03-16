db1f2703d0f29eeee8db77a4b9260e3846eff53c
Brian Broll <brian.broll@gmail.com>
2020-03-16 10:39:48 -0500

    Resolve "Congo" country name in COVID-19 Service. Fixes #2718

--- src/server/services/procedures/covid-19/data/countries.js
+++ src/server/services/procedures/covid-19/data/countries.js
@@ -2,7 +2,7 @@ module.exports = {
     US: ['United States', 'United States of America', 'USA'],
     'Korea, South': ['South Korea', 'S. Korea'],
     'Korea, North': ['North Korea', 'N. Korea'],
-    'Congo (Kinshasa)': ['Congo'],
+    'Congo (Kinshasa)': ['Congo', 'Democratic Republic of the Congo'],
     'United Arab Emirates': ['UAE'],
     'United Kingdom': ['UK'],
 };
