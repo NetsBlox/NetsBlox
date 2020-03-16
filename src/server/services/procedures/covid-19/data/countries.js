0f0dbe00549858b62a214ac590cb3fdb9abc05ff
Brian Broll <brian.broll@gmail.com>
2020-03-16 10:27:18 -0500

    Add country aliases to COVID-19. Fixes #2716

---
+++ src/server/services/procedures/covid-19/data/countries.js
@@ -0,0 +1,8 @@
+module.exports = {
+    US: ['United States', 'United States of America', 'USA'],
+    'Korea, South': ['South Korea', 'S. Korea'],
+    'Korea, North': ['North Korea', 'N. Korea'],
+    'Congo (Kinshasa)': ['Congo'],
+    'United Arab Emirates': ['UAE'],
+    'United Kingdom': ['UK'],
+};
