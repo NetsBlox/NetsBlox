0037face34dd3f87af2c28d8e73db59b246f0017
Brian Broll <brian.broll@gmail.com>
2020-03-26 23:10:48 -0500

    WIP Update to use daily reports and store in mongo

--- src/server/services/procedures/covid-19/covid-19.js
+++ src/server/services/procedures/covid-19/covid-19.js
@@ -7,11 +7,33 @@
  * @service
  * @category Science
  */
+const logger = require('../utils/logger')('covid-19');
 const _ = require('lodash');
+const getServiceStorage = require('../../advancedStorage');
+const schema = {
+    date: Date,
+    country: String,
+    state: String,
+    city: String,
+    latitude: Number,
+    longitude: Number,
+    confirmed: Number,
+    deaths: Number,
+    recovered: Number,
+};
+const COVID19Storage = getServiceStorage('COVID-19', schema);
 const COVID19 = {};
 COVID19.serviceName = 'COVID-19';
-const data = require('./data');
-const {DEATH, CONFIRMED, RECOVERED} = data.types;
+const Data = require('./data');
+COVID19._data = new Data(COVID19Storage);
+const {DEATH, CONFIRMED, RECOVERED} = COVID19._data.types;
+
+COVID19Storage.findOne({}).then(result => {
+    if (!result) {
+        logger.info('No data found in database, importing latest from JHU...');
+        COVID19._data.importPastData();
+    }
+});
 
 /**
  * Get number of confirmed cases of COVID-19 by date for a specific country and state.
@@ -20,9 +42,10 @@ const {DEATH, CONFIRMED, RECOVERED} = data.types;
  *
  * @param{String} country Country or region
  * @param{String=} state State or province
+ * @param{String=} city City
  */
-COVID19.getConfirmedCounts = async function(country, state='') {
-    return await data.getData(CONFIRMED, country, state);
+COVID19.getConfirmedCounts = async function(country, state='', city='') {
+    return await this._data.getData(CONFIRMED, country, state, city);
 };
 
 /**
@@ -32,9 +55,10 @@ COVID19.getConfirmedCounts = async function(country, state='') {
  *
  * @param{String} country Country or region
  * @param{String=} state State or province
+ * @param{String=} city City
  */
-COVID19.getDeathCounts = async function(country, state='') {
-    return await data.getData(DEATH, country, state);
+COVID19.getDeathCounts = async function(country, state='', city='') {
+    return await this._data.getData(DEATH, country, state);
 };
 
 /**
@@ -44,18 +68,19 @@ COVID19.getDeathCounts = async function(country, state='') {
  *
  * @param{String} country Country or region
  * @param{String=} state State or province
+ * @param{String=} city City
  */
 COVID19.getRecoveredCounts = async function(country, state='') {
-    return await data.getData(RECOVERED, country, state);
+    return await this._data.getData(RECOVERED, country, state);
 };
 
 /**
  * Get a list of all countries (and states) with data available.
  */
 COVID19.getLocationsWithData = function() {
-    const locations = data.getAllData().map(row => {
-        const [state, country] = row;
-        return [country, state];
+    const locations = this._data.getAllData().map(row => {
+        const {state, country, city} = row;
+        return [country, state, city];
     });
     return _.uniq(locations);
 };
@@ -65,11 +90,11 @@ COVID19.getLocationsWithData = function() {
  *
  * @param{String} country
  * @param{String=} state
+ * @param{String=} city City
  */
-COVID19.getLocationCoordinates = function(country, state='') {
-    const row = data.getRow(CONFIRMED, country, state);
-    const [latitude, longitude] = row.slice(2, 4);
-    return {latitude, longitude};
+COVID19.getLocationCoordinates = function(country, state='', city='') {
+    const data = this._data.getRow(CONFIRMED, country, state, city);
+    return _.pick(data, ['latitude', 'longitude']);
 };
 
 module.exports = COVID19;
