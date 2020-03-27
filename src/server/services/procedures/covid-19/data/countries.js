19aa80c1c74cd0b71391ed3ef74b0a0c79b5cfe4
Brian Broll <brian.broll@gmail.com>
2020-03-27 01:20:08 -0500

    Update COVID-19 service to new data format. Fixes #2749 (#2750)
    * WIP Update to use daily reports and store in mongo
    
    * Update getAllLocationsWithData
    
    * WIP resolve country, city aliases
    
    * WIP removed old debug logs
    
    * WIP fixed bugs with checking updates
    
    * Added tests for COVIDData
    
    * WIP removed unused fn
    
    * Updated tests

--- src/server/services/procedures/covid-19/data/countries.js
+++ src/server/services/procedures/covid-19/data/countries.js
@@ -1,9 +1,10 @@
-module.exports = {
-    US: ['United States', 'United States of America', 'USA'],
-    'Korea, South': ['South Korea', 'S. Korea'],
-    'Korea, North': ['North Korea', 'N. Korea'],
-    'Congo (Kinshasa)': ['Democratic Republic of the Congo'],
-    'Congo (Brazzaville)': ['Republic of the Congo'],
-    'United Arab Emirates': ['UAE'],
-    'United Kingdom': ['UK'],
-};
+module.exports = [
+    ['US', 'U.S.', 'United States', 'United States of America', 'USA'],
+    ['Korea, South', 'South Korea', 'S. Korea'],
+    ['Korea, North', 'North Korea', 'N. Korea'],
+    ['Congo (Kinshasa)', 'Democratic Republic of the Congo'],
+    ['Congo (Brazzaville)', 'Republic of the Congo'],
+    ['United Arab Emirates', 'UAE'],
+    ['United Kingdom', 'UK'],
+    ['China', 'Mainland China'],
+];
