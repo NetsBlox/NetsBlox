47ff9a8efdc5310d271a4c38638fb8142440985c
Zhengkai Xie <zhengkai.xie@vanderbilt.edu>
2020-07-07 03:00:51 -0400

    changes to test files

--- test/unit/server/services/procedures/earth-orbit.spec.js
+++ test/unit/server/services/procedures/earth-orbit.spec.js
@@ -2,10 +2,10 @@ describe('earth-orbit', function() {
     const utils = require('../../../../assets/utils');
 
     utils.verifyRPCInterfaces('EarthOrbit', [
-        ['get2004Longitude', ['startyear', 'endyear']],
-        ['get2004Obliquity', ['startyear', 'endyear']],
-        ['get2004Eccentricity', ['startyear', 'endyear']],
-        ['get2004Insolation', ['startyear', 'endyear']],
-        ['get2004Precession', ['startyear', 'endyear']]
+        ['getLongitude', ['startyear', 'endyear']],
+        ['getObliquity', ['startyear', 'endyear']],
+        ['getEccentricity', ['startyear', 'endyear']],
+        ['getInsolation', ['startyear', 'endyear']],
+        ['getPrecession', ['startyear', 'endyear']]
     ]);
 });
