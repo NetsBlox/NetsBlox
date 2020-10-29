e6dc6cf6dd5fbddfebe651ce7df01702b003fd88
Brian Broll <brian.broll@gmail.com>
2020-10-29 10:06:00 -0500

    Add CoreNLP service. Closes #2891 (#2892)
    * Add CoreNLP service. Closes #2891
    
    * Add alpha tag to CoreNLP
    
    * Remove .only

---
+++ src/server/services/procedures/core-nlp/core-nlp.js
@@ -0,0 +1,65 @@
+/**
+ * Use CoreNLP to annotate text.
+ *
+ * For more information, check out https://stanfordnlp.github.io/CoreNLP/
+ *
+ * @alpha
+ * @service
+ * @category Language
+ */
+
+const axios = require('axios');
+const nlp = {};
+
+/*
+ * Get a list of all supported annotators.
+ *
+ * Complete list available at https://stanfordnlp.github.io/CoreNLP/annotators.html
+ */
+nlp.getAnnotators = function() {
+    return [
+        'tokenize',
+        'cleanxml',
+        'docdate',
+        'ssplit',
+        'pos',
+        'lemma',
+        'ner',
+        'entitymentions',
+        'regexner',
+        'tokensregex',
+        'parse',
+        'depparse',
+        'coref',
+        'dcoref',
+        'relation',
+        'natlog',
+        'openie',
+        'entitylink',
+        'kbp',
+        'quote',
+        'quote.attribution',
+        'sentiment',
+        'truecase',
+        'udfeats',
+    ];
+};
+
+/**
+ * Annotate text using the provided annotators.
+ *
+ * @param{String} text
+ * @param{Array<String>=} annotators
+ */
+nlp.annotate = async function(text, annotators=['tokenize', 'ssplit', 'pos']) {
+    const qsData = JSON.stringify({
+        annotators: annotators.join(','),
+        outputFormat: 'json',
+    });
+    const url = `https://corenlp.run/?properties=${qsData}`;
+    const response = await axios.post(url, text);
+    return response.data;
+};
+
+nlp.serviceName = 'CoreNLP';
+module.exports = nlp;
