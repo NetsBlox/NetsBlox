c4367ae13649b6e87dc9285b60e9d8b2b7f7438b
dragazo <37459229+dragazo@users.noreply.github.com>
2020-11-30 18:29:59 -0600

    salio basic support

---
+++ src/server/services/procedures/salio/salio.js
@@ -0,0 +1,299 @@
+/*
+ * Based on the RoboScape procedure.
+ *
+ * Sensor to server messages:
+ *  mac_addr[6] time[4] 'I': identification, sent every second
+ *  mac_addr[6] time[4] 'A' x[4] y[4] z[4]: accelerometer data
+ *
+ * Server to sensor messages:
+ *  'A': get accelerometer snapshot
+ *
+ * Environment variables:
+ *  SALIO_PORT: set it to the UDP port (1975) to enable this module
+ *  SALIO_MODE: sets the NetsBlox interface type, can be "security",
+ *      "native" or "both" (default)
+ */
+
+'use strict';
+
+const logger = require('../utils/logger')('SalIO');
+const Sensor = require('./sensor');
+const acl = require('../roboscape/accessControl');
+var dgram = require('dgram'),
+    server = dgram.createSocket('udp4'),
+    SALIO_MODE = process.env.SALIO_MODE || 'both';
+
+/*
+ * SalIO - This constructor is called on the first
+ * request to an RPC from a given room.
+ * @constructor
+ * @return {undefined}
+ */
+var SalIO = function () {
+    this._state = {
+        registered: {}
+    };
+};
+
+SalIO.serviceName = 'SalIO';
+// keeps a dictionary of sensor objects keyed by mac_addr
+SalIO.prototype._sensors = {};
+
+SalIO.prototype._ensureLoggedIn = function() {
+    if (this.caller.username !== undefined)
+        throw new Error('Login required.');
+};
+
+SalIO.prototype._ensureAuthorized = async function(sensorId) {
+    await acl.ensureAuthorized(this.caller.username, sensorId);
+};
+
+// fetch the sensor and updates its address. creates one if necessary
+SalIO.prototype._getOrCreateSensor = function (mac_addr, ip4_addr, ip4_port) {
+    var sensor = this._sensors[mac_addr];
+    if (!sensor) {
+        logger.log('discovering ' + mac_addr + ' at ' + ip4_addr + ':' + ip4_port);
+        sensor = new Sensor(mac_addr, ip4_addr, ip4_port, server);
+        this._sensors[mac_addr] = sensor;
+    } else {
+        sensor.updateAddress(ip4_addr, ip4_port);
+    }
+    return sensor;
+};
+
+// find the sensor object based on the partial id or returns undefined
+SalIO.prototype._getSensor = async function (sensorId) {
+    sensorId = '' + sensorId;
+    let sensor;
+
+    if(sensorId.length < 4 || sensorId.length > 12) return undefined;
+
+    // autocomplete the sensorId and find the sensor object
+    if (sensorId.length === 12) {
+        sensor = this._sensors[sensorId];
+    } else { // try to guess the rest of the id
+        for (var mac_addr in this._sensors) { // pick the first match
+            if (mac_addr.endsWith(sensorId)) {
+                sensorId = mac_addr;
+                sensor = this._sensors[sensorId];
+            }
+        }
+    }
+
+    // if couldn't find a live sensor
+    if (!sensor) return undefined;
+
+    await this._ensureAuthorized(sensorId);
+    return sensor;
+};
+
+SalIO.prototype._heartbeat = function () {
+    for (var mac_addr in SalIO.prototype._sensors) {
+        var sensor = SalIO.prototype._sensors[mac_addr];
+        if (!sensor.heartbeat()) {
+            logger.log('forgetting ' + mac_addr);
+            delete SalIO.prototype._sensors[mac_addr];
+        }
+    }
+    setTimeout(SalIO.prototype._heartbeat, 1000);
+};
+
+/**
+ * Returns the MAC addresses of the registered sensors for this client.
+ * @returns {array} the list of registered sensors
+ */
+SalIO.prototype._getRegistered = function () {
+    var state = this._state,
+        sensors = [];
+    for (var mac_addr in state.registered) {
+        if (this._sensors[mac_addr].isMostlyAlive()) {
+            sensors.push(mac_addr);
+        } else {
+            delete state.registered[mac_addr];
+        }
+    }
+    return sensors;
+};
+
+/**
+ * Registers for receiving messages from the given sensors.
+ * @param {array} sensors one or a list of sensors
+ * @deprecated
+ */
+SalIO.prototype.eavesdrop = function (sensors) {
+    return this.listen(sensors);
+};
+
+/**
+ * Registers for receiving messages from the given sensors.
+ * @param {array} sensors one or a list of sensors
+ */
+SalIO.prototype.listen = async function (sensors) {
+    var state = this._state;
+
+    for (var mac_addr in state.registered) {
+        if (this._sensors[mac_addr]) {
+            this._sensors[mac_addr].removeClientSocket(this.socket);
+        }
+    }
+    state.registered = {};
+
+    if (!Array.isArray(sensors)) {
+        sensors = ('' + sensors).split(/[, ]/);
+    }
+
+    var ok = true;
+    for (var i = 0; i < sensors.length; i++) {
+        var sensor = await this._getSensor(sensors[i]);
+        if (sensor) {
+            state.registered[sensor.mac_addr] = sensor;
+            sensor.addClientSocket(this.socket);
+        } else {
+            ok = false;
+        }
+    }
+    return ok;
+};
+
+/**
+ * Returns the MAC addresses of all authorized sensors.
+ * @returns {array}
+ */
+SalIO.prototype.getSensors = async function () {
+    const availableSensors = Object.keys(this._sensors);
+    let sensors = await acl.authorizedRobots(this.caller.username, availableSensors);
+    return sensors;
+};
+
+//
+/**
+ * Performs the pre-checks and maps the incoming call to a sensor action.
+ * @param {String} fnName name of the method/function to call on the sensor object
+ * @param {Array} args array of arguments
+ */
+SalIO.prototype._passToSensor = async function (fnName, args) {
+    args = Array.from(args);
+    let sensorId = args.shift();
+    const sensor = await this._getSensor(sensorId);
+    if (sensor && sensor.accepts(this.caller.clientId)) {
+        let rv = sensor[fnName](sensor, args);
+        if (rv === undefined) rv = true;
+        return rv;
+    }
+    return false;
+};
+
+if (SALIO_MODE === 'native' || SALIO_MODE === 'both') {
+    /* eslint-disable no-unused-vars */
+    /**
+     * Returns true if the given sensor is alive, sent messages in the
+     * last two seconds.
+     * @param {string} sensor name of the sensor (matches at the end)
+     * @returns {boolean} True if the sensor is alive
+     */
+    SalIO.prototype.isAlive = function (sensor) {
+        return this._passToSensor('isAlive', arguments);
+    };
+
+    /**
+     * Gets the current accelerometer snapshot from the sensor.
+     * @param {string} sensor name of the sensor (matches at the end)
+     */
+    SalIO.prototype.getAccelerometer = function (sensor) {
+        return this._passToSensor('getAccelerometer', arguments);
+    };
+
+    /**
+     * Sets the total message limit for the given sensor.
+     * @param {string} sensor name of the sensor (matches at the end)
+     * @param {number} rate number of messages per seconds
+     * @returns {boolean} True if the sensor was found
+     */
+    SalIO.prototype.setTotalRate = function (sensor, rate) {
+        return this._passToSensor('setTotalRate', arguments);
+    };
+
+    /**
+     * Sets the client message limit and penalty for the given sensor.
+     * @param {string} sensor name of the sensor (matches at the end)
+     * @param {number} rate number of messages per seconds
+     * @param {number} penalty number seconds of penalty if rate is violated
+     * @returns {boolean} True if the sensor was found
+     */
+    SalIO.prototype.setClientRate = function (sensor, rate, penalty) {
+        return this._passToSensor('setClientRate', arguments);
+    };
+    /* eslint-enable no-unused-vars */
+}
+
+if (SALIO_MODE === 'security' || SALIO_MODE === 'both') {
+    /**
+     * Sends a textual command to the sensor
+     * @param {string} sensor name of the sensor (matches at the end)
+     * @param {string} command textual command
+     * @returns {string} textual response
+     */
+    SalIO.prototype.send = async function (sensor, command) {
+        sensor = await this._getSensor(sensor);
+
+        if (!sensor || typeof command !== 'string') return false;
+
+        // figure out the raw command after processing special methods, encryption, seq and client rate
+        if (command.match(/^backdoor[, ](.*)$/)) { // if it is a backdoor directly set the command
+            command = RegExp.$1;
+            logger.log('executing ' + command);
+        } else { // if not a backdoor handle seq number and encryption
+            // for replay attacks
+            sensor.commandToClient(command);
+
+            if (sensor._hasValidEncryptionSet()) // if encryption is set
+                command = sensor.decrypt(command);
+
+            var seqNum = -1;
+            if (command.match(/^(\d+)[, ](.*)$/)) {
+                seqNum = +RegExp.$1;
+                command = RegExp.$2;
+            }
+            if (!sensor.accepts(this.caller.clientId, seqNum)) {
+                return false;
+            }
+            sensor.setSeqNum(seqNum);
+        }
+
+        return sensor.onCommand(command);
+    };
+}
+
+server.on('listening', function () {
+    var local = server.address();
+    logger.log('listening on ' + local.address + ':' + local.port);
+});
+
+server.on('message', function (message, remote) {
+    if (message.length < 6) {
+        logger.log('invalid message ' + remote.address + ':' +
+            remote.port + ' ' + message.toString('hex'));
+    } else {
+        var mac_addr = message.toString('hex', 0, 6); // pull out the mac address
+        var sensor = SalIO.prototype._getOrCreateSensor(
+            mac_addr, remote.address, remote.port);
+        sensor.onMessage(message);
+    }
+});
+
+/* eslint no-console: off */
+if (process.env.SALIO_PORT) {
+    console.log('SALIO_PORT is ' + process.env.SALIO_PORT);
+    server.bind(process.env.SALIO_PORT || 1975);
+
+    setTimeout(SalIO.prototype._heartbeat, 1000);
+}
+
+SalIO.isSupported = function () {
+    if (!process.env.SALIO_PORT) {
+        console.log('SALIO_PORT is not set (to 1975), SalIO is disabled');
+    }
+    return !!process.env.SALIO_PORT;
+};
+
+module.exports = SalIO;
---
+++ src/server/services/procedures/salio/sensor.js
@@ -0,0 +1,469 @@
+'use strict';
+const getRPCLogger = require('../utils/logger');
+const acl = require('../roboscape/accessControl');
+const SALIO_MODE = process.env.SALIO_MODE || 'both';
+const ciphers = require('../roboscape/ciphers');
+
+// these might be better defined as an attribute on the sensor
+const FORGET_TIME = 120; // forgetting a sensor in seconds
+const RESPONSE_TIMEOUT = 500; // ms
+
+var Sensor = function (mac_addr, ip4_addr, ip4_port, aServer) {
+    this.id = mac_addr;
+    this.mac_addr = mac_addr;
+    this.ip4_addr = ip4_addr;
+    this.ip4_port = ip4_port;
+    this.heartbeats = 0;
+    this.timestamp = -1; // time of last message in sensor time
+    this.sockets = []; // uuids of sockets of registered clients
+    this.callbacks = {}; // callbacks keyed by msgType
+    this.encryptionKey = [0]; // encryption key
+    this.encryptionMethod = ciphers.caesar; // backward compat
+    this.buttonDownTime = 0; // last time button was pressed
+    // rate control
+    this.totalCount = 0; // in messages per second
+    this.totalRate = 0; // in messages per second
+    this.clientRate = 0; // in messages per second
+    this.clientPenalty = 0; // in seconds
+    this.clientCounts = {};
+    this.lastSeqNum = -1; // initially disabled
+    this._logger = getRPCLogger(`salio:${mac_addr}`);
+    this.server = aServer; // a handle to the udp server for communication with the sensor
+};
+
+Sensor.prototype.setTotalRate = function (rate) {
+    this._logger.log('set total rate ' + this.mac_addr + ' ' + rate);
+    this.totalRate = Math.max(rate, 0);
+};
+
+Sensor.prototype.setClientRate = function (rate, penalty) {
+    this._logger.log('set client rate ' + this.mac_addr + ' ' + rate + ' ' + penalty);
+    this.clientRate = Math.max(rate, 0);
+    this.clientPenalty = Math.min(Math.max(penalty, 0), 60);
+};
+
+Sensor.prototype.resetRates = function () {
+    this._logger.log('reset rate limits');
+    this.totalRate = 0;
+    this.clientRate = 0;
+    this.clientPenalty = 0;
+    this.clientCounts = {};
+};
+
+// resets the encryption
+// for backward compat sets it to caesar cipher with key [0]
+Sensor.prototype.resetEncryption = function () {
+    this._logger.log('resetting encryption');
+    // null would make more sense but keeping backward compatibility here
+    this.encryptionMethod = ciphers.caesar;
+    this.encryptionKey = [0];
+};
+
+Sensor.prototype.resetSeqNum = function () {
+    this._logger.log('resetting seq numbering');
+    this.setSeqNum(-1);
+};
+
+Sensor.prototype.updateAddress = function (ip4_addr, ip4_port) {
+    this.ip4_addr = ip4_addr;
+    this.ip4_port = ip4_port;
+    this.heartbeats = 0;
+};
+
+Sensor.prototype.setSeqNum = function (seqNum) {
+    this.lastSeqNum = seqNum;
+};
+
+Sensor.prototype.accepts = function (clientId, seqNum) {
+    if (this.lastSeqNum >= 0 && (seqNum <= this.lastSeqNum ||
+            seqNum > this.lastSeqNum + 100)) {
+        return false;
+    }
+
+    var client = this.clientCounts[clientId];
+    if (!client) {
+        client = {
+            count: 0,
+            penalty: 0
+        };
+        this.clientCounts[clientId] = client;
+    }
+
+    if (client.penalty > 0) {
+        this._logger.log(clientId + ' client penalty');
+        return false;
+    }
+
+    if (this.clientRate !== 0 && client.count + 1 > this.clientRate) {
+        this._logger.log(clientId + ' client rate violation');
+        client.penalty = 1 + this.clientPenalty;
+        return false;
+    }
+
+    if (this.totalRate !== 0 && this.totalCount + 1 > this.totalRate) {
+        this._logger.log(clientId + ' total rate violation');
+        return false;
+    }
+
+    this.totalCount += 1;
+    client.count += 1;
+    return true;
+};
+
+Sensor.prototype.heartbeat = function () {
+    this.totalCount = 0;
+    for (var id in this.clientCounts) {
+        var client = this.clientCounts[id];
+        client.count = 0;
+        if (client.penalty > 1) {
+            client.count = 0;
+            client.penalty -= 1;
+        } else {
+            delete this.clientCounts[id];
+        }
+    }
+
+    this.heartbeats += 1;
+    if (this.heartbeats >= FORGET_TIME) {
+        return false;
+    }
+    return true;
+};
+
+Sensor.prototype.isAlive = function () {
+    return this.heartbeats <= 2;
+};
+
+Sensor.prototype.isMostlyAlive = function () {
+    return this.heartbeats <= FORGET_TIME;
+};
+
+Sensor.prototype.addClientSocket = function (socket) {
+    const {clientId} = socket;
+    var i = this.sockets.findIndex(s => s.clientId === clientId);
+    if (i < 0) {
+        this._logger.log('register ' + clientId + ' ' + this.mac_addr);
+        this.sockets.push(socket);
+        return true;
+    }
+    return false;
+};
+
+Sensor.prototype.removeClientSocket = function (socket) {
+    const {clientId} = socket;
+    var i = this.sockets.findIndex(s => s.clientId === clientId);
+    if (i >= 0) {
+        this._logger.log('unregister ' + clientId + ' ' + this.mac_addr);
+        this.sockets.splice(i, 1);
+        return true;
+    }
+    return false;
+};
+
+Sensor.prototype.sendToSensor = function (message) {
+    this.server.send(message, this.ip4_port, this.ip4_addr, function (err) {
+        if (err) {
+            this._logger.log('send error ' + err);
+        }
+    });
+};
+
+Sensor.prototype.receiveFromSensor = function (msgType, timeout) {
+    if (!this.callbacks[msgType]) {
+        this.callbacks[msgType] = [];
+    }
+    var callbacks = this.callbacks[msgType];
+
+    return new Promise(function (resolve) {
+        callbacks.push(resolve);
+        setTimeout(function () {
+            var i = callbacks.indexOf(resolve);
+            if (i >= 0) {
+                callbacks.splice(i, 1);
+            }
+            resolve(false);
+        }, timeout || RESPONSE_TIMEOUT);
+    });
+};
+
+Sensor.prototype.getAccelerometer = async function () {
+    this._logger.log('get accelerometer ' + this.mac_addr);
+    const response = this.receiveFromSensor('accelerometer');
+    const message = Buffer.alloc(1);
+    message.write('A', 0, 1);
+    this.sendToSensor(message);
+    const resp = await response;
+    return {x:resp.x, y:resp.y, z:resp.z};   
+};
+
+Sensor.prototype.commandToClient = function (command) {
+    if (SALIO_MODE === 'security' || SALIO_MODE === 'both') {
+        var mac_addr = this.mac_addr;
+        this.sockets.forEach(socket => {
+            const content = {
+                sensor: mac_addr,
+                command: command
+            };
+            socket.sendMessage('sensor command', content);
+        });
+    }
+};
+
+Sensor.prototype.sendToClient = function (msgType, content) {
+    var myself = this;
+
+    let fields = ['time', ...Object.keys(content)];
+    content.sensor = this.mac_addr;
+    content.time = this.timestamp;
+
+    if (msgType !== 'set led') {
+        this._logger.log('event ' + msgType + ' ' + JSON.stringify(content));
+    }
+
+    if (this.callbacks[msgType]) {
+        var callbacks = this.callbacks[msgType];
+        delete this.callbacks[msgType];
+        callbacks.forEach(function (callback) {
+            callback(content);
+        });
+        callbacks.length = 0;
+    }
+
+    this.sockets.forEach(async socket => {
+        await acl.ensureAuthorized(socket.username, myself.mac_addr); // should use sensorId instead of mac_addr
+
+        if (SALIO_MODE === 'native' || SALIO_MODE === 'both') {
+            socket.sendMessage(msgType, content);
+        }
+
+        if ((SALIO_MODE === 'security' && msgType !== 'set led') ||
+            SALIO_MODE === 'both') {
+            var text = msgType;
+            for (var i = 0; i < fields.length; i++) {
+                text += ' ' + content[fields[i]];
+            }
+
+            const encryptedContent = {
+                sensor: myself.mac_addr,
+                message: this._hasValidEncryptionSet() ? myself.encrypt(text.trim()) : text.trim()
+            };
+            socket.sendMessage('sensor message', encryptedContent);
+        }
+    });
+};
+
+// used for handling incoming message from the sensor
+Sensor.prototype.onMessage = function (message) {
+    if (message.length < 11) {
+        this._logger.log('invalid message ' + this.ip4_addr + ':' + this.ip4_port +
+            ' ' + message.toString('hex'));
+        return;
+    }
+
+    var oldTimestamp = this.timestamp;
+    this.timestamp = message.readUInt32LE(6);
+    var command = message.toString('ascii', 10, 11);
+    var state;
+
+    if (command === 'I' && message.length === 11) {
+        if (this.timestamp < oldTimestamp) {
+            this._logger.log('sensor was rebooted ' + this.mac_addr);
+            this.resetSeqNum();
+            this.setEncryptionKey([]);
+            this.resetRates();
+        }
+    } else if (command === 'W' && message.length === 12) {
+        state = message.readUInt8(11);
+        this.sendToClient('whiskers', {
+            left: (state & 0x2) == 0,
+            right: (state & 0x1) == 0
+        });
+    } else if (command === 'P' && message.length === 12) {
+        state = message.readUInt8(11) == 0;
+        if (SALIO_MODE === 'native' || SALIO_MODE === 'both') {
+            this.sendToClient('button', {
+                pressed: state
+            });
+        }
+        if (SALIO_MODE === 'security' || SALIO_MODE === 'both') {
+            if (state) {
+                this.buttonDownTime = new Date().getTime();
+                setTimeout(function (sensor, pressed) {
+                    if (sensor.buttonDownTime === pressed) {
+                        sensor.resetSensor();
+                    }
+                }, 1000, this, this.buttonDownTime);
+            } else {
+                if (new Date().getTime() - this.buttonDownTime < 1000) {
+                    this.randomEncryption();
+                }
+                this.buttonDownTime = 0;
+            }
+        }
+    } else if (command === 'A' && message.length === 23) {
+        this.sendToClient('accelerometer', {
+            x: message.readFloatBE(11),
+            y: message.readFloatBE(15),
+            z: message.readFloatBE(19),
+        });
+    } else {
+        this._logger.log('unknown ' + this.ip4_addr + ':' + this.ip4_port +
+            ' ' + message.toString('hex'));
+    }
+};
+
+// handle user commands to the sensor (through the 'send' rpc)
+Sensor.prototype.onCommand = function(command) {
+    const cases = [
+        {
+            regex: /^is alive$/,
+            handler: () => {
+                this.sendToClient('alive', {});
+                return this.isAlive();
+            }
+        },
+        {
+            regex: /^get accelerometer$/,
+            handler: () => {
+                return this.getVitals();
+            }
+        },
+        {
+            regex: /^set encryption ([^ ]+)(| -?\d+([ ,]-?\d+)*)$/, // name of the cipher
+            handler: () => {
+                let cipherName = RegExp.$1.toLowerCase();
+                var key = RegExp.$2.split(/[, ]/);
+                if (key[0] === '') {
+                    key.splice(0, 1);
+                }
+                return this.setEncryptionMethod(cipherName) && this.setEncryptionKey(key);
+            }
+        },
+        { // deprecated
+            regex: /^set key(| -?\d+([ ,]-?\d+)*)$/,
+            handler: () => {
+                var encryption = RegExp.$1.split(/[, ]/);
+                if (encryption[0] === '') {
+                    encryption.splice(0, 1);
+                }
+                return this.setEncryptionKey(encryption.map(Number));
+            }
+        },
+        {
+            regex: /^set total rate (-?\d+)$/,
+            handler: () => {
+                this.setTotalRate(+RegExp.$1);
+            }
+        },
+        {
+            regex: /^set client rate (-?\d+)[, ](-?\d+)$/,
+            handler: () => {
+                this.setClientRate(+RegExp.$1, +RegExp.$2);
+            }
+        },
+        {
+            regex: /^reset seq$/,
+            handler: () => {
+                this.resetSeqNum();
+            }
+        },
+        {
+            regex: /^reset rates$/,
+            handler: () => {
+                this.resetRates();
+            }
+        },
+    ];
+
+    let matchingCase = cases.find(aCase => command.match(aCase.regex));
+    if (!matchingCase) return false; // invalid command structure
+
+    let rv = matchingCase.handler();
+    if (rv === undefined) rv = true;
+    return rv;
+};
+
+// determines whether encryption/decryption can be activated or not
+Sensor.prototype._hasValidEncryptionSet = function () {
+    let verdict = (this.encryptionKey && this.encryptionMethod && Array.isArray(this.encryptionKey) && this.encryptionKey.length !== 0);
+    return verdict;
+};
+
+Sensor.prototype.encrypt = function (text) {
+    if (!this._hasValidEncryptionSet()) {
+        throw new Error('invalid encryption setup');
+    }
+    let output = this.encryptionMethod.encrypt(text, this.encryptionKey);
+    this._logger.log('"' + text + '" encrypted to "' + output + '"');
+    return output;
+};
+
+Sensor.prototype.decrypt = function (text) {
+    if (!this._hasValidEncryptionSet()) {
+        throw new Error('invalid encryption setup');
+    }
+    let output = this.encryptionMethod.decrypt(text, this.encryptionKey);
+    this._logger.log('"' + text + '" decrypted to "' + output + '"');
+    return output;
+};
+
+// disable encryption and decryption with minimal changes
+Sensor.prototype.disableEncryption = function () {
+    this.encryptionMethod = ciphers.plain;
+};
+
+Sensor.prototype.setEncryptionMethod = function (name) {
+    if (!ciphers[name]) {
+        this._logger.warn('invalid cipher name ' + name);
+        return false;
+    }
+    this._logger.log('setting cipher to ' + name);
+
+    this.encryptionMethod = ciphers[name];
+    return true;
+};
+
+// WARN keys number?
+Sensor.prototype.setEncryptionKey = function (keys) {
+    if (!this.encryptionMethod) {
+        this._logger.warn('setting the key without a cipher ' + keys);
+        return false;
+    } else if (keys instanceof Array) { // all the supported ciphers require an array of numbers
+        keys = keys.map(num => parseInt(num));
+        this.encryptionKey = keys;
+        this._logger.log(this.mac_addr, 'encryption key set to', keys);
+        return true;
+    } else {
+        this._logger.warn('invalid encryption key ' + keys);
+        return false;
+    }
+};
+
+Sensor.prototype.randomEncryption = function () {
+    var keys = [],
+        blinks = [];
+    for (var i = 0; i < 4; i++) {
+        var a = Math.floor(Math.random() * 16);
+        keys.push(a);
+        blinks.push(a & 0x8 ? 2 : 1);
+        blinks.push(a & 0x4 ? 2 : 1);
+        blinks.push(a & 0x2 ? 2 : 1);
+        blinks.push(a & 0x1 ? 2 : 1);
+    }
+    blinks.push(3);
+    this.resetSeqNum();
+    this.resetRates();
+    this.setEncryptionKey(keys);
+    this.playBlinks(blinks);
+};
+
+// resets encryption, sequencing, and rate limits
+Sensor.prototype.resetSensor = function () {
+    this._logger.log('resetting sensor');
+    this.resetSeqNum();
+    this.resetRates();
+    this.resetEncryption();
+    this.playBlinks([3]);
+};
+
+module.exports = Sensor;
