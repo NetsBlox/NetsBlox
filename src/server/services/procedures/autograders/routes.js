5e580c096bc64fdf60f72aafdd8f9b774599139b
Brian Broll <brian.broll@gmail.com>
2020-12-08 17:09:48 -0600

    Add initial version of Autograders service. Closes #2925

---
+++ src/server/services/procedures/autograders/routes.js
@@ -0,0 +1,30 @@
+const fs = require('fs');
+const path = require('path');
+const AutograderCode = fs.readFileSync(path.join(__dirname, 'template.ejs'), 'utf8');
+const getDatabase = require('./storage');
+
+module.exports = [
+    {  // fetch autograder
+        URL: 'autograders/:name.js',
+        Method: 'get',
+        Handler: async function(req, res) {
+            const {name} = req.params;
+            const storage = getDatabase();
+            const autograder = await storage.findOne({name});
+            if (!autograder) {
+                return res.sendStatus(404);
+            }
+
+            try {
+                const code = AutograderCode.replace(
+                    'AUTOGRADER_CONFIG',
+                    JSON.stringify(autograder.config)
+                );
+                return res.send(code);
+            } catch (err) {
+                console.log(err.stack);
+                return res.send(err.message);
+            }
+        }
+    },
+];
