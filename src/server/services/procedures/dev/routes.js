fc4d051ad42f150748b8d91e1e78ab681f4c96ee
Brian Broll <brian.broll@gmail.com>
2021-01-28 09:17:48 -0600

    status code = 400 if invalid address

--- src/server/services/procedures/dev/routes.js
+++ src/server/services/procedures/dev/routes.js
@@ -9,11 +9,16 @@ router.post(
     bodyParser.json({limit: '1mb'}),
     async (req, res) => {
         const {address, messageType, data} = req.body;
-        const resolvedAddr = await NetsBloxAddress.new(address);
-        const client = new RemoteClient(resolvedAddr.projectId);
-        await client.sendMessageToRoom(messageType, data);
-        
-        res.sendStatus(200);
+        const resolvedAddr = await NetsBloxAddress.new(address)
+            .catch(err => {
+                res.status(400).send(err.message);
+            });
+
+        if (resolvedAddr) {
+            const client = new RemoteClient(resolvedAddr.projectId);
+            await client.sendMessageToRoom(messageType, data);
+            res.sendStatus(200);
+        }
     }
 );
 
