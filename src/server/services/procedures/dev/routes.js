ebd9da243e237a07fdac03bb14fd3c77315af3a2
Brian Broll <brian.broll@gmail.com>
2021-01-28 09:10:21 -0600

    Add example message-sending route to dev. Closes #2995

---
+++ src/server/services/procedures/dev/routes.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const router = express();
+const NetsBloxAddress = require('../../../netsblox-address');
+const RemoteClient = require('../../remote-client');
+const bodyParser = require('body-parser');
+
+router.post(
+    '/services/dev/send/',
+    bodyParser.json({limit: '1mb'}),
+    async (req, res) => {
+        const {address, messageType, data} = req.body;
+        const resolvedAddr = await NetsBloxAddress.new(address);
+        const client = new RemoteClient(resolvedAddr.projectId);
+        await client.sendMessageToRoom(messageType, data);
+        
+        res.sendStatus(200);
+    }
+);
+
+module.exports = router;
