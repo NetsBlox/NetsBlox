77e78af433569bcac3fe261c6ecb0a55151adc02
Brian Broll <brian.broll@gmail.com>
2021-02-08 18:12:06 -0600

    Test update again

--- src/server/services/procedures/alexa/alexa.js
+++ src/server/services/procedures/alexa/alexa.js
@@ -5,7 +5,7 @@ const Alexa = {};
 
 /**
  * Return the caller info as detected by the server.
- * This was added to test the github action :)
+ * This was added to test the github action and is hopefully useful! :)
  */
 Alexa.callerInfo = function() {
     return _.omit(this.caller, ['response', 'request', 'socket', 'apiKey']);
