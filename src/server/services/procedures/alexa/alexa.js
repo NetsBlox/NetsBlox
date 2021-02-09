687c42ae16eae0d45c0880eee8fd5dc24fb7fcf3
Brian Broll <brian.broll@gmail.com>
2021-02-08 18:06:26 -0600

    Add (and test) github deployment action

--- src/server/services/procedures/alexa/alexa.js
+++ src/server/services/procedures/alexa/alexa.js
@@ -1,4 +1,14 @@
+const _ = require('lodash');
+
 const Alexa = {};
 // TODO: Add RPCs
+
+/**
+ * Return the caller info as detected by the server.
+ * This was added to test the github action :)
+ */
+Alexa.callerInfo = function() {
+    return _.omit(this.caller, ['response', 'request', 'socket', 'apiKey']);
+};
 
 module.exports = Alexa;
