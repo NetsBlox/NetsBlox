363d87534dfc6ccbd376c1668ee68bf4f02c696b
tabithasylee <tabitha.see.ya.lee@vanderbilt.edu>
2021-03-12 22:54:52 -0600

    Adding logging line for debugging

--- src/server/services/procedures/alexa/routes.js
+++ src/server/services/procedures/alexa/routes.js
@@ -207,9 +207,10 @@ if (require.main === module) {
         // The following two lines are a workaround to bypass authentication
         // (only to make development more convenient) and should be removed
         // before this is actually used.
-        /*devLogger.log(`Bypassing authentication and setting user to tabithalee (${req.method})`);
+        devLogger.log(`Bypassing authentication and setting user to tabithalee (${req.method})`);
         req.token = {username: 'tabithalee'};  // FIXME: REMOVE
-        return next();  // FIXME: REMOVE!*/
+        devLogger.log("netsblox-side token: " + req.token);
+        return next();  // FIXME: REMOVE!
 
         const authCode = req.get('Authorization');
         if (!authCode) {
