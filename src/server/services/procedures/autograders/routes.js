4fa078ed9cd65b23347a497b4ce132758678ae5a
Brian Broll <brian.broll@gmail.com>
2021-03-16 13:47:37 -0500

    Update Autograder routes. Closes #3052

--- src/server/services/procedures/autograders/routes.js
+++ src/server/services/procedures/autograders/routes.js
@@ -2,24 +2,25 @@ const fs = require('fs');
 const path = require('path');
 const AutograderCode = fs.readFileSync(path.join(__dirname, 'template.ejs'), 'utf8');
 const getDatabase = require('./storage');
+const express = require('express');
+const router = express();
 
-module.exports = [
-    {  // fetch autograder
-        URL: 'autograders/:author/:name.js',
-        Method: 'get',
-        Handler: async function(req, res) {
-            const {author, name} = req.params;
-            const storage = getDatabase();
-            const autograder = await storage.findOne({author, name});
-            if (!autograder) {
-                return res.sendStatus(404);
-            }
-
-            const code = AutograderCode.replace(
-                'AUTOGRADER_CONFIG',
-                JSON.stringify(autograder.config)
-            );
-            return res.send(code);
+router.get(
+    '/:author/:name.js',
+    async (req, res) => {
+        const {author, name} = req.params;
+        const storage = getDatabase();
+        const autograder = await storage.findOne({author, name});
+        if (!autograder) {
+            return res.sendStatus(404);
         }
-    },
-];
+
+        const code = AutograderCode.replace(
+            'AUTOGRADER_CONFIG',
+            JSON.stringify(autograder.config)
+        );
+        return res.send(code);
+    }
+);
+
+module.exports = router;
