2af1e52b59d44a73ca63b1ae136d7f794e099b1a
tabithasylee <tabitha.see.ya.lee@vanderbilt.edu>
2021-03-27 16:54:31 -0500

    Adding basic RPCs, testing obtaining LWA tokens

--- src/server/services/procedures/alexa/alexa.js
+++ src/server/services/procedures/alexa/alexa.js
@@ -1,7 +1,89 @@
 const _ = require('lodash');
 
 const Alexa = {};
-// TODO: Add RPCs
+
+const AlexaSMAPI = require('ask-smapi-sdk');
+
+var spawnSync = require('child_process').spawnSync;
+
+var clientID = process.env.ALEXA_CLIENT_ID,
+    clientSecret = process.env.ALEXA_CLIENT_SECRET;
+
+var result = spawnSync('ask util generate-lwa-tokens',
+    ['--client-id', clientID, '--client-confirmation', clientSecret]);
+
+if (result.status === 0) {
+    var parseTokens = result.stdout;
+    devLogger.log(parseTokens);
+}
+
+//temp
+var refreshToken = "";
+var vendorID = "";
+
+const refreshTokenConfig = {
+    clientID,
+    clientSecret,
+    refreshToken,
+};
+
+//creates SMAPI client
+const smapiClient = new AlexaSMAPI.StandardSmapiClientBuilder()
+    .withRefreshTokenConfig(refreshTokenConfig)
+    .client();
+
+
+//basic listSkills RPC
+Alexa.listSkills = function() {
+    smapiClient.listSkillsForVendorV1(vendorID)
+        .then((response) => {
+            return (JSON.stringify(response));
+        })
+        .catch((err) => {
+            devLogger.log(err.message);
+            devLogger.log(JSON.stringify(err.response));
+        });
+};
+
+//gets skill Info
+Alexa.getSkillInfo = function(skillId, stage) {
+    smapiClient.getSkillManifestV1(skillId, stage)
+        .then((response) => {
+            return (JSON.stringify(response));
+        })
+        .catch((err) => {
+            devLogger.log(err.message);
+            devLogger.log(JSON.stringify(err.response));
+        });
+};
+/*
+Alexa.createSkill = function(description, examplePhrases, keywords, name) {
+    var skillManifest =
+        {
+            'publishingInformation':
+            {
+                'locales':
+                    {
+                        'en-US':
+                            {
+                                'summary': description,
+                                'examplePhrases': examplePhrases,
+                                'keywords': keywords,
+                                'name': name,
+                            }
+                    }
+            }
+        };
+    var skillRequest = {'vendorId' : process.env.ALEXA_VENDOR_ID, 'manifest' : skillManifest};
+    smapiClient.createSkillForVendorV1(skillRequest)
+        .then((response) => {
+            return (JSON.stringify(response));
+        })
+        .catch((err) => {
+            devLogger(err.message);
+            return (JSON.stringify(err.response));
+        });
+};*/
 
 /**
  * Return the caller info as detected by the server.
