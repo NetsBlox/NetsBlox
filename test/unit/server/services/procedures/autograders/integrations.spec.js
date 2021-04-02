0f1228d7a754f4eedfe3ab76869f06f6bbde8ded
Brian Broll <brian.broll@gmail.com>
2021-04-02 11:42:58 -0500

    validate autograder integrations

---
+++ test/unit/server/services/procedures/autograders/integrations.spec.js
@@ -0,0 +1,39 @@
+const utils = require('../../../../../assets/utils');
+
+describe.only(utils.suiteName(__filename), function() {
+    const Integrations = utils.reqSrc('services/procedures/autograders/integrations');
+    const {CourseraKeyError} = Integrations.errors;
+    const assert = require('assert');
+
+    describe('get', function() {
+        it('should throw error for invalid integrations', function() {
+            assert.throws(() => Integrations.get('notCoursera'));
+        });
+    });
+
+    describe('coursera', function() {
+        it('should get integration', function() {
+            const integration = Integrations.get('coursera');
+            assert(integration);
+        });
+
+        it('should ensure assignments have assignment key', async function() {
+            const integration = Integrations.get('coursera');
+            const name = 'assignment 1';
+            assert.throws(
+                () => integration.validate({assignments: [{name}]}),
+                CourseraKeyError
+            );
+        });
+
+        it('should ensure assignments have assignment key', async function() {
+            const integration = Integrations.get('coursera');
+            assert.throws(
+                () => integration.validate({assignments: [
+                    {tests: [{}]}
+                ]}),
+                CourseraKeyError
+            );
+        });
+    });
+});
