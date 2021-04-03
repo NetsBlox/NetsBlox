41702be617e28f7c4d07699fca5013513fc62a7e
Brian Broll <brian.broll@gmail.com>
2021-04-03 12:59:17 -0500

    Add coursera integration for autograding. Closes #2933 (#3065)
    * WIP add coursera integration for autograding
    
    * Switch to axios; Update service URL
    
    * Refactor Coursera credentials prompt; remember username, secret
    
    * Update autograder coursera submission
    
    * output field must be string (required by coursera)
    
    * validate autograder integrations
    
    * Clean up coursera submission
    
    * Remove .only
    
    * Remove extra comments

--- src/server/services/procedures/autograders/routes.js
+++ src/server/services/procedures/autograders/routes.js
@@ -4,6 +4,7 @@ const AutograderCode = fs.readFileSync(path.join(__dirname, 'template.ejs'), 'ut
 const getDatabase = require('./storage');
 const express = require('express');
 const router = express();
+const rp = require('request-promise');
 
 router.get(
     '/:author/:name.js',
@@ -20,6 +21,30 @@ router.get(
             JSON.stringify(autograder.config)
         );
         return res.send(code);
+    }
+);
+
+router.post(
+    '/submit/coursera',
+    async (req, res) => {
+        const url = 'https://www.coursera.org/api/onDemandProgrammingScriptSubmissions.v1';
+        try {
+            const response = await rp({
+                uri: url,
+                method: 'POST',
+                json: req.body,
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Cache-Control': 'no-cache'
+                },
+            });
+            return res.send(response);
+        } catch (err) {
+            const isStatusCodeError = err.name === 'StatusCodeError';
+            const status = isStatusCodeError ? err.statusCode : 500;
+            const message = isStatusCodeError ? err.error.message : `An error occurred: ${err.message}`;
+            return res.status(status).send(message);
+        }
     }
 );
 
