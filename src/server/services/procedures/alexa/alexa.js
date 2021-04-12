d0f52e1bdb15c587ea6d4e77e5e978712fbab8c4
tabithasylee <tabitha.see.ya.lee@vanderbilt.edu>
2021-04-12 01:21:10 -0500

    Testing interaction model creation helpers

--- src/server/services/procedures/alexa/alexa.js
+++ src/server/services/procedures/alexa/alexa.js
@@ -81,9 +81,7 @@ Alexa.getSkillInfo = async function(skillId, stage) {
     return response;
 };
 
-
-//untested createSkill RPC
-Alexa.createSkill = function(summary, description, examplePhrases, keywords, name) {
+Alexa.createManifest = function(summary, description, examplePhrases, keywords, name) {
     validateList(examplePhrases);
     validateList(keywords);
     var skillRequest =
@@ -127,21 +125,142 @@ Alexa.createSkill = function(summary, description, examplePhrases, keywords, nam
             }
         };
 
-    devLogger.log(JSON.stringify(skillRequest));
-    const response = smapiClient.createSkillForVendorV1(skillRequest);
+    return skillRequest;
+};
+
+Alexa.updateSkillManifest = async function(skillId, stage, manifest) {
+    const response = await smapiClient.updateSkillManifestV1(skillId, stage, manifest);
+    devLogger.log(JSON.stringify(response));
+
+    return response;
+};
+
+//untested createSkill RPC
+Alexa.createSkill = async function(manifest) {
+    const response = await smapiClient.createSkillForVendorV1(manifest);
     devLogger.log(JSON.stringify(response));
 
     return response;
 };
 
 //get interaction model of skill
-Alexa.getInteractionModel = function (skillId, stage) {
-    const response = smapiClient.getInteractionModelV1(skillId, stage, 'en-US');
+Alexa.getInteractionModel = async function (skillId, stage) {
+    const response = await smapiClient.getInteractionModelV1(skillId, stage, 'en-US');
     devLogger.log(JSON.stringify(response));
 
     return response;
 };
 
+Alexa.createSlot = function(intent, name, samples, prompts) {
+    validateList(prompts);
+    var slotInfo =
+        {
+            "intentSlotInfo" : {
+                "name": name,
+                "type": "AMAZON.SearchQuery",
+                "samples": samples,
+            },
+            "slotInfo" : {
+                "name": name,
+                "type": "AMAZON.SearchQuery",
+                "confirmationRequired": false,
+                "elicitationRequired": true,
+                "prompts": {
+                    "elicitation": "Elicit.Intent-:" + intent + ".IntentSlot-" + name
+                }
+            },
+            "promptInfo" : {
+                "id": "Elicit.Intent-:" + intent + ".IntentSlot-" + name,
+                "variations": []
+            }
+        };
+
+    for (let i in prompts) {
+        slotInfo.push.promptInfo.variations(
+            {
+                "type": "PlainText",
+                "value": i
+            }
+        );
+    }
+    devLogger.log(slotInfo);
+
+    return slotInfo;
+};
+
+Alexa.createIntent = function (name, slots, samples) {
+    validateList(samples);
+
+    var intent =
+        {
+            "name": name,
+            "slots": [],
+            "samples": samples
+        };
+
+    for (let i in slots) {
+        intent.slots.push(i.intentSlotInfo);
+    }
+    devLogger.log(intent);
+
+    return intent;
+};
+
+Alexa.createInteractionModel = async function (skillId, stage, intents) {
+    var intentsArray = [];
+    var dialogArray =
+        {
+            "intents" : [],
+            "prompts" : [],
+        };
+    for (let i of intents) {
+        for (let j of i.slots) {
+            dialogArray.intents.push(
+                {
+                    "name": "GetTravelTime",
+                    "confirmationRequired": false,
+                    "prompts": {},
+                    "slots": j.slotInfo
+                }
+            );
+            dialogArray.prompts.push(j.promptInfo);
+        }
+        intentsArray.push(
+            {
+                "name": i.name,
+                "slots": i.slots,
+                "samples": i.samples
+            }
+        );
+    }
+
+    var interactionModel =
+        {
+            "interactionModel": {
+                "languageModel": {
+                    "invocationName": name,
+                    "modelConfiguration": {
+                        "fallbackIntentSensitivity": {
+                            "level": "LOW"
+                        }
+                    },
+                    "intents": intentsArray,
+                },
+                "dialog": {
+                    "intents": dialogArray.intents,
+                    "delegationStrategy": "ALWAYS"
+                },
+                "prompts": dialogArray.prompts
+            }
+        };
+
+    devLogger.log(interactionModel);
+
+    const response = await smapiClient.setInteractionModelV1(skillId, stage, 'en-US', interactionModel);
+    devLogger.log(response);
+
+    return response;
+};
 
 /**
  * Return the caller info as detected by the server.
