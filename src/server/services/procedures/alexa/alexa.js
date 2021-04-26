7ef37925ea6613bc83f524d945743c79495f1f7f
tabithasylee <tabitha.see.ya.lee@vanderbilt.edu>
2021-04-25 23:52:05 -0500

    Test sample temporary model

--- src/server/services/procedures/alexa/alexa.js
+++ src/server/services/procedures/alexa/alexa.js
@@ -292,15 +292,18 @@ Alexa.createInteractionModel = async function (skillId, stage, intents, invocati
         );
     }
 
+    let j = 0;
+
     for (let i of slotInfos) {
         intentsSlots.push(
             {
-                "name": "GetTravelTime",
+                "name": intentsArray[j].name,
                 "confirmationRequired": false,
                 "prompts": {},
                 "slots": i
             }
         );
+        j++;
     }
 
     for (let i of promptInfos) {
@@ -311,19 +314,212 @@ Alexa.createInteractionModel = async function (skillId, stage, intents, invocati
         {
             "interactionModel": {
                 "languageModel": {
-                    "invocationName": invocationName,
+                    "invocationName": "my space facts",
                     "modelConfiguration": {
                         "fallbackIntentSensitivity": {
                             "level": "LOW"
                         }
                     },
-                    "intents": intentsArray,
+                    "intents": [
+                        {
+                            "name": "AMAZON.CancelIntent",
+                            "samples": []
+                        },
+                        {
+                            "name": "AMAZON.HelpIntent",
+                            "samples": []
+                        },
+                        {
+                            "name": "AMAZON.StopIntent",
+                            "samples": []
+                        },
+                        {
+                            "name": "AMAZON.FallbackIntent",
+                            "samples": []
+                        },
+                        {
+                            "name": "AMAZON.StartOverIntent",
+                            "samples": []
+                        },
+                        {
+                            "name": "GetNewFactIntent",
+                            "slots": [],
+                            "samples": [
+                                "Give me a fact",
+                                "tell me a fact"
+                            ]
+                        },
+                        {
+                            "name": "GetTravelTime",
+                            "slots": [
+                                {
+                                    "name": "DepartingPlanet",
+                                    "type": "Planet",
+                                    "samples": [
+                                        "I'm starting from {DepartingPlanet} ",
+                                        "{DepartingPlanet} ",
+                                        "I'm going from {DepartingPlanet} to {ArrivingPlanet} "
+                                    ]
+                                },
+                                {
+                                    "name": "ArrivingPlanet",
+                                    "type": "Planet",
+                                    "samples": [
+                                        "I'm going to {ArrivingPlanet} ",
+                                        "{ArrivingPlanet} "
+                                    ]
+                                }
+                            ],
+                            "samples": [
+                                "calculate travel time",
+                                "how long does it take to travel from {DepartingPlanet} to {ArrivingPlanet} "
+                            ]
+                        }
+                    ],
+                    "types": [
+                        {
+                            "name": "Planet",
+                            "values": [
+                                {
+                                    "name": {
+                                        "value": "Mercury"
+                                    }
+                                },
+                                {
+                                    "name": {
+                                        "value": "Venus"
+                                    }
+                                },
+                                {
+                                    "name": {
+                                        "value": "Earth"
+                                    }
+                                },
+                                {
+                                    "name": {
+                                        "value": "Mars"
+                                    }
+                                },
+                                {
+                                    "name": {
+                                        "value": "Jupiter"
+                                    }
+                                },
+                                {
+                                    "name": {
+                                        "value": "Saturn"
+                                    }
+                                },
+                                {
+                                    "name": {
+                                        "value": "Uranus"
+                                    }
+                                },
+                                {
+                                    "name": {
+                                        "value": "Neptune"
+                                    }
+                                },
+                                {
+                                    "name": {
+                                        "value": "Pluto"
+                                    }
+                                }
+                            ]
+                        }
+                    ]
                 },
                 "dialog": {
-                    "intents": intentsSlots,
+                    "intents": [
+                        {
+                            "name": "GetTravelTime",
+                            "confirmationRequired": false,
+                            "prompts": {},
+                            "slots": [
+                                {
+                                    "name": "DepartingPlanet",
+                                    "type": "Planet",
+                                    "confirmationRequired": false,
+                                    "elicitationRequired": true,
+                                    "prompts": {
+                                        "elicitation": "Elicit.Intent-GetTravelTime.IntentSlot-DepartingPlanet"
+                                    },
+                                    "validations": [
+                                        {
+                                            "type": "isNotInSet",
+                                            "prompt": "Slot.Validation.596358663326.282490667310.1526107495625",
+                                            "values": [
+                                                "the sun",
+                                                "sun",
+                                                "our sun"
+                                            ]
+                                        },
+                                        {
+                                            "type": "hasEntityResolutionMatch",
+                                            "prompt": "Slot.Validation.596358663326.282490667310.1366622834897"
+                                        }
+                                    ]
+                                },
+                                {
+                                    "name": "ArrivingPlanet",
+                                    "type": "Planet",
+                                    "confirmationRequired": false,
+                                    "elicitationRequired": true,
+                                    "prompts": {
+                                        "elicitation": "Elicit.Intent-GetTravelTime.IntentSlot-ArrivingPlanet"
+                                    }
+                                }
+                            ]
+                        }
+                    ],
                     "delegationStrategy": "ALWAYS"
                 },
-                "prompts": promptsSlots
+                "prompts": [
+                    {
+                        "id": "Elicit.Intent-GetTravelTime.IntentSlot-DepartingPlanet",
+                        "variations": [
+                            {
+                                "type": "PlainText",
+                                "value": "Which planet do you want to start from?"
+                            }
+                        ]
+                    },
+                    {
+                        "id": "Elicit.Intent-GetTravelTime.IntentSlot-ArrivingPlanet",
+                        "variations": [
+                            {
+                                "type": "PlainText",
+                                "value": "Which planet do you want to travel to?"
+                            }
+                        ]
+                    },
+                    {
+                        "id": "Slot.Validation.596358663326.282490667310.1526107495625",
+                        "variations": [
+                            {
+                                "type": "PlainText",
+                                "value": "I can't answer this question about the sun, only planets. Please tell me a planet."
+                            },
+                            {
+                                "type": "PlainText",
+                                "value": "While the sun is the center of our solar system, it is not a planet. Please tell me a planet."
+                            }
+                        ]
+                    },
+                    {
+                        "id": "Slot.Validation.596358663326.282490667310.1366622834897",
+                        "variations": [
+                            {
+                                "type": "PlainText",
+                                "value": "{DepartingPlanet} is not a planet. Please tell me one of the nine planets in our solar system. "
+                            },
+                            {
+                                "type": "PlainText",
+                                "value": "I don't recognize {DepartingPlanet} as a planet in our solar system. Please tell me a planet."
+                            }
+                        ]
+                    }
+                ]
             }
         };
 
