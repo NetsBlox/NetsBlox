8f46f842f09d553ba3059669c833a2356f90383f
Devin Jean <emailcruzjean@yahoo.com>
2021-04-27 15:56:13 -0500

    fix sphinx compat issue

--- src/server/docs/conf.py
+++ src/server/docs/conf.py
@@ -51,3 +51,6 @@ html_theme = 'sphinx_rtd_theme'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+# for the apt version of sphinx package, default is 'contents' - override so it works for both
+master_doc = 'index'
