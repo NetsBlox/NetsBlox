0a2030637d05f903e56c43a6dc6fa9b28ce73e1d
Siyuan Wang <siyuan.wang@vanderbilt.com>
2021-04-27 20:54:07 -0500

    Covid-19 Vaccine RPCs

---
+++ src/server/services/procedures/covid-19/Vaccine-Data/us-vaccine-categories.js
@@ -0,0 +1,15 @@
+module.exports = [
+    'total vaccinations',
+    'total distributed',
+    'people vaccinated',
+    'people fully vaccinated per hundred',
+    'total vaccinations per hundred',
+    'people fully vaccinated',
+    'people vaccinated per hundred',
+    'distributed per hundred',
+    'daily vaccinations raw',
+
+    'daily vaccinations',
+    'daily vaccinations per million',
+    'share dose used',
+];
--- src/server/services/procedures/covid-19/covid-19.js
+++ src/server/services/procedures/covid-19/covid-19.js
@@ -9,6 +9,10 @@
  */
 const _ = require('lodash');
 const getServiceStorage = require('../../advancedStorage');
+const vaccination = require('./Vaccine-Data/vaccination-data-source');
+const states = require('./Vaccine-Data/states');
+const country = require('./Vaccine-Data/country');
+const usVaccineCategories = require('./Vaccine-Data/us-vaccine-categories');
 const schema = {
     date: Date,
     country: String,
@@ -91,5 +95,178 @@ COVID19.getLocationCoordinates = async function(country, state='', city='') {
     const data = await this._data.getLocation(country, state, city);
     return _.pick(data, ['latitude', 'longitude']);
 };
+
+/**
+ * Get number of Covid Vaccination in United States.
+ *
+ * Date is in month/day/year format.
+ *
+ */
+COVID19._getFullUSVaccinationData = async function(){
+    return await vaccination.getUSData();
+};
+/**
+ * Get number of Covid Vaccination in different countries.
+ *
+ * Date is in month/day/year format.
+ *
+ */
+COVID19._getFullWorldVaccinationData = async function(){
+    return await vaccination.getWorldData();
+};
+
+/**
+ * The list of States in the US that can be entered in the getVaccinationData function
+ *
+ */
+COVID19.getStates = async function() {
+    return (states);
+};
+/**
+ * The list of countries that can be entered in the getVaccinationData function
+ *
+ */
+COVID19.getCountry = async function() {
+    return (country);
+};
+/**
+ * The list of options that can be entered in the getVaccinationData function
+ *
+ */
+COVID19.getVaccineCategories = async function() {
+    return (usVaccineCategories);
+};
+/**
+ * Get number of Covid Vaccination by State.
+ *
+ * Date is in month/day/year format.
+ *
+ * @param state name of the state, can be found in getState drop down menu
+ */
+COVID19._getAllVaccineDataForStates = async function(state) {
+    const res = (await vaccination.getUSData())[state];
+    if (res === undefined) throw new Error(`states '${state}' is not in the database`);
+    else return res;
+};
+/**
+ * Get number of Covid Vaccination by Category in a specific state.
+ *
+ * Date is in month/day/year format.
+ *
+ * @param state name of the state, can be found in getState drop down menu
+ * @param option name of the Category, can be found in getVaccineCategories drop down menu
+ */
+COVID19._getCatVaccineDataForStates = async function(state, option='') {
+    const raw = (await COVID19._getAllVaccineDataForStates(state));
+    if(option === '')
+        return raw;
+
+    const res ={};
+    for(const date in raw) {
+        const data = raw[date];
+        const t = data[option];
+        if (t === undefined) throw new Error(`option '${option}' is not in the database`);
+        res[date] = t;
+    }
+    return res;
+};
+/**
+ * Get number of Covid Vaccination by Country.
+ *
+ * Date is in month/day/year format.
+ *
+ * @param country name of the counties, can be found in getCountry drop down menu
+ */
+
+COVID19._getAllVaccineDataForCountry = async function(country) {
+    const res = (await vaccination.getWorldData())[country];
+    if (res === undefined) throw new Error(`country '${country}' is not in the database`);
+    else return res;
+};
+/**
+ * Get number of Covid Vaccination by Category in a specific country.
+ *
+ * Date is in month/day/year format.
+ *
+ * @param country name of the state, can be found in getCountry drop down menu
+ * @param option name of the Category, can be found in getVaccineCategories drop down menu
+ */
+COVID19._getCatVaccineDataForCountry = async function(country, option='') {
+    const raw = (await COVID19._getAllVaccineDataForCountry(country));
+    if(option === '')
+        return raw;
+
+    const res ={};
+    for(const date in raw) {
+        const data = raw[date];
+        const t = data[option];
+        if (t === undefined) throw new Error(`option '${option}' is not in the database`);
+        res[date] = t;
+    }
+    return res;
+};
+
+/**
+ * Get number of cases of COVID-19 in which the person recovered by date for a specific country and state.
+ *
+ * Date is in month/day/year format.
+ *
+ * @param{String} country Country
+ * @param{String=} state State
+ * @param{String=} option VaccineCategories
+ */
+COVID19._getVaccineDataForCountry = async function(country, state = '', option='') {
+    if (country === 'United States' ){return await COVID19._getCatVaccineDataForStates(state, option);}
+    else {
+        if(state !=='') {throw new Error(`Country other than the United States should not have States`);}
+        else {
+            const res = (await COVID19._getCatVaccineDataForCountry(country, option));
+            return res;
+        }
+    }
+};
+
+/**
+ * Since January of 2021, Covid Vaccine has been rolled out gradually all over the world. This
+ * feature allows users to enter a specific country. If you are searching up a specific state in
+ * the United States, you can also enter a state name. Then, you can select a specific type of
+ * data you would like to view from the getVaccineCategories menu (example: total vaccination).
+ * At last, you can enter a date starting from late January, 2021 till now. An invalid entry
+ * will produce an error message.
+ *
+ * Date is in month/day/year format.
+ *
+ *
+ * @param{String} country country name. The options are listed the getCountry drop down Menu
+ * @param{String=} state United States state name. The options are listed the getState drop down
+ * Menu
+ * @param{String=} option VaccineCategories
+ * @param{String=} startDate First Date in mm/dd/yyyy
+ * @param{String=} endDate Second Date in mm/dd/yyyy
+ */
+
+COVID19.getVaccinationData= async function(country, state='', option='', startDate='01/01/2021', endDate='12/31/3000'){
+    const raw = (await COVID19._getVaccineDataForCountry(country, state, option));
+    const res = [];
+    if (startDate === '' && endDate ==='') return raw;
+    const numStart = getDateNum(startDate);
+    const numEnd = getDateNum(endDate);
+
+    for(const rawDate in raw) {
+        const dateNum = getDateNum(rawDate);
+        if(numStart < dateNum && dateNum < numEnd) {
+            res.push([rawDate, raw[rawDate]]);
+        }
+    }
+    return res;
+};
+/**
+ * Helper Function to turn date into an int to be compared
+ */
+function getDateNum(date) {
+    const dateVal = date.split('/');
+    const numDate= parseInt(dateVal[2]) * 367 + parseInt(dateVal[0]) * 32 + parseInt(dateVal[1]);
+    return numDate;
+}
 
 module.exports = COVID19;
