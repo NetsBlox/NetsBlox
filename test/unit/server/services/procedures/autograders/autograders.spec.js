b77926f18f774c431fa9b05eec1c486395ae789b
Brian Broll <brian.broll@gmail.com>
2021-05-21 11:36:07 -0500

    Add more autograder config validation. Closes #3117

--- test/unit/server/services/procedures/autograders/autograders.spec.js
+++ test/unit/server/services/procedures/autograders/autograders.spec.js
@@ -31,9 +31,111 @@ describe(utils.suiteName(__filename), function() {
 
     describe('createAutograder', function() {
         it('should require login', async function() {
+            service.socket.username = null;
             await assert.rejects(
-                () => service.createAutograder(),
+                () => service.createAutograder({}),
                 /Login required./
+            );
+        });
+
+        it('should require name', async function() {
+            service.socket.username = 'brian';
+            await assert.rejects(
+                () => service.createAutograder({assignments: []}),
+                /name is required/i
+            );
+        });
+
+        it('should require assignments', async function() {
+            service.socket.username = 'brian';
+            await assert.rejects(
+                () => service.createAutograder({name: 'TestGrader', assignments: []}),
+                /Assignments are required/i
+            );
+        });
+
+        it('should require names for assignments', async function() {
+            service.socket.username = 'brian';
+            await assert.rejects(
+                () => service.createAutograder({name: 'TestGrader', assignments: [
+                    {}
+                ]}),
+                /Assignment name is required/i
+            );
+        });
+
+        it('should require test type', async function() {
+            service.socket.username = 'brian';
+            await assert.rejects(
+                () => service.createAutograder({
+                    name: 'TestGrader2',
+                    assignments: [
+                        Object.entries({
+                            name: 'SomeAssignment',
+                            tests: [
+                                {}
+                            ]
+                        })
+                    ]
+                }),
+                /Unspecified type of test/i
+            );
+        });
+
+        it('should require spec for custom block tests', async function() {
+            service.socket.username = 'brian';
+            await assert.rejects(
+                () => service.createAutograder({
+                    name: 'TestGrader2',
+                    assignments: [
+                        Object.entries({
+                            name: 'SomeAssignment',
+                            tests: [
+                                Object.entries({type: 'CustomBlockTest'})
+                            ]
+                        })
+                    ]
+                }),
+                /missing block spec/i
+            );
+        });
+
+        it('should require name for function tests', async function() {
+            service.socket.username = 'brian';
+            await assert.rejects(
+                () => service.createAutograder({
+                    name: 'TestGrader2',
+                    assignments: [
+                        Object.entries({
+                            name: 'SomeAssignment',
+                            tests: [
+                                {}
+                            ]
+                        })
+                    ]
+                }),
+                /Unspecified type of test/i
+            );
+        });
+
+        it('should require function or inputs/outputs for tests', async function() {
+            service.socket.username = 'brian';
+            await assert.rejects(
+                () => service.createAutograder({
+                    name: 'TestGrader2',
+                    assignments: [
+                        Object.entries({
+                            name: 'SomeAssignment',
+                            tests: [
+                                Object.entries({
+                                    type: 'CustomBlockTest',
+                                    spec: 'someBlockSpec'
+                                })
+                            ]
+                        })
+                    ]
+                }),
+                /must specify inputs and outputs or a function/i
             );
         });
     });
