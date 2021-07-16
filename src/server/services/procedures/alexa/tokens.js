5535b6a6a59be214dd246775aa4f8b430e72dd9e
Brian Broll <brian.broll@gmail.com>
2021-07-16 16:30:44 -0500

    Remove unused file

--- src/server/services/procedures/alexa/tokens.js
+++
@@ -1,9 +0,0 @@
-const Storage = require('../../storage');
-let collection = null;
-const GetTokenStore = function() {
-    if (!collection) {
-        collection = Storage.create('alexa:tokens').collection;
-    }
-    return collection;
-};
-module.exports = GetTokenStore;
