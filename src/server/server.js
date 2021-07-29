ed408dea57dfd6e277143b53d80b518f260ceb13
Brian Broll <brian.broll@gmail.com>
2021-07-29 16:01:03 -0500

    Add support for sending message to address. Closes #3222

--- src/server/server.js
+++ src/server/server.js
@@ -37,6 +37,7 @@ const assert = require('assert');
 const axios = require('axios');
 const CustomServicesHosts = require('./api/core/services-hosts');
 const RestAPI = require('./api/rest');
+const NetsBloxAddress = require('./netsblox-address');
 
 var Server = function(opts) {
     this._logger = new Logger('netsblox');
@@ -441,7 +442,19 @@ class ServicesPrivateAPI {
         this.messageHandlers = {};
         this.logger = logger.fork('services');
 
-        this.on(Messages.SendMessage, message => {
+        this.on(Messages.SendMessage, async message => {
+            const {target, projectId, roleId} = message;
+            const address = await NetsBloxAddress.new(target, projectId, roleId);
+            const states = address.resolve();
+            const clients = states
+                .flatMap(state => {
+                    const [projectId, roleId] = state;
+                    return NetworkTopology.getClientsAt(projectId, roleId);
+                });
+
+            clients.forEach(client => client.sendMessage(message.type, message.contents));
+        });
+        this.on(Messages.SendMessageToClient, message => {
             const client = NetworkTopology.getClient(message.clientId);
             if (client) {
                 const hasChangedSituation = message.projectId !== client.projectId ||
