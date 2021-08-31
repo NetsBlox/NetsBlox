296425b61da7e543501856a689b5557db1ed0b50
Devin Jean <emailcruzjean@yahoo.com>
2021-08-30 21:09:16 -0500

    clean up values/errors + more enum

--- src/server/services/procedures/fbi-crime-data/types.js
+++ src/server/services/procedures/fbi-crime-data/types.js
@@ -45,13 +45,13 @@ const OFFENSE_Data_Controller_OPTIONS = {
     'purse snatching': 'purse-snatching',
     'rape': 'rape',
     'robbery': 'robbery',
-    'sexual assult with an object': 'sexual-assult-with-an-object',  // FIXME: typo?
+    'sexual assault with an object': 'sexual-assult-with-an-object',
     'sex offenses non forcible': 'sex-offenses-non-forcible',
     'shoplifting': 'shoplifting',
     'simple assault': 'simple-assault',
     'sodomy': 'sodomy',
     'sports tampering': 'sports-tampering',
-    'statutory rape': 'statutory-rape',  // FIXME: typo?
+    'statutory rape': 'statutory-rape',
     'stolen property offenses': 'stolen-property-offenses',
     'theft from building': 'theft-from-building',
     'theft from coin operated machine or device': 'theft-from-coin-operated-machine-or-device',
@@ -82,17 +82,17 @@ const OFFENSE_Supp_Controller_OPTIONS = {
     'robbery': 'robbery',
     'not specified': 'not-specified',
     'motor vehicle theft': 'motor-vehicle-theft',
-    'larceny': 'larceny'
+    'larceny': 'larceny',
 };
 
 const OFFENSE_Arrest_Controller_OPTIONS = {
     'aggravated assault': 'aggravated-assault', 
     'all other offenses': 'all-other-offenses',
     'arson': 'arson',
-    'burglary':'burglary',
+    'burglary': 'burglary',
     'curfew': 'curfew',
     'disorderly conduct': 'disorderly-conduct',
-    'dui':'dui',
+    'dui': 'dui',
     'drug grand total': 'drug-grand-total',
     'drug possession marijuana': 'drug-possession-marijuana',
     'drug possession opium': 'drug-possession-opium',
@@ -100,7 +100,7 @@ const OFFENSE_Arrest_Controller_OPTIONS = {
     'drug possession subtotal': 'drug-possession-subtotal',
     'drug possession synthetic': 'drug-possession-synthetic',
     'drug sales marijuana': 'drug-sales-marijuana',
-    'drug sales opium':'drug-sales-opium',
+    'drug sales opium': 'drug-sales-opium',
     'drug sales other': 'drug-sales-other',
     'drug sales subtotal': 'drug-sales-subtotal',
     'drug sales synthetic': 'drug-sales-synthetic',
@@ -111,28 +111,28 @@ const OFFENSE_Arrest_Controller_OPTIONS = {
     'gambling all other': 'gambling-all-other',
     'gambling bookmaking': 'gambling-bookmaking',
     'gambling numbers': 'gambling-numbers',
-    'gambling total':'gambling-total',
-    'human trafficking commerical':'human-trafficking-commerical',
-    'human trafficking servitude':'human-trafficking-servitude',
-    'larceny':'larceny',
-    'liqour laws':'liqour-laws',
-    'motor vehcile theft':'motor-vehcile-theft',
-    'murder':'murder',
-    'offense against family':'offense-against-family',
-    'prostitution':'prostitution',
-    'prostitution assisting':'prostitution-assisting',
-    'prostitution prostitution':'prostitution-prostitution',
-    'prostitution purchasing':'prostitution-purchasing',
-    'rape':'rape',
-    'robbery':'robbery',
-    'runaway':'runaway',
-    'sexoffenses':'sex-offenses',
-    'simple assault':'simple-assault',
-    'stolen property':'stolen-property',
-    'suspicion':'suspicion',
-    'vagrancy':'vagrancy',
-    'vandalism':'vandalism',
-    'weapons':'weapons'
+    'gambling total': 'gambling-total',
+    'human trafficking commerical': 'human-trafficking-commerical',
+    'human trafficking servitude': 'human-trafficking-servitude',
+    'larceny': 'larceny',
+    'liqour laws': 'liqour-laws',
+    'motor vehcile theft': 'motor-vehcile-theft',
+    'murder': 'murder',
+    'offense against family': 'offense-against-family',
+    'prostitution': 'prostitution',
+    'prostitution assisting': 'prostitution-assisting',
+    'prostitution prostitution': 'prostitution-prostitution',
+    'prostitution purchasing': 'prostitution-purchasing',
+    'rape': 'rape',
+    'robbery': 'robbery',
+    'runaway': 'runaway',
+    'sexoffenses': 'sex-offenses',
+    'simple assault': 'simple-assault',
+    'stolen property': 'stolen-property',
+    'suspicion': 'suspicion',
+    'vagrancy': 'vagrancy',
+    'vandalism': 'vandalism',
+    'weapons': 'weapons',
 };
 
 const OFFENSE_Victim_Controller_OPTIONS = {
@@ -145,25 +145,115 @@ const OFFENSE_Victim_Controller_OPTIONS = {
     'robbery': 'robbery',
     'arson': 'arson',
     'violent crime': 'violent-crime',
-    'property crime':'property-crime'
+    'property crime': 'property-crime',
 };
 
-const OFFENSE_Data_Variable_OPTIONS={
-    'count':'COUNT',
-    'weapons':'WEAPONS',
-    'linked offense':'LINKEDOFFENSE',
-    'suspect using':'SUSPECTUSING',
-    'criminal activity':'CRIMINAL_ACTIVITY',
-    'property recovered':'PROPERTY_RECOVERED',
-    'property stolen':'PROPERTY_STOLEN',
-    'bias':'BIAS'
+const ARREST_TKM_VARIABLE_OPTIONS = {
+    'violent crime': 'violent_crime',
+    'property crime': 'property_crime',
+    'gambling': 'gambling',
+    'society': 'society',
+    'all': 'all',
+
+    // the api docs say these are valid, but their server rejects them
+    // 'drug related': 'drug',
+    // 'human trafficking': 'human_trafficking',
+    // 'juvenile': 'juvenile',
+};
+
+const AGE_RANGE = [
+    'All',
+    'Under 10',
+    '10-12', '13-14',
+    '15', '16', '17', '18', '19', '20', '21', '22', '23', '24',
+    '25-29', '30-34', '35-39', '40-44', '45-49', '50-54', '55-59', '60-64',
+    '65 and over',
+];
+
+const OFFENSE_Data_Variable_OPTIONS = {
+    'count': 'COUNT',
+    'weapons': 'WEAPONS',
+    'linked offense': 'LINKEDOFFENSE',
+    'suspect using': 'SUSPECTUSING',
+    'criminal activity': 'CRIMINAL_ACTIVITY',
+    'property recovered': 'PROPERTY_RECOVERED',
+    'property stolen': 'PROPERTY_STOLEN',
+    'bias': 'BIAS',
 };
 
 const Supplemental_Data_OPTIONS={
     'MVT Recovered': 'MVT_RECOVERED',
-    'Offense':'OFFENSE',
-    'Offense Sub Category':'OFFENSE_SUB_CATEGORY',
-    'Larceny Type': 'LARCENY_TYPE'
+    'Offense': 'OFFENSE',
+    'Offense Sub Category': 'OFFENSE_SUB_CATEGORY',
+    'Larceny Type': 'LARCENY_TYPE',
+};
+
+const US_TERRITORIES = {
+    'Alaska': 'AK',
+    'Alabama': 'AL',
+    'Arkansas': 'AR',
+    'American Samoa': 'AS',
+    'Arizona': 'AZ',
+    'California': 'CA',
+    'Colorado': 'CO',
+    'Connecticut': 'CT',
+    'District of Columbia': 'DC',
+    'Delaware': 'DE',
+    'Florida': 'FL',
+    'Georgia': 'GA',
+    'Hawaii': 'HI',
+    'Iowa': 'IA',
+    'Idaho': 'ID',
+    'Illinois': 'IL',
+    'Indiana': 'IN',
+    'Kansas': 'KS',
+    'Kentucky': 'KY',
+    'Louisiana': 'LA',
+    'Massachusetts': 'MA',
+    'Maryland': 'MD',
+    'Maine': 'ME',
+    'Michigan': 'MI',
+    'Minnesota': 'MN',
+    'Missouri': 'MO',
+    'Mississippi': 'MS',
+    'Montana': 'MT',
+    'Nebraska': 'NE',
+    'North Carolina': 'NC',
+    'North Dakota': 'ND',
+    'New Hampshire': 'NH',
+    'New Jersey': 'NJ',
+    'New Mexico': 'NM',
+    'Nevada': 'NV',
+    'New York': 'NY',
+    'Ohio': 'OH',
+    'Oklahoma': 'OK',
+    'Oregon': 'OR',
+    'Pennsylvania': 'PA',
+    'Puerto Rico': 'PR',
+    'Rhode Island': 'RI',
+    'South Carolina': 'SC',
+    'South Dakota': 'SD',
+    'Tennessee': 'TN',
+    'Texas': 'TX',
+    'Utah': 'UT',
+    'Virginia': 'VA',
+    'Vermont': 'VT',
+    'Washington': 'WA',
+    'Wisconsin': 'WI',
+    'West Virginia': 'WV',
+    'Wyoming': 'WY',
+    'Mariana Islands': 'MP',
+    'US Virgin Islands': 'VI',
+    'Guam': 'GM',
+};
+
+const US_REGIONS = {
+    'Territories': 'Territories',
+    'Northeast': 'Northeast',
+    'Midwest': 'Midwest',
+    'South': 'South',
+    'West': 'West',
+    'Other': 'Other',
 };
 
 function registerTypes() {
@@ -209,6 +299,33 @@ function registerTypes() {
         baseParams: OFFENSE_Data_Variable_OPTIONS,
     });
 
+    types.defineType({
+        name: 'ArrestCategory',
+        description: 'Reason for arrest or type of crime.',
+        baseType: 'Enum',
+        baseParams: ARREST_TKM_VARIABLE_OPTIONS,
+    });
+
+    types.defineType({
+        name: 'USTerritory',
+        description: 'The name of an (extended) US territory, which includes all US states.',
+        baseType: 'Enum',
+        baseParams: US_TERRITORIES,
+    });
+
+    types.defineType({
+        name: 'USRegion',
+        description: 'The name of an (extended) US region, which includes a breakdown of the continental US.',
+        baseType: 'Enum',
+        baseParams: US_REGIONS,
+    });
+
+    types.defineType({
+        name: 'ArrestAgeCategory',
+        description: 'A statistical category for the age of a perpetrator in the FBICrimeData service.',
+        baseType: 'Enum',
+        baseParams: AGE_RANGE,
+    });
 }
 
 module.exports = {registerTypes};
