65cb86b33b4636cff256c38e666a5883f0b5d6c1
Devin Jean <emailcruzjean@yahoo.com>
2021-11-16 13:12:56 -0600

    Filter out invalid data in vaccination data. Fixes #3291 (#3296)
    * fixed vaccination csv loading
    
    * failure logging + tests
    
    * codeclimate fix

--- src/server/services/procedures/covid-19/vaccination/vaccination-data-source.js
+++ src/server/services/procedures/covid-19/vaccination/vaccination-data-source.js
@@ -22,7 +22,8 @@ function dictPathOrEmptyInit(dict, path) {
 
 function isGoodData(data){
     for(const key in data) {
-        if (!data[key]) return false;
+        const v = data[key];
+        if (v === undefined || v === null || v === '' || isNaN(v)) return false;
     }
     return true;
 }
@@ -99,37 +100,56 @@ async function getWorldData() {
     logger.info('download complete - restructuring data');
 
     const res = {};
-    let first = true;
-    for (const row of resp.data.split(/\r?\n/)) {
-        if(first) {
-            first = false;
-            continue;
+    const rows = resp.data.split(/\r?\n/);
+    const fields = rows.shift().split(',').map(s => s.replace(/[-_\s]+/g, ' ').trim());
+    const desired_fields = [
+        'total vaccinations',
+        'people vaccinated',
+        'people fully vaccinated',
+        'daily vaccinations raw',
+        'daily vaccinations',
+        'total vaccinations per hundred',
+        'people vaccinated per hundred',
+        'people fully vaccinated per hundred',
+        'daily vaccinations per million',
+    ];
+    const missing_fields = desired_fields.filter(f => !fields.includes(f));
+    if (missing_fields.length !== 0) {
+        logger.error(`World Data missing ${missing_fields.length} expected fields: ${missing_fields}`);
+    }
+
+    let goodRows = 0;
+    for (const row of rows) {
+        const rawVals = row.split(',');
+        if (rawVals.length !== fields.length) continue; // bad csv (unlikely), skip the row
+
+        const vals = {};
+        for (let i = 0; i < fields.length; ++i) {
+            vals[fields[i]] = rawVals[i];
         }
 
-        const vals = row.split(',');
-        if (vals.length !== 12) continue;
-
-        const country = vals[0];
-        const rawDate = vals[2].split('-');
+        const country = vals['location'];
+        const rawDate = vals['date'].split('-');
         const date = `${rawDate[1]}/${rawDate[2]}/${rawDate[0]}`;
 
-        const data = {
-            'total vaccinations': parseFloat(vals[3]),
-            'people vaccinated': parseFloat(vals[4]),
-            'people fully vaccinated': parseFloat(vals[5]),
-            'daily vaccinations raw': parseFloat(vals[6]),
-            'daily vaccinations': parseFloat(vals[7]),
-            'total vaccinations per hundred': parseFloat(vals[8]),
-            'people vaccinated per hundred': parseFloat(vals[9]),
-            'people fully vaccinated per hundred':parseFloat(vals[10]),
-            'daily vaccinations per million':parseFloat(vals[11]),
-        };
-
+        const data = {};
+        for (const field of desired_fields) {
+            data[field] = parseFloat(vals[field]);
+        }
         if (!isGoodData(data)) continue;
+        goodRows += 1;
+
         const entry = dictPathOrEmptyInit(res, [country, date]);
         for (const key in data) {
             entry[key] = data[key];
         }
+    }
+
+    if (goodRows === 0) {
+        logger.error('World Data got no data (all rows were invalid)');
+    } else if (goodRows !== rows.length) {
+        const bad = rows.length - goodRows;
+        logger.warn(`World Data: ${bad} of ${rows.length} rows (${100 * bad / rows.length}%) were discarded due to incomplete data`);
     }
 
     logger.info('restructure complete - caching result');
