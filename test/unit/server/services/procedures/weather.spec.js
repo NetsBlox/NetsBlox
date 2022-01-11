0fe59d6f0c80f2cfb81d436ebdd65b8be6d7b1d6
KurtXiao <zechuan.xiao@vanderbilt.edu>
2022-01-11 15:52:31 -0600

    1/11/2022

--- test/unit/server/services/procedures/weather.spec.js
+++ test/unit/server/services/procedures/weather.spec.js
@@ -8,6 +8,9 @@ describe(utils.suiteName(__filename), function() {
         ['description', ['latitude', 'longitude']],
         ['windSpeed', ['latitude', 'longitude']],
         ['windAngle', ['latitude', 'longitude']],
-        ['icon', ['latitude', 'longitude']]
+        ['icon', ['latitude', 'longitude']],
+        ['listRadars', ['latitude', 'longitude', 'width', 'height', 'zoom']],
+        ['plotAllRadarImages', ['latitude', 'longitude', 'width', 'height', 'zoom', 'type']],
+        ['plotRadarImages', ['latitude', 'longitude', 'width', 'height', 'zoom', 'radars', 'type']],
     ]);
 });
