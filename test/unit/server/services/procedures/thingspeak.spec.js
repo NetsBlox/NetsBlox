3ace0ece2ad9ac3772911df0c62a0e6c2e3bc6f8
Devin Jean <emailcruzjean@yahoo.com>
2022-01-18 23:39:26 -0600

    updatedSince filtering

--- test/unit/server/services/procedures/thingspeak.spec.js
+++ test/unit/server/services/procedures/thingspeak.spec.js
@@ -2,9 +2,9 @@ const utils = require('../../../../assets/utils');
 
 describe(utils.suiteName(__filename), function() {
     utils.verifyRPCInterfaces('Thingspeak', [
-        ['searchByTag', ['tag', 'limit']],
-        ['searchByLocation', ['latitude', 'longitude', 'distance', 'limit']],
-        ['searchByTagAndLocation', ['tag','latitude', 'longitude', 'distance', 'limit']],
+        ['searchByTag', ['tag', 'limit', 'updatedSince']],
+        ['searchByLocation', ['latitude', 'longitude', 'distance', 'limit', 'updatedSince']],
+        ['searchByTagAndLocation', ['tag','latitude', 'longitude', 'distance', 'limit', 'updatedSince']],
         ['channelFeed', ['id', 'numResult']],
         ['privateChannelFeed', ['id', 'numResult', 'apiKey']],
         ['channelDetails', ['id']]
