09d66c70c549dd464eba9264e8c3cb929a212308
Brian Broll <brian.broll@gmail.com>
2022-01-31 08:57:48 -0600

    Improve WordGuess feedback method. Fixes #3360 (#3361)
    * Add failing test
    
    * Add additional tests
    
    * Pass test
    
    * One last test for completeness
    
    * Add additional test cases
    
    * Fix all 1s test
    
    Co-authored-by: Gordon Stein <gstein@ltu.edu>

--- test/unit/server/services/procedures/word-guess.spec.js
+++ test/unit/server/services/procedures/word-guess.spec.js
@@ -32,7 +32,7 @@ describe(utils.suiteName(__filename), function () {
 
     it('correct guess should win game', function () {
         wordguess.start(5);
-        assert(wordguess.guess(WordGuess._states[Object.keys(WordGuess._states)[0]].word));
+        wordguess.guess(WordGuess._states[Object.keys(WordGuess._states)[0]].word);
         assert.equal(WordGuess._states[Object.keys(WordGuess._states)[0]].gamestate, WordGuess._GameState.Won);
         delete WordGuess._states[Object.keys(WordGuess._states)[0]];
     });
@@ -49,6 +49,53 @@ describe(utils.suiteName(__filename), function () {
         // Valid attempt
         assert.doesNotThrow(() => { wordguess.guess('apple'); });
 
+        delete WordGuess._states[Object.keys(WordGuess._states)[0]];
+    });
+
+    it('should return all 3s when letters are in correct places', function () {
+        const feedback = WordGuess._calculateMatches('crack', 'crack');
+        assert.deepEqual(feedback, [3, 3, 3, 3, 3]);
+
+        wordguess.start(5);
+        assert.deepEqual(wordguess.guess(WordGuess._states[Object.keys(WordGuess._states)[0]].word), [3, 3, 3, 3, 3]);
+        delete WordGuess._states[Object.keys(WordGuess._states)[0]];
+    });
+
+    it('should return all 1s when no letters are in correct places', function () {
+        const feedback = WordGuess._calculateMatches('crack', '_____');
+        assert.deepEqual(feedback, [1, 1, 1, 1, 1]);
+
+        wordguess.start(5);
+        WordGuess._states[Object.keys(WordGuess._states)[0]].word = 'crack';
+        assert.deepEqual(wordguess.guess('beets'), [1, 1, 1, 1, 1]);
+        delete WordGuess._states[Object.keys(WordGuess._states)[0]];
+    });
+    
+    it('should return 2s when correct letters are in wrong places', function () {
+        const feedback = WordGuess._calculateMatches('crack', 'kcrac');
+        assert.deepEqual(feedback, [2, 2, 2, 2, 2]);
+
+        const feedback2 = WordGuess._calculateMatches('tests', '--est');
+        assert.deepEqual(feedback2, [1, 1, 2, 2, 2]);
+
+
+        wordguess.start(5);
+        WordGuess._states[Object.keys(WordGuess._states)[0]].word = 'crack';
+        assert.deepEqual(wordguess.guess('rakes'), [2, 2, 2, 1, 1]);
+        assert.deepEqual(wordguess.guess('occur'), [1, 2, 2, 1, 2]);
+        delete WordGuess._states[Object.keys(WordGuess._states)[0]];
+    });
+
+    it('should return 3s when multiple letters are in correct places', function () {
+        const feedback = WordGuess._calculateMatches('crack', 'c__c_');
+        assert.deepEqual(feedback, [3, 1, 1, 3, 1]);
+
+        const feedback2 = WordGuess._calculateMatches('crack', 'ccccc');
+        assert.deepEqual(feedback2, [3, 1, 1, 3, 1]);
+
+        wordguess.start(5);
+        WordGuess._states[Object.keys(WordGuess._states)[0]].word = 'crack';
+        assert.deepEqual(wordguess.guess('chuck'), [3, 1, 1, 3, 3]);
         delete WordGuess._states[Object.keys(WordGuess._states)[0]];
     });
 });
