5937977058d69dcecce3fb6e77c25c9a2d36c773
Michelle Zhu <michelle.l.zhu@vanderbilt.edu>
2022-03-17 17:41:47 -0500

    Chart.draw() done

--- src/server/services/procedures/global-equities/global-equities.js
+++ src/server/services/procedures/global-equities/global-equities.js
@@ -6,43 +6,118 @@
  * @service
  */
 
-const ApiConsumer = require('../utils/api-consumer');
-const {GlobalEquitiesKey} = require('../utils/api-key');
-const baseUrl = 'alphavantage.co';
-const GlobalEquities = new ApiConsumer('GlobalEquities', baseUrl, {cache: {ttl: Infinity}});
-ApiConsumer.setRequiredApiKey(GlobalEquities, GlobalEquitiesKey);
-
-GlobalEquities._raw_search = async function(apifunction, symbol, interval) {
-    const data = await this._requestData({path:'query?', queryString:`function=${apifunction}&symbol=${symbol}&interval=${interval}&apikey=${this.apiKey.value}`});
-    const matches = [];
-    for (const item of data.rows) {
-        const metadata = [];
-        if ('Meta Data' in item.context.freetext.matches) {
-            for (const match of listify(item.content.freetext.matches)) {
-                matches.push(match.content);
-            }
-        }
-        const timeseries = [];
-        if ('Time Series ' in item.context.freetext.matches) {
-            for (const timeserie of listify(item.content.freetext.timeserise)) {
-                timeseries.push(match.content);
-            }
-        }
-    }
-    const id = item.id;
-    const title = item.title;
-    matches.push({id, title, metadata, timeseries});
-    return matches;
-};
-
-/**
- * Get data based on a specified ticker
- * @param {String} apifunction Function to utilize
- * @param {String} ticker Ticker symbol
- * @param {String} interval Time interval to retrieve data in minutes
- * Do I need a return here?
- */
-GlobalEquities.search = function(apifunction = 'TIME_SERIES_INTRADAY', symbol, interval = '5min') {
-    GlobalEquities._raw_search(apifunction, symbol, interval);
-}
-module.exports = GlobalEquities;
+ const ApiConsumer = require('../utils/api-consumer');
+ const {GlobalEquitiesKey} = require('../utils/api-key');
+ const baseUrl = 'https://alphavantage.co';
+ const GlobalEquities = new ApiConsumer('GlobalEquities', baseUrl, {cache: {ttl: Infinity}});
+ ApiConsumer.setRequiredApiKey(GlobalEquities, GlobalEquitiesKey);
+ const types = require('../../input-types');
+ 
+ function listify(item) {
+     return item === undefined ? [] : item instanceof Array ? item : [item];
+ }
+ 
+ types.defineType({
+     name: 'AlphaVantageTimePeriod',
+     description: 'The style to use for displaying :doc:`/services/PhoneIoT/index` slider, as created by :func:`PhoneIoT.addSlider`.',
+     baseType: 'Enum',
+     baseParams: { '5 min': 5, '10 min': 10, '15 min': 15, 'Daily':'daily','Weekly':'weekly','Monthly':'monthly'},
+ });
+ 
+ types.defineType({
+     name: 'AlphaVantageAttributes',
+     description: 'The style to use for displaying :doc:`/services/PhoneIoT/index` slider, as created by :func:`PhoneIoT.addSlider`.',
+     baseType: 'Enum',
+     baseParams: { 'All': 'all', 'Open': 'open', 'High': 'high', 'Low':'low', 'Close':'close', 'Volume':'volume'},
+ });
+ 
+ types.defineType({
+     name: 'AlphaVantageFunctions',
+     description: 'The style to use for displaying :doc:`/services/PhoneIoT/index` slider, as created by :func:`PhoneIoT.addSlider`.',
+     baseType: 'Enum',
+     baseParams: { 'TIME_SERIES_INTRADAY': 'TIME_SERIES_INTRADAY', 'TIME_SERIES_DAILY': 'TIME_SERIES_DAILY', 'TIME_SERIES_WEEKLY': 'TIME_SERIES_WEEKLY', 'TIME_SERIES_MONTHLY': 'TIME_SERIES_MONTHLY'},
+ });
+ 
+ GlobalEquities._raw_search = async function(apifunction, symbol, interval, attributes) {
+     if (apifunction == 'TIME_SERIES_INTRADAY') {
+         data = await this._requestData({path:'query', queryString:`function=${apifunction}&symbol=${symbol}&interval=${interval}min&apikey=${this.apiKey.value}`});
+         labelAppend = `Time Series (${interval}min)`; 
+     }
+     else if (apifunction == 'TIME_SERIES_DAILY') {
+         data = await this._requestData({path:'query', queryString:`function=${apifunction}&symbol=${symbol}&apikey=${this.apiKey.value}`});
+         labelAppend = 'Time Series (Daily)';
+     }
+     else if (apifunction == 'TIME_SERIES_WEEKLY') {
+         data = await this._requestData({path:'query', queryString:`function=${apifunction}&symbol=${symbol}&apikey=${this.apiKey.value}`});
+         labelAppend = 'Weekly Time Series';
+     }
+     else if (apifunction == 'TIME_SERIES_MONTHLY') {
+         data = await this._requestData({path:'query', queryString:`function=${apifunction}&symbol=${symbol}&apikey=${this.apiKey.value}`});
+         labelAppend = 'Monthly Time Series';
+     }
+     // Properties within the second property ("Time Series: (XX min)")
+     const matches = [];
+ 
+     // Print statements to verify read
+     // console.log(Object.keys(data));
+     // console.log(data[`${labelAppend}`]);
+     
+     // Fill time series with time tag and open/high/close
+     for (const entry in data[`${labelAppend}`]) {
+         const timeserie = [];
+         yearString = entry.substr(0,entry.indexOf('-'));
+         yearDefaultDate = new Date(yearString);
+         entryMillisec = new Date(entry) - yearDefaultDate;
+         entryYear = parseInt(yearString) + entryMillisec/31556952000;
+         // Push the key first (time)
+         timeserie.push(entryYear);
+         // Push the rest of the information
+         const raw = data[`${labelAppend}`][entry];
+         cleaned = {
+             'open': parseFloat(raw['1. open']),
+             'high': parseFloat(raw['2. high']),
+             'low': parseFloat(raw['3. low']),
+             'close': parseFloat(raw['4. close']),
+             'volume': parseFloat(raw['5. volume'])
+         };
+         if (attributes !== 'all') {
+             cleaned = cleaned[attributes];
+         }
+         timeserie.push(cleaned);
+         console.log(timeserie);
+         // Add to return array
+         matches.push(timeserie);
+     }
+     return matches;
+ };
+ 
+ /**
+  * Get data using either intraday, daily, weekly, or monthly API call
+  * @param {String} symbol Ticker symbol
+  * @param {AlphaVantageTimePeriod=} interval If intraday, 5, 10, or 15 minutes
+  * @param {AlphaVantageAttributes=} attributes May be all or only 1
+  */
+ GlobalEquities.timeSeriesSearch = function(symbol = 'IBM', interval = 5, attributes = 'all') {
+     const api_funcs = { daily: 'TIME_SERIES_DAILY', weekly: 'TIME_SERIES_WEEKLY', monthly: 'TIME_SERIES_MONTHLY'};
+     const api_func = api_funcs[interval];
+     if (api_func == undefined) {
+         return GlobalEquities._raw_search('TIME_SERIES_INTRADAY', symbol, interval, attributes);
+     }
+     return GlobalEquities._raw_search(api_func,symbol, null, attributes);
+ }
+ 
+ // Other RPC Ideas
+ 
+ // /**
+ //  * Calculate delta
+ //  */
+ 
+ // GlobalEquities.delta = function(symbol = 'IBM') {
+ //     dataSet = GlobalEquities.timeSeriesSearch(symbol, 'daily');
+ // }
+ 
+ // Pick a stock and a buy date and sell date - did you lose or gain money? how much?
+ 
+ // Difference between all attributes between 2 dates
+ 
+ module.exports = GlobalEquities;
