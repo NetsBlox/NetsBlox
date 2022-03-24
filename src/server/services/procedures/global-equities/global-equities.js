c74d60ba31b1130f0380a60a07971740a8961b4c
Michelle Zhu <michelle.l.zhu@vanderbilt.edu>
2022-03-24 14:01:22 -0500

    bug fixes in getDataCrypto

--- src/server/services/procedures/global-equities/global-equities.js
+++ src/server/services/procedures/global-equities/global-equities.js
@@ -9,9 +9,10 @@
  const ApiConsumer = require('../utils/api-consumer');
  const {GlobalEquitiesKey} = require('../utils/api-key');
  const baseUrl = 'https://alphavantage.co';
- const GlobalEquities = new ApiConsumer('GlobalEquities', baseUrl, {cache: {ttl: Infinity}});
+ const GlobalEquities = new ApiConsumer('GlobalEquities', baseUrl, {cache: {ttl: 30}});
  ApiConsumer.setRequiredApiKey(GlobalEquities, GlobalEquitiesKey);
  const types = require('../../input-types');
+ const { matches } = require('lodash');
  
  types.defineType({
      name: 'EquitiesTimePeriod',
@@ -34,6 +35,65 @@
      baseParams: { 'All': 'all', 'Open': 'open', 'High': 'high', 'Low':'low', 'Close':'close', 'Volume':'volume'},
  });
  
+ const DATA_SOURCE_LIFETIME = 1 * 24 * 60 * 60 * 1000;
+ let CACHED_DATA = undefined;
+ let CACHE_TIME_STAMP = undefined;
+ 
+ async function getStockSymbol() {
+     if (CACHED_DATA !== undefined && Date.now() - CACHE_TIME_STAMP <= DATA_SOURCE_LIFETIME) return CACHED_DATA;
+ 
+     let resultSet = [];
+     data = await GlobalEquities._requestData({path:'query', queryString:`function=LISTING_STATUS&apikey=${GlobalEquities.apiKey.value}`});
+     var lines = data.split('\n');
+     // Process each entry
+     for (var i = 0; i < lines.length; i++) {
+         tickerResult = [];
+         categories = lines[i].split(',');
+         // First value is symbol, second is company name
+         ticker = categories[0];
+         companyName = categories[1];
+         if (ticker !== undefined && companyName !== undefined) {
+             tickerResult.push(ticker);
+             tickerResult.push(companyName);
+             resultSet.push(tickerResult);
+         }
+     }
+     CACHED_DATA = resultSet;
+     CACHE_TIME_STAMP = Date.now();
+ 
+     // console.log(resultSet.slice(0,10));
+     return resultSet;
+ }
+ 
+ /**
+  * @param {String} searchString String to search matches
+  * @return {Array} Array of results
+  */
+ GlobalEquities.symbolSearch = async function(searchString){
+     const matches = [];
+     const listingResult = await getStockSymbol();
+     for (const element of listingResult) {
+         const results = [];
+         if (element[1] !== undefined && (element[1].toLowerCase()).includes(searchString.toLowerCase())) {
+             results.push(element[0]);
+             results.push(element[1]);
+             matches.push(results);
+         }
+     }
+     if (matches.length > 1) {
+         return matches;
+     }
+     else {
+         throw new Error('Keyword not recognized within any actively traded stocks or ETFs.');
+     }
+ }
+ 
+ /**
+  * 
+  * @param {String} searchString 
+  * @return {Array}  Array of all substring matches
+  */
+ 
  GlobalEquities._raw_search_equities = async function(apifunction, symbol, interval, attributes) {
      if (apifunction == 'TIME_SERIES_INTRADAY') {
          data = await this._requestData({path:'query', queryString:`function=${apifunction}&symbol=${symbol}&interval=${interval}min&apikey=${this.apiKey.value}`});
@@ -51,13 +111,14 @@
          data = await this._requestData({path:'query', queryString:`function=${apifunction}&symbol=${symbol}&apikey=${this.apiKey.value}`});
          labelAppend = 'Monthly Time Series';
      }
+ 
+     if ("Error Message" in data) {
+         throw new Error(`Unknown symbol: ${symbol}`);
+     }
+     
      // Properties within the second property ("Time Series: (XX min)")
      const matches = [];
  
-     // Print statements to verify read
-     // console.log(Object.keys(data));
-     // console.log(data[`${labelAppend}`]);
-     
      // Fill time series with time tag and open/high/close
      for (const entry in data[`${labelAppend}`]) {
          const timeserie = [];
@@ -70,6 +131,7 @@
          timeserie.push(entryYear);
          // Push the rest of the information
          const raw = data[`${labelAppend}`][entry];
+         // If key with error message, throw exception string w message
          cleaned = {
              'open': parseFloat(raw['1. open']),
              'high': parseFloat(raw['2. high']),
@@ -89,13 +151,14 @@
  };
  
  /**
-  * Get data using either intraday, daily, weekly, or monthly API call
+  * Get data of publicly traded stocks
   * @param {String} symbol Ticker symbol
   * @param {EquitiesTimePeriod=} interval If intraday, 5, 10, or 15 minutes
   * @param {EquitiesCryptoAttributes=} attributes May be all or only 1
   * @return {Array} Array of time stamps and associated stock price
   */
  GlobalEquities.getEquityData = function(symbol = 'IBM', interval = 5, attributes = 'all') {
+     symbol = symbol.toUpperCase();
      const api_funcs = { daily: 'TIME_SERIES_DAILY', weekly: 'TIME_SERIES_WEEKLY', monthly: 'TIME_SERIES_MONTHLY'};
      const api_func = api_funcs[interval];
      if (api_func == undefined) {
@@ -121,6 +184,11 @@
          data = await this._requestData({path:'query', queryString:`function=${apifunction}&symbol=${symbol}&market=${market}&apikey=${this.apiKey.value}`});
          labelAppend = 'Time Series (Digital Currency Monthly)';
      }
+ 
+     if ("Error Message" in data) {
+         throw new Error(`Unknown symbol: ${symbol}`);
+     }
+ 
      // Properties within the second property ("Time Series: (XX min)")
      const matches = [];
  
@@ -140,13 +208,24 @@
          timeserie.push(entryYear);
          // Push the rest of the information
          const raw = data[`${labelAppend}`][entry];
-         cleaned = {
-             'open': parseFloat(raw['1. open']),
-             'high': parseFloat(raw['2. high']),
-             'low': parseFloat(raw['3. low']),
-             'close': parseFloat(raw['4. close']),
-             'volume': parseFloat(raw['5. volume'])
-         };
+         if (apifunction == 'CRYPTO_INTRADAY') {
+             cleaned = {
+                 'open': parseFloat(raw['1. open']),
+                 'high': parseFloat(raw['2. high']),
+                 'low': parseFloat(raw['3. low']),
+                 'close': parseFloat(raw['4. close']),
+                 'volume': parseFloat(raw['5. volume'])
+             };
+         }
+         else {
+             cleaned = {
+                 'open': parseFloat(raw['1b. open (USD)']),
+                 'high': parseFloat(raw['2b. high (USD)']),
+                 'low': parseFloat(raw['3b. low (USD)']),
+                 'close': parseFloat(raw['4b. close (USD)']),
+                 'volume': parseFloat(raw['5. volume'])
+             };
+         }
          if (attributes !== 'all') {
              cleaned = cleaned[attributes];
          }
@@ -159,18 +238,20 @@
  }
  
  /**
+  * Get data of cryptocurrencies / digital currencies
   * @param {String} symbol Crypto symbol
   * @param {CryptoTimePeriod=} interval Interval selected by user
   * @param {EquitiesCryptoAttributes=} attributes May be all or only 1
   * @return {Array} Array of time stamps and associated stock price
  */
- GlobalEquities.getCryptoData = function(symbol = 'ETH', interval = 5, market = 'USD', attributes = 'all') {
+ GlobalEquities.getCryptoData = function(symbol = 'ETH', interval = 5, attributes = 'all') {
+     symbol = symbol.toUpperCase();
      const api_funcs = { daily: 'DIGITAL_CURRENCY_DAILY', weekly: 'DIGITAL_CURRENCY_WEEKLY', monthly: 'DIGITAL_CURRENCY_MONTHLY'};
      const api_func = api_funcs[interval];
      if (api_func == undefined) {
-         return GlobalEquities._raw_search_crypto('CRYPTO_INTRADAY', symbol, interval, market, attributes);
+         return GlobalEquities._raw_search_crypto('CRYPTO_INTRADAY', symbol, interval, 'USD', attributes);
      }
-     return GlobalEquities._raw_search_crypto(api_func,symbol, null, market, attributes);
+     return GlobalEquities._raw_search_crypto(api_func,symbol, null, 'USD', attributes);
  }
  
  module.exports = GlobalEquities;
