d8b2930600de331e0097cd72e5b4dfff08066de3
Devin Jean <emailcruzjean@yahoo.com>
2022-04-20 15:51:51 -0500

    Better link markup cleaning (#3399)
    * better link markup cleaning
    
    * codeclimate fix

--- src/server/services/procedures/shared-canvas/shared-canvas.js
+++ src/server/services/procedures/shared-canvas/shared-canvas.js
@@ -3,7 +3,7 @@
  * Users can view the canvas, or edit it pixel by pixel.
  * However, there is a cooldown between canvas edits, meaning one user cannot dominate the entire canvas.
  *
- * SharedCanvas was inspired by `Place <https://en.wikipedia.org/wiki/Place_(Reddit)>`__, which was
+ * SharedCanvas was inspired by `Place <https://en.wikipedia.org/wiki/R/place>`__, which was
  * a social experiment started on Reddit that functioned in much the same way. Place came to capture
  * Reddit's online culture through the combined efforts of many users striving to control the canvas
  * and display their own images.
