27201eb2836605416f68dcde5d01567d5911cfc2
Devin Jean <emailcruzjean@yahoo.com>
2022-09-26 12:19:25 -0500

    Alphavantage update (#3453)
    * return all data
    
    * date filtering + chronological

--- test/unit/server/services/procedures/alpha-vantage.spec.js
+++ test/unit/server/services/procedures/alpha-vantage.spec.js
@@ -5,8 +5,8 @@ describe(utils.suiteName(__filename), function() {
         ['convertCurrency', ['fromSymbol', 'amount', 'toSymbol']],
         ['currencySymbolSearch', ['search']],
         ['equitiesSymbolSearch', ['search']],
-        ['getCryptoData', ['symbol', 'interval', 'attributes', 'dateFormat']],
-        ['getEquityData', ['symbol', 'interval', 'attributes', 'dateFormat']],
-        ['getExchangeData', ['fromSymbol', 'toSymbol', 'interval', 'attributes', 'dateFormat']],
+        ['getCryptoData', ['symbol', 'interval', 'attributes', 'dateFormat', 'startDate', 'endDate']],
+        ['getEquityData', ['symbol', 'interval', 'attributes', 'dateFormat', 'startDate', 'endDate']],
+        ['getExchangeData', ['fromSymbol', 'toSymbol', 'interval', 'attributes', 'dateFormat', 'startDate', 'endDate']],
     ]);
 });
