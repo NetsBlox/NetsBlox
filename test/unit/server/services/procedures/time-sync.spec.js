c14753b73727b31a1309a9e961c0128aa32c3486
Devin Jean <emailcruzjean@yahoo.com>
2022-10-14 10:47:05 -0500

    Add TimeSync Service (#3464)
    * first draft
    
    * better offset timing

---
+++ test/unit/server/services/procedures/time-sync.spec.js
@@ -0,0 +1,9 @@
+const utils = require('../../../../assets/utils');
+
+describe(utils.suiteName(__filename), function() {
+    utils.verifyRPCInterfaces('TimeSync', [
+        ['prepare', ['sleepTime']],
+        ['step', []],
+        ['complete', []],
+    ]);
+});
